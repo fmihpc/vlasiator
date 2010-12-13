b3870fdf27607db3899a55e613ff34227cc06060
Arto Sandroos <arto.sandroos@fmi.fi>
2010-12-13 13:52:20 +0000

    Added a few test setups, in which there is only a single magnetic field component. Thus, you should see gyromotion.

---
+++ projects/Bx_const.h
@@ -0,0 +1,87 @@
+#include "definitions.h"
+
+/** Query if spatial cell parameters (of any cell) have changed and need to be 
+ * recalculated. If you have a completely static case, then you can always return 
+ * false here. Otherwise you need to return true so that function calcCellParameters
+ * gets called for each spatial cell.
+ * @param t The current value of time.
+ * @return If true, the spatial cell parameters need to be recalculated.
+ */
+bool cellParametersChanged(creal& t);
+
+/** Calculate parameters for the given velocity grid block.
+ * Here you need to set a value for array indices: BlockParams::Q_PER_M.
+ * @param blockParams Array containing the block parameters
+ */
+void calcBlockParameters(Real* blockParams);
+
+/** Calculate parameters for the given spatial cell at the given time.
+ * Here you need to set values for the following array indices:
+ * CellParams::EX, CellParams::EY, CellParams::EZ, CellParams::BX, CellParams::BY, and CellParams::BZ.
+ * 
+ * The following array indices contain the coordinates of the "lower left corner" of the cell: 
+ * CellParams::XCRD, CellParams::YCRD, and CellParams::ZCRD.
+ * The cell size is given in the following array indices: CellParams::DX, CellParams::DY, and CellParams::DZ.
+ * @param cellParams Array containing cell parameters.
+ * @param t The current value of time. This is passed as a convenience. If you need more detailed information 
+ * of the state of the simulation, you can read it from Parameters.
+ */
+void calcCellParameters(Real* cellParams,creal& t);
+
+/** Integrate the distribution function over the given six-dimensional phase-space cell.
+ * @param x Starting value of the x-coordinate of the cell.
+ * @param y Starting value of the y-coordinate of the cell.
+ * @param z Starting value of the z-coordinate of the cell.
+ * @param dx The size of the cell in x-direction.
+ * @param dy The size of the cell in y-direction.
+ * @param dz The size of the cell in z-direction.
+ * @param vx Starting value of the vx-coordinate of the cell.
+ * @param vy Starting value of the vy-coordinate of the cell.
+ * @param vz Starting value of the vz-coordinate of the cell.
+ * @param dvx The size of the cell in vx-direction.
+ * @param dvy The size of the cell in vy-direction.
+ * @param dvz The size of the cell in vz-direction.
+ * @return The integral of the distribution function over the given six-dimensional phase-space cell.
+ */
+Real calcPhaseSpaceDensity(creal& x,creal& y,creal& z,creal& dx,creal& dy,creal& dz,
+			   creal& vx,creal& vy,creal& vz,creal& dvx,creal& dvy,creal& dvz);
+
+template<typename T> T spatialFluxX(cuint& i,const T& avg_neg,const T& avg_pos,const T* const blockParams) {
+   creal VX = blockParams[BlockParams::VXCRD] + i*blockParams[BlockParams::DVX];
+   return convert<T>(0.5)*VX*(avg_neg+avg_pos) - convert<T>(0.5)*fabs(VX)*(avg_pos-avg_neg);
+}
+
+template<typename T> T spatialFluxY(cuint& j,const T& avg_neg,const T& avg_pos,const T* const blockParams) {
+   creal VY = blockParams[BlockParams::VYCRD] + j*blockParams[BlockParams::DVY];
+   return convert<T>(0.5)*VY*(avg_neg+avg_pos) - convert<T>(0.5)*fabs(VY)*(avg_pos-avg_neg);
+}
+
+template<typename T> T spatialFluxZ(cuint& k,const T& avg_neg,const T& avg_pos,const T* const blockParams) {
+   creal VZ = blockParams[BlockParams::VZCRD] + k*blockParams[BlockParams::DVZ];
+   return convert<T>(0.5)*VZ*(avg_neg+avg_pos) - convert<T>(0.5)*fabs(VZ)*(avg_pos-avg_neg);
+}
+
+template<typename T> T velocityFluxX(const T& J,const T& K,const T& avg_neg,const T& avg_pos,const T* const cellParams,const T* const blockParams) {
+   return convert<T>(0.0);
+}
+
+template<typename T> T velocityFluxY(const T& I,const T& K,const T& avg_neg,const T& avg_pos,const T* const cellParams,const T* const blockParams) {
+   const T VX = blockParams[BlockParams::VXCRD] + (I+convert<T>(0.5))*blockParams[BlockParams::DVX];
+   const T VZ = blockParams[BlockParams::VZCRD] + (K+convert<T>(0.5))*blockParams[BlockParams::DVZ];
+   const T BX = cellParams[CellParams::BX];
+   
+   const T AY = blockParams[BlockParams::Q_PER_M]*(VZ*BX);
+   return convert<T>(0.5)*AY*(avg_neg + avg_pos) - convert<T>(0.5)*fabs(AY)*(avg_pos-avg_neg);
+}
+
+template<typename T> T velocityFluxZ(const T& I,const T& J,const T& avg_neg,const T& avg_pos,const T* const cellParams,const T* const blockParams) {
+   const T VX = blockParams[BlockParams::VXCRD] + (I+convert<T>(0.5))*blockParams[BlockParams::DVX];
+   const T VY = blockParams[BlockParams::VYCRD] + (J+convert<T>(0.5))*blockParams[BlockParams::DVY];
+   const T BX = cellParams[CellParams::BX];
+   
+   const T AZ = blockParams[BlockParams::Q_PER_M]*(-VY*BX);
+   return convert<T>(0.5)*AZ*(avg_neg + avg_pos) - convert<T>(0.5)*fabs(AZ)*(avg_pos-avg_neg);
+}
+
+
+
---
+++ projects/Bz_const.cpp
@@ -0,0 +1,64 @@
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+
+#include "common.h"
+#include "project.h"
+#include "parameters.h"
+
+using namespace std;
+
+bool cellParametersChanged(creal& t) {return false;}
+
+Real calcPhaseSpaceDensity(creal& x,creal& y,creal& z,creal& dx,creal& dy,creal& dz,
+			   creal& vx,creal& vy,creal& vz,creal& dvx,creal& dvy,creal& dvz) {
+   // Every spatial cell gets the same velocity distribution.
+   // The velocity distribution is a shell distribution centered at v = V0.
+   
+   const Real V0 = 0.5; // Center point of Gaussian distrib.
+   const Real DV = 0.1; // Sigma of Gaussian distrib.   
+   
+   const Real DV2 = DV*DV;                 // Square of sigma
+   const Real NORM = convert<Real>(1.0) / pow(convert<Real>(2.0)*M_PI*DV2,convert<Real>(3.0/2.0)); // Normalization const.
+   const Real PHI_MIN = -20.0*M_PI/180.0;
+   const Real PHI_MAX = +20.0*M_PI/180.0;
+   
+   // Sample the distribution with N points in each coordinate direction:
+   const uint N = 2;
+   Real result = 0.0;
+   for (uint k=0; k<N; ++k) {
+      const Real VZ = vz + (k+0.5)*dvz/N;
+      for (uint j=0; j<N; ++j) {
+	 const Real VY = vy + (j+0.5)*dvy/N;
+	 for (uint i=0; i<N; ++i) {
+	    const Real VX = vx + (i+0.5)*dvx/N;
+	    const Real V = sqrt(VX*VX+VY*VY);
+	    Real PHI = acos(VX/V);
+	    if (VY < 0.0) PHI *= -1.0;
+	    
+	    if (PHI < PHI_MIN || PHI > PHI_MAX) return 0.0;
+	    result += exp(-convert<Real>(0.5)*(V-V0)*(V-V0)/DV2)*exp(-convert<Real>(0.5)*VZ*VZ/DV2);
+	 }
+      }
+   }
+   return result * NORM / (N*N*N);
+}
+
+void calcBlockParameters(Real* blockParams) {
+   blockParams[BlockParams::Q_PER_M] = 1.0;
+}
+
+void calcCellParameters(Real* cellParams,creal& t) {
+   creal x = cellParams[CellParams::XCRD];
+   creal dx = cellParams[CellParams::DX];
+   
+   cellParams[CellParams::EX   ] = 0.0;
+   cellParams[CellParams::EY   ] = 0.0;
+   cellParams[CellParams::EZ   ] = 0.0;
+   cellParams[CellParams::BX   ] = 0.0;
+   cellParams[CellParams::BY   ] = 0.0;
+   cellParams[CellParams::BZ   ] = 1.0;
+}
+
+
+
