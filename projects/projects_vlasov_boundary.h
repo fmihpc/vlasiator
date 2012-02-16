bfbf0af8cbb00777942bea368b2b2153a393dfa5
galfthan <sebastian.von.alfthan@fmi.fi>
2012-02-16 19:48:44 +0000

    Updates copy boundary conditions to sparse dist funct.
    There is still problems in using this in harm1D, but that is probably
    because of bugs with the ghost cell lists in leveque solver.
    
    QUESPACE-90

--- projects/projects_vlasov_boundary.h
+++ projects/projects_vlasov_boundary.h
@@ -7,33 +7,39 @@
 // ***** TEMPLATE DECLARATIONS *****
 // *********************************
 
-#ifdef PARGRID
-template<typename CELLID,typename UINT>
-void vlasovBoundaryCopyFromExistingFaceNbr(const CELLID& cellID,const UINT& boundaryFlags,const ParGrid<SpatialCell>& mpiGrid);
-template<typename CELLID,typename UINT,typename REAL> 
-void vlasovBoundarySetValue(const CELLID& cellID,const UINT& boundaryFlags,const ParGrid<SpatialCell>& mpiGrid,const REAL& value);
-#else
+
 template<typename CELLID,typename UINT>
 void vlasovBoundaryCopyFromExistingFaceNbr(const CELLID& cellID,const UINT& boundaryFlags,const dccrg::Dccrg<SpatialCell>& mpiGrid);
-template<typename CELLID,typename UINT,typename REAL> 
-void vlasovBoundarySetValue(const CELLID& cellID,const UINT& boundaryFlags,const dccrg::Dccrg<SpatialCell>& mpiGrid,const REAL& value);
-#endif
+
 
 // ********************************
 // ***** TEMPLATE DEFINITIONS *****
 // ********************************
-#ifdef PARGRID
-template<typename CELLID,typename UINT>
-void vlasovBoundaryCopyFromExistingFaceNbr(const CELLID& cellID,const UINT& existingCells,
-                                           const UINT& nonExistingCells,const ParGrid<SpatialCell>& mpiGrid) {
-#else
+
+//if the spatialcells are neighbors
+inline void copyCellData(SpatialCell *from, SpatialCell *to){
+   for(unsigned int block_i=0; block_i<to->number_of_blocks;block_i++){
+      unsigned int block = to->velocity_block_list[block_i];         
+      Velocity_Block* to_block_ptr = to->at(block);
+      //if block does not exist in from cell, then the empty null_block will be returned        
+      Velocity_Block* from_block_ptr = from->at(block); 
+      for (uint i=0; i<SIZE_VELBLOCK; i++) to_block_ptr->data[i]=from_block_ptr->data[i];
+   }
+}
+
+inline void zeroCellData( SpatialCell *to){
+   for(unsigned int block_i=0; block_i<to->number_of_blocks;block_i++){
+      unsigned int block = to->velocity_block_list[block_i];         
+      Velocity_Block* to_block_ptr = to->at(block);
+      //if block does not exist in from cell, then the empty null_block will be returned        
+      for (uint i=0; i<SIZE_VELBLOCK; i++) to_block_ptr->data[i]=0.0;
+   }
+}
+
+
 template<typename CELLID,typename UINT>
     void vlasovBoundaryCopyFromExistingFaceNbr(const CELLID& cellID,const UINT& existingCells,
                                                const UINT& nonExistingCells,const dccrg::Dccrg<SpatialCell>& mpiGrid) {
-#endif
-
-   std::cout <<"vlasovBounadryCopyFromExistingFaceNbr not yet functional for sparse spatial cells. Needs copyfunctions in spatialCell"<<std::endl;
-   /*
    const UINT missingPosX = (1 << 14);
    const UINT missingNegX = (1 << 12);
    const UINT missingPosY = (1 << 16);
@@ -41,90 +47,74 @@ template<typename CELLID,typename UINT>
    const UINT missingPosZ = (1 << 22);
    const UINT missingNegZ = (1 << 4);
    const UINT mask = ((1 << 4) | (1 << 10) | (1 << 12) | (1 << 14) | (1 << 16) | (1 << 22));
-   
+
+   SpatialCell* spatialCell=mpiGrid[cellID];
+//   std::cout << "cellID " << cellID << " existingcells " << existingCells <<" "<<nonExistingCells <<std::endl;
+//   std::cout << "cellID " << cellID << " " << mask << " " << missingPosX << " " << missingPosY << " " << missingPosZ <<
+//      missingNegX << " " << missingNegY << " " << missingNegZ << std::endl;
+
    switch ((nonExistingCells & mask)) {
-    case missingNegZ:
-      // If +z neighbour exists, copy values from there:
-      if (((existingCells >> 22) & 1) == 1) {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i) 
-	   mpiGrid[cellID]->cpu_avgs[i] = mpiGrid[getNeighbour(mpiGrid,cellID,0,0,+1)]->cpu_avgs[i];
-      } else {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i)
-	   mpiGrid[cellID]->cpu_avgs[i] = 0.0;
-      }
-      break;
-    case missingNegY:
+       case missingNegZ:
+          // If +z neighbour exists, copy values from there:
+          if (((existingCells >> 22) & 1) == 1){
+             copyCellData(mpiGrid[getNeighbour(mpiGrid,cellID,0,0,+1)],mpiGrid[cellID]);
+          }
+          else{
+             zeroCellData(mpiGrid[cellID]);
+          }
+          break;
+       case missingNegY:
       // If +y neighbour exists, copy values from there:
-      if (((existingCells >> 16) & 1) == 1) {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i)
-	   mpiGrid[cellID]->cpu_avgs[i] = mpiGrid[getNeighbour(mpiGrid,cellID,0,+1,0)]->cpu_avgs[i];
-      } else {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i)
-	   mpiGrid[cellID]->cpu_avgs[i] = 0.0;
-      }
-      break;
-    case missingNegX:
-      // If +x neighbour exists, copy values from there:
-      if (((existingCells >> 14) & 1) == 1) {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i) 
-	   mpiGrid[cellID]->cpu_avgs[i] = mpiGrid[getNeighbour(mpiGrid,cellID,+1,0,0)]->cpu_avgs[i];
-      } else {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i)
-	   mpiGrid[cellID]->cpu_avgs[i] = 0.0;
-      }
-      break;
-    case missingPosX:
-      // If -x neighbour exists, copy values from there:
-      if (((existingCells >> 12) & 1) == 1) {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i) 
-	   mpiGrid[cellID]->cpu_avgs[i] = mpiGrid[getNeighbour(mpiGrid,cellID,-1,0,0)]->cpu_avgs[i];
-      } else {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i)
-	   mpiGrid[cellID]->cpu_avgs[i] = 0.0;
-      }
-      break;
-    case missingPosY:
-      // If -y neighbour exists, copy values from there:
-      if (((existingCells >> 10) & 1) == 1) {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i) 
-	   mpiGrid[cellID]->cpu_avgs[i] = mpiGrid[getNeighbour(mpiGrid,cellID,0,-1,0)]->cpu_avgs[i];
-      } else {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i)
-	   mpiGrid[cellID]->cpu_avgs[i] = 0.0;
-      }
-      break;
-    case missingPosZ:
-      // If -z neighbour exists, copy values from there:
-      if (((existingCells >>  4) & 1) == 1) {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i) 
-	   mpiGrid[cellID]->cpu_avgs[i] = mpiGrid[getNeighbour(mpiGrid,cellID,0,0,-1)]->cpu_avgs[i];
-      } else {
-	 for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i)
-	   mpiGrid[cellID]->cpu_avgs[i] = 0.0;
-      }
-      break;
-    default:
-      // Set boundary cells to zero value:
-      for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i) mpiGrid[cellID]->cpu_avgs[i] = 0.0;
-      break;
+          if (((existingCells >> 16) & 1) == 1){
+             copyCellData(mpiGrid[getNeighbour(mpiGrid,cellID,0,1,0)],mpiGrid[cellID]);
+          }
+          else{
+             zeroCellData(mpiGrid[cellID]);
+          }
+          break;
+       case missingNegX:
+          // If +x  neighbour exists, copy values from there:
+          if (((existingCells >> 14) & 1) == 1) {
+             copyCellData(mpiGrid[getNeighbour(mpiGrid,cellID,1,0,0)],mpiGrid[cellID]);
+          }
+          else{
+             zeroCellData(mpiGrid[cellID]);
+          }
+          
+          break;
+       case missingPosX:
+          //If  -x neighbour exists, copy values from there:
+          if (((existingCells >> 12) & 1) == 1) {
+             copyCellData(mpiGrid[getNeighbour(mpiGrid,cellID,-1,0,0)],mpiGrid[cellID]);
+          }
+          else{
+             zeroCellData(mpiGrid[cellID]);
+          }  
+          
+          break;
+       case missingPosY:
+          // If -y neighbour exists, copy values from there:
+          if (((existingCells >> 10) & 1) == 1) {
+             copyCellData(mpiGrid[getNeighbour(mpiGrid,cellID,0,-1,0)],mpiGrid[cellID]);
+          }
+          else{
+             zeroCellData(mpiGrid[cellID]);
+          }
+          break;
+       case missingPosZ:
+          // If -z neighbour exists, copy values from there:
+          if (((existingCells >>  4) & 1) == 1) {
+             copyCellData(mpiGrid[getNeighbour(mpiGrid,cellID,0,0,-1)],mpiGrid[cellID]);
+          }
+          else{
+             zeroCellData(mpiGrid[cellID]);
+          }
+          break;
+       default:
+          // Set boundary cells to zero value:
+          zeroCellData(mpiGrid[cellID]);
+          break;
    }
-   */
 }
-
-#ifdef PARGRID 
-template<typename CELLID,typename UINT,typename REAL> 
-void vlasovBoundarySetValue(const CELLID& cellID,const UINT& existingCells,
-                            const UINT& nonExistingCells,const ParGrid<SpatialCell>& mpiGrid,const REAL& value) {
-#else
-template<typename CELLID,typename UINT,typename REAL> 
-void vlasovBoundarySetValue(const CELLID& cellID,const UINT& existingCells,
-                            const UINT& nonExistingCells,const dccrg::Dccrg<SpatialCell>& mpiGrid,const REAL& value) {
-#endif
-    for (size_t i=0; i<mpiGrid[cellID]->N_blocks*SIZE_VELBLOCK; ++i) mpiGrid[cellID]->cpu_avgs[i] = value;
-}
-
-
-
-
 
 #endif
