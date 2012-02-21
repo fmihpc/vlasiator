4be4caee30a8365663167412bb269227e686e8e0
iljah <ilja.honkonen@fmi.fi>
2012-02-21 06:54:30 +0000

    Fix copyright in test particle test.

--- projects/solar_wind_test.cpp
+++ projects/solar_wind_test.cpp
@@ -1,19 +1,19 @@
 /*
-Test particle test in GUMICS E and B fields.
+Test particle test for Vlasiator in GUMICS E and B fields.
 
-Copyright 2010, 2011 Ilja Honkonen
+Copyright 2010, 2011 Finnish Meteorological Institute
 
-This program is free software: you can redistribute it and/or modify
+Vlasiator is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License version 3
 as published by the Free Software Foundation.
 
-This program is distributed in the hope that it will be useful,
+Vlasiator is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with Vlasiator.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "boost/array.hpp"
