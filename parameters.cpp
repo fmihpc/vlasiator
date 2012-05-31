523ce7807cf25e38b11b06daaeb7dbe4e344573e
ykempf <yann.kempf@fmi.fi>
2012-05-31 11:20:38 +0000

    Updated the help entries to the latest list of variables/diagnostic available.

--- parameters.cpp
+++ parameters.cpp
@@ -144,8 +144,8 @@ bool Parameters::addParameters(){
         Readparameters::add("loadBalance.rebalanceInterval", "Load rebalance interval (steps)", 10);
 	
 	// Output variable parameters
-	Readparameters::addComposing("variables.output", "List of data reduction operators (DROs) to add to the grid file output. Each variable to be added has to be on a new line output = XXX. Available (20120403) are B E Rho RhoV MPIrank Blocks VolE VolB Pressure.");
-	Readparameters::addComposing("variables.diagnostic", "List of data reduction operators (DROs) to add to the diagnostic runtime output. Each variable to be added has to be on a new line diagnostic = XXX. Available (20120413) are Blocks FluxB Rho MaxVi.");
+	Readparameters::addComposing("variables.output", "List of data reduction operators (DROs) to add to the grid file output. Each variable to be added has to be on a new line output = XXX. Available (20120531) are B E Rho RhoV RhoLossAdjust RhoLossVelBoundary MPIrank Blocks VolE VolB Pressure dBxdz.");
+	Readparameters::addComposing("variables.diagnostic", "List of data reduction operators (DROs) to add to the diagnostic runtime output. Each variable to be added has to be on a new line diagnostic = XXX. Available (20120531) are Blocks FluxB Rho RhoLossAdjust RhoLossVelBoundary MaxVi.");
         return true;
 }
 
