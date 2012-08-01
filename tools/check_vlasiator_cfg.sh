25539f62254efac2331f8b6d039ef74d388e241b
galfthan <sebastian.von.alfthan@fmi.fi>
2012-08-01 12:47:55 +0000

    Fixes for QUESPACE-215
    A bit of help and error-correction added
    Disregards commentlines starting with #

--- tools/check_vlasiator_cfg.sh
+++ tools/check_vlasiator_cfg.sh
@@ -5,7 +5,37 @@ vlasiator=$1
 cfg=$2
 
 
-cat $cfg |gawk '{if ( $1 ~ /\[/) {prefix=substr($1,2,length($1)-2);prefix=sprintf("%s.",prefix);} else if(NF>0) printf("%s%s\n",prefix,$0)}' > .cfg_variables
+
+
+
+if [ $# -ne 2 ] 
+then
+    cat    <<EOF
+Prints out differences between parameters in a cfg fileand the options that the vlasiator executable understands.
+
+Usage: $0 vlasiator_executable cfg_file
+
+EOF
+    exit
+fi
+
+if [ ! -x $vlasiator ] 
+then    
+    echo "ERROR: Vlasiator executable $vlasiator does not exist or is not executable"
+    exit
+fi
+
+
+if [ ! -e $cfg ]
+then
+    echo "ERROR: cfg file $cfg does not exist"
+    exit
+fi
+
+
+
+#long one-liner. First remove comments, then add prefix to each name and only print if line is not empty
+cat $cfg |  grep -v "^[ ]*#" |gawk '{if ( $1 ~ /\[/) {prefix=substr($1,2,length($1)-2);prefix=sprintf("%s.",prefix);} else if(NF>0) printf("%s%s\n",prefix,$0)}' > .cfg_variables
 
 $mpirun_cmd $vlasiator --help|grep "\-\-" | sed 's/--//g'  > .vlasiator_variables
 
