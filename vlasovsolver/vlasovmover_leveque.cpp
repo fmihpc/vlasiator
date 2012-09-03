fa21946159dd6650c05498594a1ef39f26371b18
ykempf <yann.kempf@fmi.fi>
2012-09-03 12:28:59 +0000

    Managed to compile most of the code with O3 zith mpixlcxx_r but there are loads of warnings. Due to the system being down today, no testing will be done.

--- vlasovsolver/vlasovmover_leveque.cpp
+++ vlasovsolver/vlasovmover_leveque.cpp
@@ -16,6 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+# pragma options maxmem=46829
 
 #include <cstdlib>
 #include <iostream>
