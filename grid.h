c3cdd0117c68f5e1cf7fd4f51863e921def3ac00
galfthan <sebastian.von.alfthan@fmi.fi>
2012-09-25 12:35:15 +0000

    Removed definitions for writegrid and readgrid from grid.h, they are now in fileio.cpp

--- grid.h
+++ grid.h
@@ -39,33 +39,7 @@ data. This is needed if one has locally adjusted velocity blocks
 */
 void updateRemoteVelocityBlockLists(dccrg::Dccrg<SpatialCell>& mpiGrid);
 
-/*!
 
-\brief Write out system into a vlsv file
-
-\param mpiGrid   The DCCRG grid with spatial cells
-\param dataReducer Contains datareductionoperators that are used to compute data that is added into file
-\param name       File name prefix, file will be called "name.index.vlsv"
-\param index      File index, file will be called "name.index.vlsv"
-\param writeRestart If true, the full velocity distribution will be written out.
-*/
-
-
-bool writeGrid(const dccrg::Dccrg<spatial_cell::SpatialCell>& mpiGrid,
-               DataReducer& dataReducer,
-               const std::string& name,
-               const uint& index,
-               const bool& writeRestart);
-
-
-/*!
-
-\brief Read in state from a vlsv file in order to restart simulations
-*/
-
-
-bool readGrid(dccrg::Dccrg<spatial_cell::SpatialCell>& mpiGrid,
-              const std::string& name);
 
 
 
