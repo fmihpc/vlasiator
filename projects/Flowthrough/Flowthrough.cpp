3a683e7ca717df93f468f263bbe560230ddc067d
ykempf <yann.kempf@fmi.fi>
2012-10-19 07:54:01 +0000

    Bugfixes and removed unnecessary functions.

--- projects/Flowthrough/Flowthrough.cpp
+++ projects/Flowthrough/Flowthrough.cpp
@@ -87,6 +87,91 @@ bool getProjectParameters(){
    return true;
 }
 
+Real getDistribValue(creal& x,creal& y, creal& z, creal& vx, creal& vy, creal& vz, creal& dvx, creal& dvy, creal& dvz) {
+   return FTP::rho * pow(physicalconstants::MASS_PROTON / (2.0 * M_PI * physicalconstants::K_B * FTP::T), 1.5) *
+   exp(- physicalconstants::MASS_PROTON * (vx*vx + vy*vy + vz*vz) / (2.0 * physicalconstants::K_B * FTP::T));
+}
+
+/** Integrate the distribution function over the given six-dimensional phase-space cell.
+ * @param x Starting value of the x-coordinate of the cell.
+ * @param y Starting value of the y-coordinate of the cell.
+ * @param z Starting value of the z-coordinate of the cell.
+ * @param dx The size of the cell in x-direction.
+ * @param dy The size of the cell in y-direction.
+ * @param dz The size of the cell in z-direction.
+ * @param vx Starting value of the vx-coordinate of the cell.
+ * @param vy Starting value of the vy-coordinate of the cell.
+ * @param vz Starting value of the vz-coordinate of the cell.
+ * @param dvx The size of the cell in vx-direction.
+ * @param dvy The size of the cell in vy-direction.
+ * @param dvz The size of the cell in vz-direction.
+ * @return The volume average of the distribution function in the given phase space cell.
+ * The physical unit of this quantity is 1 / (m^3 (m/s)^3).
+ */
+Real calcPhaseSpaceDensity(creal& x, creal& y, creal& z, creal& dx, creal& dy, creal& dz, creal& vx, creal& vy, creal& vz, creal& dvx, creal& dvy, creal& dvz) {   
+   creal d_x = dx / (FTP::nSpaceSamples-1);
+   creal d_y = dy / (FTP::nSpaceSamples-1);
+   creal d_z = dz / (FTP::nSpaceSamples-1);
+   creal d_vx = dvx / (FTP::nVelocitySamples-1);
+   creal d_vy = dvy / (FTP::nVelocitySamples-1);
+   creal d_vz = dvz / (FTP::nVelocitySamples-1);
+   
+   Real avg = 0.0;
+// #pragma omp parallel for collapse(6) reduction(+:avg)
+   // WARNING No threading here if calling functions are already threaded
+   for (uint i=0; i<FTP::nSpaceSamples; ++i)
+      for (uint j=0; j<FTP::nSpaceSamples; ++j)
+         for (uint k=0; k<FTP::nSpaceSamples; ++k)
+            for (uint vi=0; vi<FTP::nVelocitySamples; ++vi)
+               for (uint vj=0; vj<FTP::nVelocitySamples; ++vj)
+                  for (uint vk=0; vk<FTP::nVelocitySamples; ++vk) {
+                     avg += getDistribValue(x+i*d_x, y+j*d_y, z+k*d_z, vx+vi*d_vx, vy+vj*d_vy, vz+vk*d_vz, dvx, dvy, dvz);
+                  }
+   return avg / pow(FTP::nSpaceSamples, 3.0) / pow(FTP::nVelocitySamples, 3.0);
+   
+//    CellID cellID = 1 + round((x - Parameters::xmin) / dx + 
+//    (y - Parameters::ymin) / dy * Parameters::xcells_ini +
+//    (z - Parameters::zmin) / dz * Parameters::ycells_ini * Parameters::xcells_ini);
+   
+//    return cellID * pow(physicalconstants::MASS_PROTON / (2.0 * M_PI * physicalconstants::K_B * FTP::T), 1.5) *
+//    exp(- physicalconstants::MASS_PROTON * (vx*vx + vy*vy + vz*vz) / (2.0 * physicalconstants::K_B * FTP::T));
+}
+
+/** Calculate parameters for the given spatial cell at the given time.
+ * Here you need to set values for the following array indices:
+ * CellParams::EX, CellParams::EY, CellParams::EZ, CellParams::BX, CellParams::BY, and CellParams::BZ.
+ * 
+ * The following array indices contain the coordinates of the "lower left corner" of the cell: 
+ * CellParams::XCRD, CellParams::YCRD, and CellParams::ZCRD.
+ * The cell size is given in the following array indices: CellParams::DX, CellParams::DY, and CellParams::DZ.
+ * @param cellParams Array containing cell parameters.
+ * @param t The current value of time. This is passed as a convenience. If you need more detailed information 
+ * of the state of the simulation, you can read it from Parameters.
+ */
+void calcCellParameters(Real* cellParams,creal& t) {
+   cellParams[CellParams::EX  ] = 0.0;
+   cellParams[CellParams::EY  ] = 0.0;
+   cellParams[CellParams::EZ  ] = 0.0;
+   cellParams[CellParams::BGBX] = FTP::Bx;
+   cellParams[CellParams::BGBY] = FTP::By;
+   cellParams[CellParams::BGBZ] = FTP::Bz;
+}
+
+void dipole(creal x, creal y, creal z, Real& Bx, Real &By, Real& Bz) {
+   creal k_0 = 8.0e15; // Wb m
+   Real r = sqrt(x*x + y*y + z*z); // radial
+   Real theta = atan2(sqrt(x*x + y*y), z); // polar
+   Real phi = atan2(y, x); // azimuthal
+   
+   Bx = sin(theta) * cos(theta) * cos(phi);
+   By = sin(theta) * cos(theta) * sin(phi);
+   Bz = cos(theta)*cos(theta) - 1.0 / 3.0;
+   
+   Bx *= 3.0 * k_0 / (r*r*r);
+   By *= 3.0 * k_0 / (r*r*r);
+   Bz *= 3.0 * k_0 / (r*r*r);
+}
+
 void setProjectCell(SpatialCell* cell) {
    // Set up cell parameters:
    calcCellParameters(&((*cell).parameters[0]), 0.0);
@@ -135,78 +220,8 @@ void setProjectCell(SpatialCell* cell) {
                      }
                   }
          }
-   calculateCellVelocityMoments(cell);
-   
-   //let's get rid of blocks not fulfilling the criteria here to save memory.
-   cell->adjustSingleCellVelocityBlocks();
-}
-
-Real getDistribValue(creal& x,creal& y, creal& z, creal& vx, creal& vy, creal& vz, creal& dvx, creal& dvy, creal& dvz) {
-   return FTP::rho * pow(physicalconstants::MASS_PROTON / (2.0 * M_PI * physicalconstants::K_B * FTP::T), 1.5) *
-   exp(- physicalconstants::MASS_PROTON * (vx*vx + vy*vy + vz*vz) / (2.0 * physicalconstants::K_B * FTP::T));
-}
-
-Real calcPhaseSpaceDensity(creal& x, creal& y, creal& z, creal& dx, creal& dy, creal& dz, creal& vx, creal& vy, creal& vz, creal& dvx, creal& dvy, creal& dvz) {   
-   creal d_x = dx / (FTP::nSpaceSamples-1);
-   creal d_y = dy / (FTP::nSpaceSamples-1);
-   creal d_z = dz / (FTP::nSpaceSamples-1);
-   creal d_vx = dvx / (FTP::nVelocitySamples-1);
-   creal d_vy = dvy / (FTP::nVelocitySamples-1);
-   creal d_vz = dvz / (FTP::nVelocitySamples-1);
-   
-   Real avg = 0.0;
-// #pragma omp parallel for collapse(6) reduction(+:avg)
-   // WARNING No threading here if calling functions are already threaded
-   for (uint i=0; i<FTP::nSpaceSamples; ++i)
-      for (uint j=0; j<FTP::nSpaceSamples; ++j)
-         for (uint k=0; k<FTP::nSpaceSamples; ++k)
-            for (uint vi=0; vi<FTP::nVelocitySamples; ++vi)
-               for (uint vj=0; vj<FTP::nVelocitySamples; ++vj)
-                  for (uint vk=0; vk<FTP::nVelocitySamples; ++vk) {
-                     avg += getDistribValue(x+i*d_x, y+j*d_y, z+k*d_z, vx+vi*d_vx, vy+vj*d_vy, vz+vk*d_vz, dvx, dvy, dvz);
-                  }
-   return avg / pow(FTP::nSpaceSamples, 3.0) / pow(FTP::nVelocitySamples, 3.0);
-   
-//    CellID cellID = 1 + round((x - Parameters::xmin) / dx + 
-//    (y - Parameters::ymin) / dy * Parameters::xcells_ini +
-//    (z - Parameters::zmin) / dz * Parameters::ycells_ini * Parameters::xcells_ini);
-   
-//    return cellID * pow(physicalconstants::MASS_PROTON / (2.0 * M_PI * physicalconstants::K_B * FTP::T), 1.5) *
-//    exp(- physicalconstants::MASS_PROTON * (vx*vx + vy*vy + vz*vz) / (2.0 * physicalconstants::K_B * FTP::T));
-}
-
-bool cellParametersChanged(creal& t) {return false;}
-
-void calcBlockParameters(Real* blockParams) { }
-
-void calcCellParameters(Real* cellParams,creal& t) {
-   cellParams[CellParams::EX   ] = 0.0;
-   cellParams[CellParams::EY   ] = 0.0;
-   cellParams[CellParams::EZ   ] = 0.0;
-   cellParams[CellParams::BX   ] = FTP::Bx;
-   cellParams[CellParams::BY   ] = FTP::By;
-   cellParams[CellParams::BZ   ] = FTP::Bz;
-}
-
-// TODO use this instead: template <class Grid, class CellData> void calcSimParameters(Grid<CellData>& mpiGrid...
-void calcSimParameters(dccrg::Dccrg<SpatialCell>& mpiGrid, creal& t, Real& /*dt*/) {
-   std::vector<uint64_t> cells = mpiGrid.get_cells();
-   for (uint i = 0; i < cells.size(); ++i) {
-      calcCellParameters(mpiGrid[cells[i]]->parameters, t);
-   }
-}
-
-void dipole(creal x, creal y, creal z, Real& Bx, Real &By, Real& Bz) {
-   creal k_0 = 8.0e15; // Wb m
-   Real r = sqrt(x*x + y*y + z*z); // radial
-   Real theta = atan2(sqrt(x*x + y*y), z); // polar
-   Real phi = atan2(y, x); // azimuthal
-   
-   Bx = sin(theta) * cos(theta) * cos(phi);
-   By = sin(theta) * cos(theta) * sin(phi);
-   Bz = cos(theta)*cos(theta) - 1.0 / 3.0;
-   
-   Bx *= 3.0 * k_0 / (r*r*r);
-   By *= 3.0 * k_0 / (r*r*r);
-   Bz *= 3.0 * k_0 / (r*r*r);
+         calculateCellVelocityMoments(cell);
+         
+         //let's get rid of blocks not fulfilling the criteria here to save memory.
+         cell->adjustSingleCellVelocityBlocks();
 }
--- projects/Flowthrough/Flowthrough.h
+++ projects/Flowthrough/Flowthrough.h
@@ -51,55 +51,6 @@ bool addProjectParameters(void);
  */
 bool getProjectParameters(void);
 
-/** Query if spatial cell parameters (of any cell) have changed and need to be 
- * recalculated. If you have a completely static case, then you can always return 
- * false here. Otherwise you need to return true so that function calcCellParameters
- * gets called for each spatial cell.
- * @param t The current value of time.
- * @return If true, the spatial cell parameters need to be recalculated.
- */
-bool cellParametersChanged(creal& t);
-
-/** Calculate parameters for the given velocity grid block.
- * Here you need to set a value for array indices: BlockParams::Q_PER_M.
- * @param blockParams Array containing the block parameters
- */
-void calcBlockParameters(Real* blockParams);
-
-/** Calculate parameters for the given spatial cell at the given time.
- * Here you need to set values for the following array indices:
- * CellParams::EX, CellParams::EY, CellParams::EZ, CellParams::BX, CellParams::BY, and CellParams::BZ.
- * 
- * The following array indices contain the coordinates of the "lower left corner" of the cell: 
- * CellParams::XCRD, CellParams::YCRD, and CellParams::ZCRD.
- * The cell size is given in the following array indices: CellParams::DX, CellParams::DY, and CellParams::DZ.
- * @param cellParams Array containing cell parameters.
- * @param t The current value of time. This is passed as a convenience. If you need more detailed information 
- * of the state of the simulation, you can read it from Parameters.
- */
-void calcCellParameters(Real* cellParams,creal& t);
-
-void calcSimParameters(dccrg::Dccrg<SpatialCell>& mpiGrid, creal& t, Real& dt);
-
-/** Integrate the distribution function over the given six-dimensional phase-space cell.
- * @param x Starting value of the x-coordinate of the cell.
- * @param y Starting value of the y-coordinate of the cell.
- * @param z Starting value of the z-coordinate of the cell.
- * @param dx The size of the cell in x-direction.
- * @param dy The size of the cell in y-direction.
- * @param dz The size of the cell in z-direction.
- * @param vx Starting value of the vx-coordinate of the cell.
- * @param vy Starting value of the vy-coordinate of the cell.
- * @param vz Starting value of the vz-coordinate of the cell.
- * @param dvx The size of the cell in vx-direction.
- * @param dvy The size of the cell in vy-direction.
- * @param dvz The size of the cell in vz-direction.
- * @return The volume average of the distribution function in the given phase space cell.
- * The physical unit of this quantity is 1 / (m^3 (m/s)^3).
- */
-Real calcPhaseSpaceDensity(creal& x,creal& y,creal& z,creal& dx,creal& dy,creal& dz,
-			   creal& vx,creal& vy,creal& vz,creal& dvx,creal& dvy,creal& dvz);
-
 /*!\brief Set the fields and distribution of a cell according to the default simulation settings.
  * This is used for the NOT_SYSBOUNDARY cells and some other system boundary conditions (e.g. Outflow).
  * \param cell Pointer to the cell to set.
