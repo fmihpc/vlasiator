b1eb5edd654ca8d46c5965cb52f9af2d09ff6aab
galfthan <sebastian.von.alfthan@fmi.fi>
2012-10-19 08:45:14 +0000

    Harm1d compiles, cfg needs work

--- projects/harm1D/harm1D.cpp
+++ projects/harm1D/harm1D.cpp
@@ -28,6 +28,39 @@ along with Vlasiator. If not, see <http://www.gnu.org/licenses/>.
 #include "readparameters.h"
 #include "vlasovmover.h"
 
+
+/** Calculate parameters for the given spatial cell at the given time.
+ * Here you need to set values for the following array indices:
+ * CellParams::EX, CellParams::EY, CellParams::EZ, CellParams::BX, CellParams::BY, and CellParams::BZ.
+ * 
+ * The following array indices contain the coordinates of the "lower left corner" of the cell: 
+ * CellParams::XCRD, CellParams::YCRD, and CellParams::ZCRD.
+ * The cell size is given in the following array indices: CellParams::DX, CellParams::DY, and CellParams::DZ.
+ * @param cellParams Array containing cell parameters.
+ * @param t The current value of time. This is passed as a convenience. If you need more detailed information 
+ * of the state of the simulation, you can read it from Parameters.
+ */
+void calcCellParameters(Real* cellParams,creal& t);
+
+/** Integrate the distribution function over the given six-dimensional phase-space cell.
+ * @param x Starting value of the x-coordinate of the cell.
+ * @param y Starting value of the y-coordinate of the cell.
+ * @param z Starting value of the z-coordinate of the cell.
+ * @param dx The size of the cell in x-direction.
+ * @param dy The size of the cell in y-direction.
+ * @param dz The size of the cell in z-direction.
+ * @param vx Starting value of the vx-coordinate of the cell.
+ * @param vy Starting value of the vy-coordinate of the cell.
+ * @param vz Starting value of the vz-coordinate of the cell.
+ * @param dvx The size of the cell in vx-direction.
+ * @param dvy The size of the cell in vy-direction.
+ * @param dvz The size of the cell in vz-direction.
+ * @return The volume average of the distribution function in the given phase space cell.
+ * The physical unit of this quantity is 1 / (m^3 (m/s)^3).
+ */
+Real calcPhaseSpaceDensity(creal& x,creal& y,creal& z,creal& dx,creal& dy,creal& dz,
+			   creal& vx,creal& vy,creal& vz,creal& dvx,creal& dvy,creal& dvz);
+
 using namespace std;
 
 bool initializeProject(void) {return true;}
@@ -88,7 +121,6 @@ void setProjectCell(SpatialCell* cell) {
          cell->adjustSingleCellVelocityBlocks();
 }
 
-bool cellParametersChanged(creal& t) {return false;}
 
 Real calcPhaseSpaceDensity(creal& x,creal& y,creal& z,creal& dx,creal& dy,creal& dz,
 			   creal& vx,creal& vy,creal& vz,creal& dvx,creal& dvy,creal& dvz) {
@@ -123,9 +155,6 @@ Real calcPhaseSpaceDensity(creal& x,creal& y,creal& z,creal& dx,creal& dy,creal&
    return 1.0;
 }
 
-void calcBlockParameters(Real* blockParams) {
-   //blockParams[BlockParams::Q_PER_M] = 1.0;
-}
 
 void calcCellParameters(Real* cellParams,creal& t) {
    creal x = cellParams[CellParams::XCRD];
@@ -142,24 +171,8 @@ void calcCellParameters(Real* cellParams,creal& t) {
    cellParams[CellParams::BGBX   ] = 0.0;
    cellParams[CellParams::BGBY   ] = 0.0;
    cellParams[CellParams::BGBZ   ] = 0.0;
-   
-   // Volume-averaged fields need to be set:
-   cellParams[CellParams::EXVOL] = -1.0*(x+0.5*dx);
-   cellParams[CellParams::EYVOL] = 0.0;
-   cellParams[CellParams::EZVOL] = 0.0;
-   cellParams[CellParams::BXVOL] = 0.0;
-   cellParams[CellParams::BYVOL] = 0.0;
-   cellParams[CellParams::BZVOL] = 0.0;
 }
 
-// TODO use this instead: template <class Grid, class CellData> void calcSimParameters(Grid<CellData>& mpiGrid...
-
-void calcSimParameters(dccrg::Dccrg<SpatialCell>& mpiGrid, creal& t, Real& /*dt*/) {
-   std::vector<uint64_t> cells = mpiGrid.get_cells();
-   for (uint i = 0; i < cells.size(); ++i) {
-      calcCellParameters(mpiGrid[cells[i]]->parameters, t);
-   }
-}
 
 
 
