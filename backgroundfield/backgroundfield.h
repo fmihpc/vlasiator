660498f62bffc015ccc3caf6a6bdfbe4ccd820cf
galfthan <sebastian.von.alfthan@fmi.fi>
2013-01-16 10:45:12 +0000

    New bg field compiles

--- backgroundfield/backgroundfield.h
+++ backgroundfield/backgroundfield.h
@@ -21,7 +21,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include "fieldfunction.hpp"
 
-void setBackgroundField(const FieldFunction& bgFunction,Real* cellParams);
+void setBackgroundField(FieldFunction& bgFunction,Real* cellParams);
 
 #endif
 
