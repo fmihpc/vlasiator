77a91e96dd8780b7099dccd3e38fc444d9cc8baf
galfthan <sebastian.von.alfthan@fmi.fi>
2013-01-21 14:31:33 +0000

    More modifications, new names for tests

--- tools/test_vlasiator/run_tests.sh
+++ tools/test_vlasiator/run_tests.sh
@@ -7,58 +7,55 @@
 
 
 ## variables ##
-
+#vlasiator binary
+bin=vlasiator
+#processes and threads
 p=2
 t=12
-run_command="aprun"
-
-#IFS=$',' # separate test names by , without spaces
-
-create_verification_files=0
+#where the tests are run
+run_dir="run"
 
 # choose tests to run
 #run_tests=(1 2
 run_tests=(1 )
 
-
 # test 1
-test_name[1]="Fluctuations"
-test_cfg[1]="data/Fluctuations.cfg"
+test_name[1]="Fluctuations_A"
+test_cfg[1]="data/Fluctuations_A.cfg"
+comparison_vlsv[1]="grid.0000010.vlsv"
 
-# test 1
-test_name[2]="test_fp"
-test_cfg[2]="data/test_fp.cfg"
+# test 2
+test_name[2]="test_fp_A"
+test_cfg[2]="data/test_fp_A.cfg"
+comparison_vlsv[2]="grid.0000001.vlsv"
 
+
+
+#If 1, the reference vlsv files are generated, if 0 then we do the comparison
+create_verification_files=0
 #folder for reference data
-reference_dir="reference"
-#where the tests are run
-run_dir="run"
-
-#vlasiator binary
-bin=vlasiator
-
+reference_dir="/stornext/field/users/alfthan/vlasiator_reference_data"
 # give here t%he variables you want to be tested
 variables_name=( "rho" "rho_v" "rho_v" "rho_v" "B" "B" "B" "E" "E" "E" )
 # and the corresponding components to variables, 
 variables_components=( 0 0 1 2 0 1 2 0 1 2)
 #arrays variables_name and variables_components should have same number of elements, e.g., 4th variable is variables_name[4] variables_components[4]=  
 
-# choose the length of the test run: short/long
-#short_long="short"
 
 
 
-
-
+##--------------------------------------------------##
 ## code, no need to touch ##
+##--------------------------------------------------##
 
-#Go to PBS_O_WORKDIR if set (batch job), otherwise go to current folder
+
+#command for running stuff, FIXME: should be a function or so that could easily be extended to mpirun etc
+run_command="aprun"
+
+#get baseddir from PBS_O_WORKDIR if set (batch job), otherwise go to current folder
 #http://stackoverflow.com/questions/307503/whats-the-best-way-to-check-that-environment-variables-are-set-in-unix-shellscr
-
 base_dir=${PBS_O_WORKDIR:=$(pwd)}
-
 cd  $base_dir
-
 
 ## add absolute paths to folder names, filenames
 reference_dir=$( readlink -f $reference_dir )
@@ -70,103 +67,76 @@ for run in ${run_tests[*]}
 done  
 
 
-
 if [ $create_verification_files == 1 ]
     then
-    # directory where the good results are for comparison
-    if [ !  -d $reference_dir ]
-        then
-        mkdir $reference_dir 
-    fi
-
-    
-    for run in ${run_tests[*]}
-      do
-# directory where the good results are for comparison
-      result_dir=${reference_dir}/${test_name[$run]}
-      if [ -d $result_dir ]
-          then
-          #we do not want to save old stuff
-          #rm -rf $result_dir
-          echo "delete"
-      fi
-      
-      mkdir $result_dir
-      cd  $result_dir
-      echo "Compute reference for ${test_name[$run]} into $( pwd )"
-      
-      export OMP_NUM_THREADS=$t
-      export MPICH_MAX_THREAD_SAFETY=funneled
-      $run_command -n $p -d $t $bin --run_config=${test_cfg[$run]}
-      
-      #down to basic folder, strictly taken not needed
-      cd $base_dir
-    done
+    #if we create the references, then lets simply run in the reference dir and turn off tests below
+    run_dir=$reference_dir
+    echo "Computing reference results into $run_dir"
 else
-
-    if [ !  -d $run_dir ]
-        then
-        mkdir $run_dir 
-    fi
-
-#print header
+    #print header
+    echo "Verifying $bin"
     echo "------------------------------------------------------------------------------------------------------------" 
     echo "      Test case         |     variable     |          value           |           ok/bad                    "  
+fi
+
+
+#create main run folder if it doesn not exist
+if [ !  -d $run_dir ]
+    then
+    mkdir $run_dir 
+fi
 
 
 # loop over different test cases
-    for run in ${run_tests[*]}
-      do
-      
-      
+for run in ${run_tests[*]}
+  do
 # directory for test results
-      vlsv_dir=${run_dir}/${test_name[$run]}
-
+  vlsv_dir=${run_dir}/${test_name[$run]}
+  
 # Check if files for new vlsv files exists, if not create them
-      if [ ! -d ${vlsv_dir} ]; then
-          echo creating ${vlsv_dir}
-          mkdir ${vlsv_dir}
-      else
-          rm -f ${vlsv_dir}/*
-      fi
+  if [ ! -d ${vlsv_dir} ]; then
+      mkdir ${vlsv_dir}
+  else
+      rm -f ${vlsv_dir}/*
+  fi
 
 # copy cfg file and change to run directory of the test case, e.g. test_Fluctuations
-      cd ${vlsv_dir}
-      
-      export OMP_NUM_THREADS=$t
-      export MPICH_MAX_THREAD_SAFETY=funneled
-      $run_command -n $p -d $t $bin --run_config=${test_cfg[$run]}
+  cd ${vlsv_dir}
+  
+  export OMP_NUM_THREADS=$t
+  export MPICH_MAX_THREAD_SAFETY=funneled
+  $run_command -n $p -d $t $bin --run_config=${test_cfg[$run]}
+  cd $base_dir
 
 
-      cd $base_dir
 
+### TESTS #####
+  if [ ! $create_verification_files == 1 ]
+      then
 ##Compare test case with right solutions
-
-# directory where the good results are for comparison
       result_dir=${reference_dir}/${test_name[$run]}
-# loop over variables
 
+#print header
+      
       echo "------------------------------------------------------------------------------------------------------------"
       for i in ${!variables_name[*]}
         do
 
-        value=$( vlsvdiff_DP ${result_dir}/grid.0000001.vlsv ${vlsv_dir}/grid.0000001.vlsv ${variables_name[$i]} ${variables_components[$i]} |grep "The relative 0-distance between both datasets" |gawk '{print $8}'  )
+        value=$( vlsvdiff_DP ${result_dir}/${comparison_vlsv[$run]} ${vlsv_dir}/${comparison_vlsv[$run]} ${variables_name[$i]} ${variables_components[$i]} |grep "The relative 0-distance between both datasets" |gawk '{print $8}'  )
 
-
-        a=$(awk -vx="$value" 'BEGIN{print (x > 0 ? x:-x)}')
-        b=$(awk -vn="$a" 'BEGIN{print ( n<=10^(-14))?1:0 }')
-
+        b=$(awk -vn="$value" 'BEGIN{print ( n<=10^(-14) && n>=-10^(-14) )?1:0 }')
 
         if [ "$b" == "1" ]; then
             status="ok"
         else
-            status="bad"
+            status="!!!!  ERROR  !!!!"
         fi
 #print the results      
         echo "      ${test_name[$run]}          ${variables_name[$i]} ${variables_components[$i]}            $value                $status              "
       done # loop over variables
       echo "------------------------------------------------------------------------------------------------------------"
-    done # loop over tests
+  fi
+done # loop over tests
 
-fi
+  
 
