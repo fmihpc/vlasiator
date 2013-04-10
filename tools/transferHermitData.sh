fd6cf106fe15d1611318f16dc2ad07af5b5c026a
galfthan <sebastian.von.alfthan@fmi.fi>
2013-04-10 11:40:30 +0000

    Bugfixes, and some documentation for tranfer script

--- tools/transferHermitData.sh
+++ tools/transferHermitData.sh
@@ -4,33 +4,53 @@
 server=gsiftp://gridftp-fr1.hww.de:2812
 path=$1
 inputfile=$2
-echo $inputfile
+
+if [ ! $# -eq 2 ]
+then
+cat <<EOF
+transferHermitData path transfer_file
+
+    Script for transferring data using gridFTP. Please start up the proxy using grid_proxy_init first
+
+    path          is a path on hermit (e.g. /univ_1/ws1/ws/iprsalft-paper1-runs-0/2D/ecliptic/AAE)"
+    transfer_file is a file in the path on hermit created using ls -la *myfiles* > tranfer.txt"       
+EOF
+
+exit
+
+fi
+
+export GLOBUS_TCP_SOURCE_RANGE=20000,20500
+export GLOBUS_TCP_PORT_RANGE=20000,20500
 
 
-echo "Downloading inputfile" 
+
+echo "Downloading inputfile $inputfile from $path" 
 globus-url-copy  -rst  gsiftp://gridftp-fr1.hww.de:2812/${path}/$inputfile ./$inputfile
+cat $inputfile |gawk '{s=s+$5} END {print "Downloading ", NR, " files with ",s/(1024*1024*1024),"GB of data"}'
+
 
 while read line; do
-
-#inputfile produced with ls -la
+    #inputfile produced with ls -la, get name and size
     file=$(echo $line | gawk '{print $9}')  
     size=$(echo $line | gawk '{print $5}')
   
-#chunksize
+    #chunksize
     chunkSize=1000000000
-    retval=0
     totalChunks=$(( 1+size/chunkSize )) 
+    retval=0
     if [ -e $file ] 
 	then
+	#file exists
 	localSize=$( ls -la  $file | gawk '{print $5}' )
 	#Start from next possible chunkposition, some data may be lost from incomplete chunk
 	i=$(( localSize / chunkSize ))
 	if [ $localSize -eq  $size ]
 	then
+	    #file complete
 	    retval=1
 	    echo "File $file is already transferred"
 	fi
-	    
     else
 	#nothing has been transferred, start from beginning
 	i=0
@@ -39,6 +59,7 @@ while read line; do
     retryIndex=0
     while [ $retval -eq 0 ]
     do
+	#offset into file where we start to download data
 	offset=$(echo $i $chunkSize|gawk '{print $1*$2}')
 	
 	echo "Downloading $f chunk $((i+1))/$totalChunks at $offset for $file" 
@@ -47,32 +68,29 @@ while read line; do
 	localSize=$( ls -la  $file | gawk '{print $5}' )
 	if [ $localSize -eq $size ] 
 	then
+            #the whole file has been downloaded, excellent!
 	    echo "Done"
 	    retval=1
 	    retryIndex=0
-	fi
-	
-    
-
-	if [ $localSize -lt $(( chunkSize + offset )) ]
-	then
-	    if [ $retval -eq 0 ]
-	    then
-		echo "Last transfer failed, retry"
-	        retryIndex=$(( retryIndex+1 ))
-	    fi
 	else
-	    i=$(( i+1 ))
-	    retryIndex=0
+	    #file not complete
+	    if [ $localSize -lt $(( chunkSize + offset )) ]
+	    then
+     	        #we failed to download the whole chunk
+	        retryIndex=$(( retryIndex+1 ))
+		echo "Chunk transfer failed, retry number $retryIndex "
+		if [ $retryIndex -gt 2 ]
+		then
+		    echo "Too many retries, abort"
+		    echo "Failed on reading to offset $offset"
+		    retval=2
+		fi
+	    else
+		#chunk downloaded, lets get the next one
+		i=$(( i+1 ))
+		retryIndex=0
+	    fi 
 	fi
-
-	if [ $retryIndex -eq 2 ]
-	then
-	    echo "Too many retries, abort"
-	    echo "Failed on reading to offset $offset"
-	    retVal=2
-	fi
-	    
     done
 done < $inputfile
 
