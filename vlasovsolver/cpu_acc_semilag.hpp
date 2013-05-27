d83582078d742f090cb1312a36bade6ef96aa3b2
galfthan <sebastian.von.alfthan@fmi.fi>
2013-05-27 12:00:33 +0000

    Added rotation origin

--- vlasovsolver/cpu_acc_semilag.hpp
+++ vlasovsolver/cpu_acc_semilag.hpp
@@ -53,6 +53,10 @@ double get_relative_shared_volume(
 
 /*!
 Propagates the distribution function in velocity space of given real space cell.
+
+TODO:
+  now this is all double: enable Real
+
 */
 void cpu_accelerate_cell(
    SpatialCell* spatial_cell,
@@ -66,16 +70,31 @@ void cpu_accelerate_cell(
    /*perturbed field*/
    const double perBx = spatial_cell->parameters[CellParams::PERBXVOL];
    const double perBy = spatial_cell->parameters[CellParams::PERBYVOL];
-   const double perBz = spatial_cell->parameters[CellParams::PERBZVOL];
-
-
-   
-   Eigen::Vector3d B(Bx,By,Bz);
-   Eigen::Vector3d unitB(B.normalized());
+   const double perBz = spatial_cell->parameters[CellParams::PERBZVOL];   
+   const Eigen::Vector3d B(Bx,By,Bz);
+   const Eigen::Vector3d unitB(B.normalized());
    const double gyro_period = 2 * M_PI * Parameters::m  / (fabs(Parameters::q) * B.norm());
 
+   //Compute maximum timestep limit for this cell, based ona  maximum allowed rotation angle
+   //TODO, max angle could be read in from cfg
+   spatial_cell->parameters[CellParams::MAXVDT]=gyro_period*(10.0/360.0);
+
    
-   /*copy distribution function values int o the flux table, and zero the existing distribution function*/
+   //compute initial moments, based on actual distribution function
+   spatial_cell->parameters[CellParams::RHO_V  ] = 0.0;
+   spatial_cell->parameters[CellParams::RHOVX_V] = 0.0;
+   spatial_cell->parameters[CellParams::RHOVY_V] = 0.0;
+   spatial_cell->parameters[CellParams::RHOVZ_V] = 0.0;
+   for(unsigned int block_i=0; block_i< spatial_cell->number_of_blocks;block_i++){
+      unsigned int block = spatial_cell->velocity_block_list[block_i];         
+      cpu_calcVelocityMoments(spatial_cell,block,CellParams::RHO_V,CellParams::RHOVX_V,CellParams::RHOVY_V,CellParams::RHOVZ_V);   
+   }
+   const double rho=spatial_cell->parameters[CellParams::RHO_V];
+   Eigen::Vector3d bulk_velocity(spatial_cell->parameters[CellParams::RHOVX_V]/rho,
+                                 spatial_cell->parameters[CellParams::RHOVY_V]/rho,
+                                 spatial_cell->parameters[CellParams::RHOVZ_V]/rho);   
+   
+   /*copy distribution function values into the flux table, and zero the existing distribution function*/
    for (unsigned int block_i = 0; block_i < spatial_cell->number_of_blocks; block_i++) {
       const unsigned int block = spatial_cell->velocity_block_list[block_i];
       Velocity_Block* block_ptr = spatial_cell->at(block);
@@ -84,12 +103,39 @@ void cpu_accelerate_cell(
          block_ptr->data[cell] = 0.0;
       }
    }
+
    
-   // Make a copy of the blocklist as we  don't want to iterate over blocks added by this function
+   // Make a copy of the blocklist as we don't want to iterate over blocks added by this function
    std::vector<unsigned int> blocks;
    for (unsigned int block_i = 0; block_i < spatial_cell->number_of_blocks; block_i++) {
       blocks.push_back(spatial_cell->velocity_block_list[block_i]);
    }
+
+
+   /*rotation origin is the point through which we place our rotation axis (direction of which is unitB)*/
+   /*first add bulk velocity*/
+   Eigen::Vector3d rotation_origin(-bulk_velocity);
+   
+   if(Parameters::lorentzHallTerm) {
+      //inlude lorentzHallTerm (we should include, always)
+      //read in derivatives need for curl of B (only pertrubed, curl of background field is always 0!)
+      const double dBXdy = spatial_cell->derivativesBVOL[bvolderivatives::dPERBXVOLdy]/spatial_cell->parameters[CellParams::DY];
+      const double dBXdz = spatial_cell->derivativesBVOL[bvolderivatives::dPERBXVOLdz]/spatial_cell->parameters[CellParams::DZ];
+      const double dBYdx = spatial_cell->derivativesBVOL[bvolderivatives::dPERBYVOLdx]/spatial_cell->parameters[CellParams::DX];
+      const double dBYdz = spatial_cell->derivativesBVOL[bvolderivatives::dPERBYVOLdz]/spatial_cell->parameters[CellParams::DZ];
+      const double dBZdx = spatial_cell->derivativesBVOL[bvolderivatives::dPERBZVOLdx]/spatial_cell->parameters[CellParams::DX];
+      const double dBZdy = spatial_cell->derivativesBVOL[bvolderivatives::dPERBZVOLdy]/spatial_cell->parameters[CellParams::DY];
+      //scale rho, if user requests
+      const double hallRho =  (rho <= Parameters::lorentzHallMinimumRho ) ? Parameters::lorentzHallMinimumRho : rho ;
+      const double hallPrefactor = 1.0 / (physicalconstants::MU_0 * hallRho * Parameters::q );
+      rotation_origin[0]+=hallPrefactor*(dBZdy - dBYdz);
+      rotation_origin[1]+=hallPrefactor*(dBXdz - dBZdx);
+      rotation_origin[2]+=hallPrefactor*(dBYdx - dBXdy);
+   }
+      
+
+   
+     	
 
    
    
