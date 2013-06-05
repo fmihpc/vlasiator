6fb2786b746244b6a927b0311fa70a91925b183c
ohannuks <otto.hannuksela@fmi.fi>
2013-06-05 07:11:18 +0000

    Fixed name clashes with the vlsv_lib

--- muxml.h
+++ muxml.h
@@ -16,8 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef MUXML_H
-#define MUXML_H
+#ifndef MUXML_NOT_NEW_H
+#define MUXML_NOT_NEW_H
 
 #include <ostream>
 #include <map>
