c9a92eec0a4e5c1c1f67aa7d8332e58f840cf23a
ohannuks <otto.hannuksela@fmi.fi>
2013-06-05 07:12:20 +0000

    Added some custom-made functions and a class 'Zone'
    Whether the custom-made functions are of any as they are is still a
    small mystery

--- iowrite.cpp
+++ iowrite.cpp
@@ -5,20 +5,328 @@
 #include <vector>
 #include <sstream>
 #include <ctime>
+#include <array>
 #include "iowrite.h"
 #include "phiprof.hpp"
 #include "parameters.h"
 #include "logger.h"
 #include "vlsvwriter2.h"
 #include "vlsvreader2.h"
+#include "vlsv_writer.h"
 #include "vlasovmover.h"
 
 using namespace std;
 using namespace phiprof;
+using namespace vlsv;
 
 extern Logger logFile, diagnostic;
 
 typedef Parameters P;
+
+
+//O: NOT USED CURRENTLY!
+class Zone {
+   private:
+      //local id of the zone
+      int localId;
+      //MPI process rank owning the zone:
+      //O: NOTE: I actually don't know what this means
+      int myRank;
+      //Has the zone's indices
+      array<unsigned int, 3> indices;
+   public:
+      const int getLocalId() const {
+         return localId;
+      }
+      const int getLocalId() {
+         return localId;
+      }
+      void setLocalId( const int _localId ) {
+         localId = _localId;
+      }
+      const int getRank() {
+         return myRank;
+      }
+      const int getRank() const {
+         return myRank;
+      }
+      void setRank( const int rank ) {
+         myRank = rank;
+      }
+      const array<unsigned int, 3> & getIndices() {
+         return indices;
+      }
+      const array<unsigned int, 3> & getIndices() const {
+         return indices;
+      }
+      void setIndices( const array<unsigned int, 3> _indices ) {
+         indices = _indices;
+      }
+      Zone() {
+         //Put on default values
+         myRank = -1;
+         localId = -1;
+         indices[0] = 0;
+         indices[1] = 0;
+         indices[2] = 0;
+      }
+      ~Zone();
+};
+
+
+
+//O: Ok this function name is ridiculously long. I just lack the imagination for a proper name.
+//O: Not done
+
+bool o_writeGhostZoneDomainAndLocalIdNumbers( Writer & vlsvWriter,
+                                              const string & meshName,
+                                              const array<unsigned int, 3> & _cells,
+                                              const vector<Zone> & ghost_zones ) {
+   //We need to keep track of how many ghost domain ids and ghost local ids there are:
+   unsigned int numberOfGhosts = 0;
+
+   //Declare vectors for storing data
+   vector<unsigned int> ghostDomainIds;
+   vector<unsigned int> ghostLocalIds;
+
+   //Iterate through all ghost zones:
+   vector<Zone>::const_iterator it;
+   for( it = ghost_zones.begin(); it != ghost_zones.end(); ++it ) {
+      //Domain id is the MPI process rank owning the ghost zone
+      //O: Find out about the mpi process rank
+      const int domainId = (*it).getRank();
+      //get the local id
+      //O: How to define?
+      const int localId = (*it).getLocalId();
+      //Append to the vectors
+      ghostDomainIds.push_back( domainId );
+      ghostLocalIds.push_back( localId );
+
+      //Increment the number of ghosts as ghostDomainIds has been pushed back
+      //O: Couldn't this be simply fetched with ghostDomainIds.size() as well?
+      numberOfGhosts++;
+   }
+
+   //Write the array:
+   map<string, string> xmlAttributes;
+   //Note: should be "SpatialGrid"
+   xmlAttributes["mesh"] = meshName;
+   if( vlsvWriter.writeArray( "MESH_GHOST_DOMAINS", xmlAttributes, numberOfGhosts, 1, &(ghostDomainIds[0]) ) == false ) {
+      return false;
+   }
+   if( vlsvWriter.writeArray( "MESH_DOMAIN_SIZES", xmlAttributes, numberOfGhosts, 1, &(ghostLocalIds[0]) ) == false ) {
+      return false;
+   }
+   //Everything good
+   return true;
+}
+
+
+
+bool o_writeDomainSizes( Writer & vlsvWriter,
+                         const string & meshName,
+                         const unsigned int numberOfLocalZones,
+                         const unsigned int numberOfGhostZones ) {
+   //Declare domainSize. There are two types of domain sizes -- ghost and local
+   const int numberOfDomainTypes = 2;
+   int domainSize[numberOfDomainTypes];
+   domainSize[0] = numberOfLocalZones;
+   domainSize[1] = numberOfGhostZones;
+
+   //Write the array:
+   map<string, string> xmlAttributes;
+   //Put the meshName
+   xmlAttributes["mesh"] = meshName;
+   //Write (writeArray does the writing) Note: Here the important part is "MESH_DOMAIN_SIZES" -- visit plugin needs this
+   if( vlsvWriter.writeArray( "MESH_DOMAIN_SIZES", xmlAttributes, 1, 2, domainSize ) == false ) {
+      return false;
+   } else {
+      //write successful, return true
+      return true;
+   }
+}
+
+
+
+
+//Writes the global id numbers. Note: vector< array<unsigned int, 3> > & local_zones is a vector which has coordinates for local zones
+bool o_writeZoneGlobalIdNumbers( Writer & vlsvWriter,
+                                 const string & meshName,
+                                 const array<unsigned int, 3> & _cells,
+                                 const vector< array<unsigned int, 3> > & local_zones,
+                                 const vector< array<unsigned int, 3> > & ghost_zones ) {
+   //Get the cells in x, y, z direction right off the bat (for the sake of clarity):
+   const unsigned int xCells = _cells[0];
+   const unsigned int yCells = _cells[1];
+   const unsigned int zCells = _cells[2];
+
+   unsigned int localId = 0;
+   vector<unsigned int> globalIds;
+
+   //Iterate through local_zones and store the values into globalIDs
+   //Note: globalID is defined as follows: global ID = z*yCells*xCells + y*xCells + x
+   vector< array<unsigned int, 3> >::const_iterator it;
+   for( it = local_zones.begin(); it != local_zones.end(); ++it ) {
+      //Get the coordinates for the sake of clarity:
+      //Note: These are cell coordinates (integers)
+      const unsigned int x = (*it)[0];
+      const unsigned int y = (*it)[1];
+      const unsigned int z = (*it)[2];
+
+      //Calculate the global id:
+      const unsigned int globalId = z * yCells * xCells + y * xCells + x;
+
+      //Add the global id:
+      globalIds.push_back( globalId );
+
+      //Increment the number of local ids
+      ++localId;
+   }
+   //Do the same for ghost zones: (Append to the end of the list of global ids)
+   for( it = ghost_zones.begin(); it != ghost_zones.end(); ++it ) {
+      //Get the coordinates for the sake of clarity:
+      //Note: These are cell coordinates (integers)
+      const unsigned int x = (*it)[0];
+      const unsigned int y = (*it)[1];
+      const unsigned int z = (*it)[2];
+
+      //Calculate the global id:
+      const unsigned int globalId = z * yCells * xCells + y * xCells + x;
+
+      //Add the global id:
+      globalIds.push_back( globalId );
+
+      //Increment the number of local ids
+      ++localId;
+   }
+
+   //Get the total number of zones:
+   const unsigned int numberOfZones = globalIds.size();
+
+   //Write the array:
+   map<string, string> xmlAttributes;
+   //The name of the mesh (user input -- should be "SpatialGrid")
+   xmlAttributes["name"] = meshName;
+   //A mandatory 'type' -- just something visit hopefully understands, because I dont :)
+   xmlAttributes["type"] = "multi_ucd";
+   //Write:
+   //O: NOTE: FIND OUT WHY ITS globalIds[0]!!!!!!!!!!!!!!!!!!!!!!!!!!
+   if( vlsvWriter.writeArray( "MESH", xmlAttributes, numberOfZones, 1, &(globalIds[0]) ) == false ) {
+      cout << "Unsuccessful writing of MESH at: " << __FILE__ << " " << __LINE__ << endl;
+      return false;
+   } else {
+      //Successfully wrote the array
+      return true;
+   }
+}
+
+//Writes the cell coordinates
+//Note: meshName should probably be spatialGrid
+//_cells contains number of cells in x, y and z direction
+bool o_writeBoundingBoxNodeCoordinates ( Writer & vlsvWriter,
+                                         const string & meshName,
+                                         const array<unsigned int, 3> & _cells,
+                                         const int masterRank,
+                                         MPI_Comm comm ) {
+   //Create variables xCells, yCells, zCells which tell the number of zones in the given direction
+   //Note: This is for the sake of clarity.
+   const unsigned int xCells = _cells[0];
+   const unsigned int yCells = _cells[1];
+   const unsigned int zCells = _cells[2];
+
+   //Create node coordinates:
+   //These are the coordinates for any given node in x y or z direction
+   //Note: Why is it xCells + 1 and not xCells?
+   float * xNodeCoordinates = new float[xCells + 1];
+   float * yNodeCoordinates = new float[yCells + 1];
+   float * zNodeCoordinates = new float[zCells + 1];
+
+   //Input the coordinates for the nodes:
+   //O: NOTE! I am not actually sure whether these are supposed to be the cell coordinates(integers) or the 'real' coordinates(doubles)
+   for( unsigned int i = 0; i < xCells + 1; ++i ) {
+      //The x coordinate of the first node should be 0, the second 1, and so on
+      xNodeCoordinates[i] = i;
+   }
+   for( unsigned int i = 0; i < yCells + 1; ++i ) {
+      yNodeCoordinates[i] = i;
+   }
+   for( unsigned int i = 0; i < zCells + 1; ++i ) {
+      zNodeCoordinates[i] = i;
+   }
+
+   //Write the arrays:
+   map<string, string> xmlAttributes;
+   //Note: meshName should be "SpatialGrid", probably
+   xmlAttributes["mesh"] = meshName;
+   //"success"'s value will be returned. By default it's true but if some of the vlsvWriter operations fail it will be false:
+   bool success = true;
+   //Depending on whether our rank is master rank or not the operation is slightly different, so let's get out rank from the MPI_Comm comm:
+   int myRank;
+   //Input myRank:
+   MPI_Comm_rank(comm, &myRank);
+   //Check the rank:
+   if( myRank == masterRank ) {
+      //Save with the correct name "MESH_NODE_CRDS_X" -- writeArray returns false if something goes wrong
+      if( vlsvWriter.writeArray("MESH_NODE_CRDS_X", xmlAttributes, xCells + 1, 1, xNodeCoordinates) == false ) success = false;
+      if( vlsvWriter.writeArray("MESH_NODE_CRDS_Y", xmlAttributes, yCells + 1, 1, yNodeCoordinates) == false ) success = false;
+      if( vlsvWriter.writeArray("MESH_NODE_CRDS_Z", xmlAttributes, zCells + 1, 1, zNodeCoordinates) == false ) success = false;
+   } else {
+      //O: Wait what's happening here? 0 here and xCells in the other?
+      if( vlsvWriter.writeArray("MESH_NODE_CRDS_X", xmlAttributes, 0, 1, xNodeCoordinates) == false ) success = false;
+      if( vlsvWriter.writeArray("MESH_NODE_CRDS_Y", xmlAttributes, 0, 1, yNodeCoordinates) == false ) success = false;
+      if( vlsvWriter.writeArray("MESH_NODE_CRDS_Z", xmlAttributes, 0, 1, zNodeCoordinates) == false ) success = false;
+   }
+   return success;
+}
+
+//Writes the boundaries for the grid. box_values should be a vector of size 6 and should contain:
+//num of blocks in x, y, z direction, number of cells in x, y, z direction
+//O: meshName should probably be "SpatialGrid"
+bool o_writeMeshBoundingBox( Writer & vlsvWriter, 
+                             const string & meshName, 
+                             const array<long unsigned int, 6> & box_values,
+                             const int masterRank,
+                             MPI_Comm comm ) {
+   const unsigned int box_size = 6;
+
+   //Check for correct input
+   if( box_values.size() != box_size ) {
+      cerr << "Box_values must be of size 6 at: " << __FILE__ << " " << __LINE__ << endl;
+      exit(1);
+   }
+
+   //Get my rank from the MPI_Comm
+   int myRank;
+   MPI_Comm_rank(comm, &myRank);
+
+   //Declare boundaryBox (writeArray expects it like this but it will have the same values as box_values)
+   long unsigned int boundaryBox[box_size];
+   //Save the box_values into boundaryBox:
+   array<long unsigned int, 6>::const_iterator it = box_values.begin();
+   for( int i = 0; i < 6; ++it, ++i ) {
+      //Save the value to bbox:
+      boundaryBox[i] = *it;
+   }
+
+   //Write the array:
+   //Declare attributes
+   map<string, string> xmlAttributes;
+   //We received mesh name as a parameter: MOST LIKELY THIS IS SpatialGrid!
+   xmlAttributes["mesh"] = meshName;
+
+   //Write an array (NOTE: success will be returned and writeArray will return true or false depending on whether or not the write is successful)
+   bool success;
+   if( myRank == masterRank ) {
+      //The visit plugin expects MESH_BBOX as a keyword
+      //NOTE: writeArray writes boundaryBox
+      success = vlsvWriter.writeArray("MESH_BBOX", xmlAttributes, 6, 1, boundaryBox);
+   } else {
+      success = vlsvWriter.writeArray("MESH_BBOX", xmlAttributes, 0, 1, boundaryBox);
+   }
+   return success;
+}
+
+
 
 
 bool globalSuccess(bool success,string errorMessage,MPI_Comm comm){
