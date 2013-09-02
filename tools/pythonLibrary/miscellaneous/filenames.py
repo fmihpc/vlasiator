9514a188498ab8fd6b5b863c1cb8ce86bdf7042c
ohannuks <otto.hannuksela@fmi.fi>
2013-09-02 11:59:11 +0000

    Added a separate branch for python tools

--- tools/pythonLibrary/miscellaneous/filenames.py
+++
@@ -1,8 +0,0 @@
-import glob
-import os
-
-def get_sorted_file_names(name="*.vlsv"):
-   fileNames=glob.glob(name)
-   fileNames.sort()
-   return fileNames
-   
--- tools/pythonLibrary/pyVisit/moviecompilescript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-filePath=$1
-fileName=$2
-frameRate=$3
-cd ${filePath}
-avconv -r ${frameRate} -i ${fileName}%04d.png -r ${frameRate} -b 40M ${fileName}.mp4
-cd -
