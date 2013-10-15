a786817ec13a209bde03db59981d8e0110196edd
ykempf <yann.kempf@fmi.fi>
2013-10-15 16:50:27 +0000

    Debig 2. Compiles in all settings of spatiel and time accuracy of field solver.

--- spatial_cell.hpp
+++ spatial_cell.hpp
@@ -1050,7 +1050,7 @@ namespace velocity_neighbor {
                
                // send Hell term components
                if((SpatialCell::mpi_transfer_type & Transfer::CELL_HALL_TERM)!=0){
-                  displacements.push_back((uint8_t*) &(this->parameters[CellParams::JXB_000_100]) - (uint8_t*) this);
+                  displacements.push_back((uint8_t*) &(this->parameters[CellParams::EXHALL_000_100]) - (uint8_t*) this);
                   block_lengths.push_back(sizeof(Real) * 12);
                }
                
