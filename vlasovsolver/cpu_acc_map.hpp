29694dda3f5286af9c6e135651295cc9390b9b6e
galfthan <sebastian.von.alfthan@fmi.fi>
2013-12-06 22:26:48 +0000

    Combined copy function to one  --This line, and those below, will be ignored--
    M    slice-3d-acceleration-QUESPACE-340/MAKE/Makefile.voima
    M    slice-3d-acceleration-QUESPACE-340/vlsvwriter2.h
    M    slice-3d-acceleration-QUESPACE-340/vlasovsolver/cpu_acc_map.hpp

--- vlasovsolver/cpu_acc_map.hpp
+++ vlasovsolver/cpu_acc_map.hpp
@@ -11,7 +11,6 @@ Copyright 2012 Finnish Meteorological Institute
 #include "algorithm"
 #include "cmath"
 #include "utility"
-
 #include "common.h"
 #include "spatial_cell.hpp"
 
@@ -44,126 +43,109 @@ template<typename T> inline T slope_limiter(const T& l,const T& m, const T& r) {
 }
 
 // indices in padded z block
-#define i_pblock(i,j,k) ( ((k) + 2) * WID2 + (j) * WID + (i) )
+#define i_pblock(i,j,k) ( ((k) + STENCIL_WIDTH ) * WID2 + (j) * WID + (i) )
 // indices in normal block
 #define i_block(i,j,k) ( (k) * WID2 + (j) * WID + (i) )
 
 /*
-  copy data for x mappings, no rotation here
-*/
-
-inline void copy_block_data_z(Velocity_Block *block,Real *values){
-    Velocity_Block *nbrBlock;  
-    // Construct values
-    // Copy averages from -z neighbour if it exists (if not, array is initialized to zero)
-    nbrBlock = block->neighbors[velocity_neighbor::XCC_YCC_ZM1];
-    if ( nbrBlock != NULL) {
-      for (int k=-STENCIL_WIDTH; k<0; ++k) 
-	for (uint j=0; j<WID; ++j) 
-	  for (uint i=0; i<WID; ++i) {
-	    values[i_pblock(i,j,k)] = nbrBlock->fx[i_block(i,j,k+WID)];
-	  }
-    }   
-    // Copy volume averages of this block:
-    for (uint k=0; k<WID; ++k) 
-      for (uint j=0; j<WID; ++j) 
-	for (uint i=0; i<WID; ++i) {
-	  values[i_pblock(i,j,k)] = block->fx[i_block(i,j,k)];
-	}    
-    // Copy averages from +z neighbour if it exists (if not, array is initialized to zero)
-    nbrBlock = block->neighbors[velocity_neighbor::XCC_YCC_ZP1];
-    if ( nbrBlock != NULL) {
-      for (uint k=WID; k<WID+STENCIL_WIDTH; ++k) 
-	for (uint j=0; j<WID; ++j) 
-	  for (uint i=0; i<WID; ++i) {
-	    values[i_pblock(i,j,k)] = nbrBlock->fx[i_block(i,j,k-WID)];
-	  }
-    }
-}
-
-/*
-  copy data for x mappings, note how we have rotated data
+  value array should be initialized to zero
+  
+  For dimension=0 data copy  we have rotated data
   i -> k
   j -> j
   k -> i
-
-This is so that the mapping computation can remain the same pretty much, and data is in a nice order for vectorization
-*/  
-inline void copy_block_data_x(Velocity_Block *block,Real *values){
-    Velocity_Block *nbrBlock;  
-    // Construct values
-    // Copy averages from -z neighbour if it exists (if not, array is initialized to zero)
-    nbrBlock = block->neighbors[velocity_neighbor::XM1_YCC_ZCC];
-    if ( nbrBlock != NULL) {
-      for (uint j=0; j<WID; ++j) 
-	for (int k=-STENCIL_WIDTH; k<0; ++k) 
-	  for (uint i=0; i<WID; ++i) {
-	    values[i_pblock(i,j,k)] = nbrBlock->fx[i_block(k+WID,j,i)];
-	  }
-    }   
-    // Copy volume averages of this block:
-    for (uint j=0; j<WID; ++j) 
-      for (uint k=0; k<WID; ++k) 
-	for (uint i=0; i<WID; ++i) {
-	  values[i_pblock(i,j,k)] = block->fx[i_block(k,j,i)];
-      }    
-    // Copy averages from +z neighbour if it exists (if not, array is initialized to zero)
-    nbrBlock = block->neighbors[velocity_neighbor::XP1_YCC_ZCC];
-    if ( nbrBlock != NULL) {
-      for (uint j=0; j<WID; ++j) 
-	for (uint k=WID; k<WID+STENCIL_WIDTH; ++k) 
-	  for (uint i=0; i<WID; ++i) {
-	    values[i_pblock(i,j,k)] = nbrBlock->fx[i_block(k-WID,j,i)];
-	  }
-    }
-}
-
-
-
-/*
-  copy data for y mappings, note how we have rotated data
+For dimension=1 data copy  we have rotated data
   i -> i
   j -> k
   k -> j
-
-This is so that the mapping computation can remain the same pretty much, and data is in a nice order for vectorization
-*/  
-inline void copy_block_data_y(Velocity_Block *block,Real *values){
-    Velocity_Block *nbrBlock;  
-    // Construct values
-    // Copy averages from -z neighbour if it exists (if not, array is initialized to zero)
-    nbrBlock = block->neighbors[velocity_neighbor::XCC_YM1_ZCC];
-    if ( nbrBlock != NULL) {
-      for (uint j=0; j<WID; ++j) 
-	for (int k=-STENCIL_WIDTH; k<0; ++k) 
-	  for (uint i=0; i<WID; ++i) {
-	    values[i_pblock(i,j,k)] = nbrBlock->fx[i_block(i,k+WID,j)];
-	  }
-    }   
-    // Copy volume averages of this block:
-    for (uint j=0; j<WID; ++j) 
-      for (uint k=0; k<WID; ++k) 
-	for (uint i=0; i<WID; ++i) {
-	  values[i_pblock(i,j,k)] = block->fx[i_block(i,k,j)];
-      }    
-    // Copy averages from +z neighbour if it exists (if not, array is initialized to zero)
-    nbrBlock = block->neighbors[velocity_neighbor::XCC_YP1_ZCC];
-    if ( nbrBlock != NULL) {
-      for (uint j=0; j<WID; ++j) 
-	for (uint k=WID; k<WID+STENCIL_WIDTH; ++k) 
-	  for (uint i=0; i<WID; ++i) {
-	    values[i_pblock(i,j,k)] = nbrBlock->fx[i_block(i,k-WID,j)];
-	  }
+For dimension=0 data copy 
+*/
+inline void copy_block_data(Velocity_Block *block,Real * __restrict__ values, int dimension){
+    Velocity_Block *nbrBlock;
+    uint cell_indices_to_id[3];
+    uint block_P1,block_M1;
+    switch (dimension){
+        case 0:
+           /* i and k coordinates have been swapped*/
+           cell_indices_to_id[0]=WID2;
+           cell_indices_to_id[1]=WID;
+           cell_indices_to_id[2]=1;
+           block_P1=velocity_neighbor::XP1_YCC_ZCC;
+           block_M1=velocity_neighbor::XM1_YCC_ZCC;
+           break;
+        case 1:
+           /* i and k coordinates have been swapped*/
+           cell_indices_to_id[0]=1;
+           cell_indices_to_id[1]=WID2;
+           cell_indices_to_id[2]=WID;
+           block_P1=velocity_neighbor::XCC_YP1_ZCC;
+           block_M1=velocity_neighbor::XCC_YM1_ZCC;
+           break;
+        case 2:
+           cell_indices_to_id[0]=1;
+           cell_indices_to_id[1]=WID;
+           cell_indices_to_id[2]=WID2;
+           block_P1=velocity_neighbor::XCC_YCC_ZP1;
+           block_M1=velocity_neighbor::XCC_YCC_ZM1;
+           break;
     }
+    
+    // Construct values
+    // Copy averages from -1 neighbour if it exists (if not, array is initialized to zero)
+    nbrBlock = block->neighbors[block_M1];
+    if ( nbrBlock != NULL) {
+       Real * __restrict__ ngbr_fx = nbrBlock->fx;
+       for (int k=-STENCIL_WIDTH; k<0; ++k) {
+          for (uint j=0; j<WID; ++j) {
+             for (uint i=0; i<WID; ++i) {
+                const uint cell =
+                   i * cell_indices_to_id[0] +
+                   j * cell_indices_to_id[1] +
+                   (k + WID) * cell_indices_to_id[2];
+                values[i_pblock(i,j,k)] = ngbr_fx[cell];
+             }
+          }
+       }
+    }
+
+    Real * __restrict__ fx = block->fx;
+    // Copy volume averages of this block:
+    for (uint k=0; k<WID; ++k) {
+       for (uint j=0; j<WID; ++j) {
+          for (uint i=0; i<WID; ++i) {
+             const uint cell =
+                i * cell_indices_to_id[0] +
+                j * cell_indices_to_id[1] +
+                k * cell_indices_to_id[2];
+             values[i_pblock(i,j,k)] = fx[cell];
+          }
+       }
+    }
+    
+    // Copy averages from +1 neighbour if it exists (if not, array is initialized to zero)
+    nbrBlock = block->neighbors[block_P1];
+    if ( nbrBlock != NULL) {
+       Real * __restrict__ ngbr_fx = nbrBlock->fx;
+       for (uint k=WID; k<WID+STENCIL_WIDTH; ++k) {             
+          for (uint j=0; j<WID; ++j) {
+             for (uint i=0; i<WID; ++i) {
+                const uint cell =
+                   i * cell_indices_to_id[0] +
+                   j * cell_indices_to_id[1] +
+                   (k-WID) * cell_indices_to_id[2];
+                values[i_pblock(i,j,k)] = ngbr_fx[cell];
+             }
+          }
+       }
+    }
+    
 }
 
-
-// TODO, looking at data the target block computation might be off?
 
 bool map_1d(SpatialCell* spatial_cell,   Real intersection, Real intersection_di, Real intersection_dj,Real intersection_dk,
 	   uint dimension ) {
   /*values used with an stencil in 1 dimension, initialized to 0 */  
-  Real values[WID*WID*(WID+2*STENCIL_WIDTH)]={};
+   Real values[WID*WID*(WID+2*STENCIL_WIDTH)]={};
   // Make a copy of the blocklist, the blocklist will change during this algorithm
   uint*  blocks=new uint[spatial_cell->number_of_blocks];
   const uint nblocks=spatial_cell->number_of_blocks;
@@ -249,25 +231,23 @@ bool map_1d(SpatialCell* spatial_cell,   Real intersection, Real intersection_di
     velocity_block_indices_t block_indices=SpatialCell::get_velocity_block_indices(blocks[block_i]);
     uint temp;
     switch (dimension){
-    case 0:
-      /* i and k coordinates have been swapped*/
-      copy_block_data_x(block,values); 
-      temp=block_indices[2];
-      block_indices[2]=block_indices[0];
-      block_indices[0]=temp;
-      break;
-    case 1:
-      /*in values j and k coordinates have been swapped*/
-      copy_block_data_y(block,values); 
-      temp=block_indices[2];
-      block_indices[2]=block_indices[1];
-      block_indices[1]=temp;
-      break;
-    case 2:
-      copy_block_data_z(block,values);
-      break;
+        case 0:
+           /*i and k coordinates have been swapped*/
+           temp=block_indices[2];
+           block_indices[2]=block_indices[0];
+           block_indices[0]=temp;
+           break;
+        case 1:
+           /*in values j and k coordinates have been swapped*/
+           temp=block_indices[2];
+           block_indices[2]=block_indices[1];
+           block_indices[1]=temp;
+           break;
+        case 2:
+           break;
     }
-    
+    copy_block_data(block,values,dimension); 
+
     /*i,j,k are now relative to the order in which we copied data to the values array. 
       After this point in the k,j,i loops there should be no branches based on dimensions
      */
@@ -297,7 +277,7 @@ bool map_1d(SpatialCell* spatial_cell,   Real intersection, Real intersection_di
 	  //can have 1-2, or 2-3 iterations, we could split the whole
 	  //thing..
 	  for(uint gk=lagrangian_gk_l;gk<=lagrangian_gk_r;gk++){
-	    //the blocks of the two lagrangian cells
+	    //the blocks of the lagrangian cell to which we map
 	    const uint target_block = 
 	      block_indices[0]*block_indices_to_id[0]+
 	      block_indices[1]*block_indices_to_id[1]+
