f453e3ddb4d0d59d0b3a01312df0079036b977f5
ohannuks <otto.hannuksela@fmi.fi>
2014-03-12 14:57:08 +0200

    added == operator

--- datareduction/reducepopulation.cpp
+++ datareduction/reducepopulation.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace spatial_cell;
 
-// Note: This is done to save memory
+// Note: This is done to save memory (hopefully)
 class Velocity_Cell {
    private:
       const SpatialCell * cell;
@@ -34,16 +34,28 @@ class Velocity_Cell {
       bool operator<( const Velocity_Cell & other ) const {
          return cell->block_data[index] < cell->block_data[other.index];
       }
+
+      // Compare equality
+      bool operator==(  const Velocity_Cell & other ) const {
+         return index == other.index;
+      }
+
       // Function for getting the avgs value
       inline Realf get_avgs() const {
          return cell->block_data[index];
       }
 };
 
-////Fast implementation for clustering
-//Real evaluate_speed_one( const SpatialCell * cell ) {
-//   
-//}
+// Returns neighbors of the velociy cell
+Velocity_Cell * neighbors( const Velocity_Cell & vCell ) {
+   // Get the velocity cell block id:
+   const uint32_t blockId = vCell.blockId();
+   // Get the velocity cell id:
+   const uint16_t vCellId = vCell.vCellId();
+   
+}
+
+
 
 //Fast implementation
 Real evaluate_speed( const SpatialCell * cell ) {
@@ -96,33 +108,22 @@ Real evaluate_speed_parallel( const SpatialCell * cell ) {
    __gnu_parallel::sort(velocityCells.begin(), velocityCells.end());
    // Return value:
    Real value_to_return = 0;
-   for( unsigned int i = 0; i < block_data->size(); ++i ) {
-      if( i%2 == 0 ) {
-         value_to_return += (Real)(velocityCells[i].get_avgs());
-      } else {
-         value_to_return -= (Real)(velocityCells[i].get_avgs());
+   #pragma omp parallel
+   {
+      Real tmp_value = 0;
+      #pragma omp for
+      for( unsigned int i = 0; i < block_data->size(); ++i ) {
+         if( i%2 == 0 ) {
+            tmp_value += (Real)(velocityCells[i].get_avgs());
+         } else {
+            tmp_value -= (Real)(velocityCells[i].get_avgs());
+         }
       }
+      #pragma omp atomic
+      value_to_return += tmp_value;
    }
    return value_to_return;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
