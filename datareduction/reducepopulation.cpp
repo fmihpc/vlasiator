c00f32f4b347328d0e5c31b2a723e50f513e8e94
ohannuks <otto.hannuksela@fmi.fi>
2014-03-13 16:19:05 +0200

    Added neighbor cell function

--- datareduction/reducepopulation.cpp
+++ datareduction/reducepopulation.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include "reducepopulation.h"
 
+
+
 using namespace std;
 using namespace spatial_cell;
 
@@ -13,47 +15,80 @@ class Velocity_Cell {
    private:
       const SpatialCell * cell;
    public:
-      uint64_t index; //Index could be uint32_t is enough
+      //uint64_t index; //Index could be uint32_t is enough
+      uint32_t blockId;
+      uint16_t vCellId;
 
-      inline void set_data( const SpatialCell * input_cell, const uint32_t input_index ) {
+      inline void set_data( const SpatialCell * input_cell, const uint32_t index ) {
          cell = input_cell;
-         index = input_index;
+         blockId = cell->velocity_block_list[index/64];
+         vCellId = index%64;
       }
 
-      // Returns block id
-      inline uint32_t blockId() const {
-         return cell->velocity_block_list[index/64];
-      }
-
-      //Returns velocity cell id
-      inline uint16_t vCellId() const {
-         return index%64;
+      inline void set_data( const SpatialCell * input_cell, const uint32_t input_blockId, const uint16_t input_vCellId ) {
+         cell = input_cell;
+         blockId = input_blockId;
+         vCellId = input_vCellId;
       }
 
       // Compare values
       bool operator<( const Velocity_Cell & other ) const {
-         return cell->block_data[index] < cell->block_data[other.index];
+         return cell->at(blockId)->data[vCellId] < cell->at(blockId)->data[other.vCellId];
       }
 
       // Compare equality
       bool operator==(  const Velocity_Cell & other ) const {
-         return index == other.index;
+         return (blockId == other.blockId) && (vCellId == other.vCellId);
       }
 
       // Function for getting the avgs value
       inline Realf get_avgs() const {
-         return cell->block_data[index];
+         return cell->at(blockId)->data[vCellId];
       }
 };
 
+
 // Returns neighbors of the velociy cell
-Velocity_Cell * neighbors( const Velocity_Cell & vCell ) {
+Velocity_Cell * neighbors( const SpatialCell * cell, const Velocity_Cell & vCell ) {
    // Get the velocity cell block id:
-   const uint32_t blockId = vCell.blockId();
+   const uint32_t blockId = vCell.blockId;
    // Get the velocity cell id:
-   const uint16_t vCellId = vCell.vCellId();
+   const uint16_t vCellId = vCell.vCellId;
    // Transform to coordinates:
-   
+   const uint32_t indices[3] = { 
+                        WID * (blockId % SpatialCell::vx_length) + vCellId % block_vx_length,
+                        WID * ((blockId / SpatialCell::vx_length) % SpatialCell::vy_length) + (vCellId / block_vx_length) % block_vy_length,
+                        WID * (blockId / (SpatialCell::vx_length * SpatialCell::vy_length)) + vCellId / (block_vx_length * block_vy_length)
+                               };
+   // Reserve space for the neighbors:
+   Velocity_Cell * neighbor_vCells = new Velocity_Cell[block_vx_length*block_vz_length*block_vy_length - 1];
+   // Get the neighbors:
+   // Go through neighbor indices e.g. i, j, k (-1,0,1)
+   unsigned int iterator = 0;
+   for( unsigned int i = -1; i <= 1; ++i ) for( unsigned int j = -1; j <= 1; ++j ) for( unsigned int k = -1; k <=1; ++k ) {
+      // Do not include self as neighbor
+      if( i == 0 && j == 0 && k == 0 ) { continue; }
+      // Get the neighbor's indices
+      const uint32_t neighbor_indices[3] = {
+                                           indices[0] + i,
+                                           indices[0] + j,
+                                           indices[0] + k,
+                                           };
+      // Transform into blockid and velocity cell id:
+      const uint32_t neighbor_blockId = 
+                             neighbor_indices[0] / WID
+                             + (neighbor_indices[1] / WID) * SpatialCell::vx_length
+                             + (neighbor_indices[2] / WID) * SpatialCell::vx_length * SpatialCell::vy_length;
+      const uint16_t neighbor_vCellId = 
+                             (neighbor_indices[0] % WID)
+                           + (neighbor_indices[1] % WID) * block_vx_length
+                           + (neighbor_indices[2] % WID) * block_vx_length * block_vy_length;
+      // Get index of the neighbor cell:
+      
+      neighbor_vCells[iterator].set_data( cell, neighbor_blockId, neighbor_vCellId );
+      iterator++;
+   }
+   return neighbor_vCells;
 }
 
 
