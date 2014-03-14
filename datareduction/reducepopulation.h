af44063c3470e72e620e6d39a00483ab367f8551
ohannuks <otto.hannuksela@fmi.fi>
2014-03-14 13:24:22 +0200

    Changed function inputs and added a new function

--- datareduction/reducepopulation.h
+++ datareduction/reducepopulation.h
@@ -1,6 +1,20 @@
-
+#include <utility>
+#include <vector>
+#include <array>
 #include "../spatial_cell.hpp"
 
 
-Real evaluate_speed( const spatial_cell::SpatialCell * cell );
-Real evaluate_speed_parallel( const spatial_cell::SpatialCell * cell );
+void set_local_and_remote_velocity_cell_neighbors(
+       array<vector<uint16_t>, VELOCITY_BLOCK_LENGTH> & local_vcell_neighbors,
+       array< vector< pair<uint16_t, vector<uint16_t> > > > , VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
+                                                 );
+Real evaluate_speed(
+                const SpatialCell * cell,
+                const array<vector<uint16_t>, VELOCITY_BLOCK_LENGTH> & local_vcell_neighbors,
+                const array< vector< pair<uint16_t, vector<uint16_t> > > > , VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
+                   );
+Real evaluate_speed_parallel(
+                const SpatialCell * cell,
+                const array<vector<uint16_t>, VELOCITY_BLOCK_LENGTH> & local_vcell_neighbors,
+                const array< vector< pair<uint16_t, vector<uint16_t> > > > , VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
+                            );
