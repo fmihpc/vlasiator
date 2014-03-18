01f82960341dff3ac5291ce0b94f8dd2a16e31c3
ohannuks <otto.hannuksela@fmi.fi>
2014-03-18 09:49:02 +0200

    Fixed namespace

--- datareduction/reducepopulation.h
+++ datareduction/reducepopulation.h
@@ -5,16 +5,16 @@
 
 
 void set_local_and_remote_velocity_cell_neighbors(
-       array<vector<uint16_t>, VELOCITY_BLOCK_LENGTH> & local_vcell_neighbors,
-       array< vector< pair<uint16_t, vector<uint16_t> > > > , VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
+       std::array<std::vector<uint16_t>, VELOCITY_BLOCK_LENGTH> & local_vcell_neighbors,
+       std::array< std::vector< std::pair<uint16_t, std::vector<uint16_t> > >, VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
                                                  );
 Real evaluate_speed(
                 const SpatialCell * cell,
-                const array<vector<uint16_t>, VELOCITY_BLOCK_LENGTH> & local_vcell_neighbors,
-                const array< vector< pair<uint16_t, vector<uint16_t> > > > , VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
+                const std::array<std::vector<uint16_t>, VELOCITY_BLOCK_LENGTH> & local_vcell_neighbors,
+                const std::array< std::vector< std::pair<uint16_t, std::vector<uint16_t> > > , VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
                    );
 Real evaluate_speed_parallel(
                 const SpatialCell * cell,
-                const array<vector<uint16_t>, VELOCITY_BLOCK_LENGTH> & local_vcell_neighbors,
-                const array< vector< pair<uint16_t, vector<uint16_t> > > > , VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
+                const std::array<std::vector<uint16_t>, VELOCITY_BLOCK_LENGTH> & local_vcell_neighbors,
+                const std::array< std::vector< std::pair<uint16_t, std::vector<uint16_t> > > , VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
                             );
