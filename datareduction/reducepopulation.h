4dc0857831415fb131f0331bfb59bc9a3cc23b2a
ohannuks <otto.hannuksela@fmi.fi>
2014-03-25 09:46:37 +0200

    Fixed syntax errors

--- datareduction/reducepopulation.h
+++ datareduction/reducepopulation.h
@@ -9,12 +9,12 @@ void set_local_and_remote_velocity_cell_neighbors(
        std::array< std::vector< std::pair<uint16_t, std::vector<uint16_t> > >, VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
                                                  );
 Real evaluate_speed(
-                const SpatialCell * cell,
+                spatial_cell::SpatialCell * cell,
                 const std::array<std::vector<uint16_t>, VELOCITY_BLOCK_LENGTH> & local_vcell_neighbors,
                 const std::array< std::vector< std::pair<uint16_t, std::vector<uint16_t> > > , VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
                    );
 Real evaluate_speed_parallel(
-                const SpatialCell * cell,
+                spatial_cell::SpatialCell * cell,
                 const std::array<std::vector<uint16_t>, VELOCITY_BLOCK_LENGTH> & local_vcell_neighbors,
                 const std::array< std::vector< std::pair<uint16_t, std::vector<uint16_t> > > , VELOCITY_BLOCK_LENGTH> & remote_vcell_neighbors
                             );
