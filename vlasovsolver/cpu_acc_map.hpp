5219722871bdefabc17e95abe26f9d3555573661
Otto Hannuksela <hannukse@voima-login1.(none)>
2014-06-06 14:36:59 +0300

    Added sorting into map_1d

--- vlasovsolver/cpu_acc_map.hpp
+++ vlasovsolver/cpu_acc_map.hpp
@@ -164,6 +164,69 @@ inline void copy_block_data(SpatialCell* spatial_cell, uint blockID,Real * __res
     }
 }
 
+// Comparator function for sorting vector of pairs
+inline bool paircomparator( const pair<uint, uint> & l, const pair<uint, uint> & r ) {
+   return l.first < r.first;
+}
+
+/*
+   This function copies the block list from spatial cell to blocks and sorts it
+
+   Note: blocks must be allocated
+*/
+static void sort_blocklist_by_dimension( const SpatialCell* spatial_cell, 
+                                         const uint dimension,
+                                         uint* blocks ) {
+  const uint nBlocks = spatial_cell->number_of_blocks; // Number of blocks
+  // Copy block data to vector
+  vector<pair<uint, uint> > block_pairs;
+  block_pairs.resize( nBlocks );
+  for( uint i = 0; i < nBlocks; ++i ) {
+    const uint block = spatial_cell->velocity_block_list[i];
+    switch( dimension ) {
+    case 0:
+      {
+      const uint blockId_mapped = block; // Mapping the block id to different coordinate system if dimension is not zero:
+      block_pairs[i] = make_pair( blockId_mapped, block );
+      break;
+      }
+    case 1:
+      {
+      // Do operation: 
+      //   block = x + y*x_max + z*y_max*x_max 
+      //=> block' = block - (x + y*x_max) + y + x*y_max = x + y*x_max + z*y_max*x_max - (x + y*x_max) + y + x*y_max
+      //          = y + x*y_max + z*y_max*x_max
+      const uint x_indice = block%SpatialCell::vx_length;
+      const uint y_indice = (block/SpatialCell::vx_length)%SpatialCell::vy_length;
+      // Mapping the block id to different coordinate system if dimension is not zero:
+      const uint blockId_mapped = block - (x_indice + y_indice*SpatialCell::vx_length) + y_indice + x_indice * SpatialCell::vy_length;
+      block_pairs[i] = make_pair( blockId_mapped, block );
+      break;
+      }
+    case 2:
+      {
+      // Do operation: 
+      //   block = x + y*x_max + z*y_max*x_max 
+      //=> block' = z + y*z_max + x*z_max*y_max
+      const uint x_indice = block%SpatialCell::vx_length;
+      const uint y_indice = (block/SpatialCell::vx_length)%SpatialCell::vy_length;
+      const uint z_indice =  (block/(SpatialCell::vx_length*SpatialCell::vy_length));
+      // Mapping the block id to different coordinate system if dimension is not zero:
+      const uint blockId_mapped = z_indice + y_indice * SpatialCell::vz_length + x_indice*SpatialCell::vy_length*SpatialCell::vz_length;
+      block_pairs[i] = make_pair( blockId_mapped, block );
+      break;
+      }
+    }
+  }
+  // Sort the list:
+  sort( block_pairs.begin(), block_pairs.end(), paircomparator );
+  // Put in the sorted blocks:
+  for( uint i = 0; i < nBlocks; ++i ) {
+     blocks[i] = block_pairs[i].second;
+  }
+  return;
+}
+
 /* 
    Here we map from the current time step grid, to a target grid which
    is the lagrangian departure grid (so th grid at timestep +dt,
@@ -200,7 +263,8 @@ bool map_1d(SpatialCell* spatial_cell,
      const unsigned int block = spatial_cell->velocity_block_list[block_i];
      blocks[block_i] = block; 
   }
-
+  // Copy blocklist and sort it according to the dimension (0=x-dimension, 1=y-dimension, 2=z-dimension)
+  //sort_blocklist_by_dimension( spatial_cell, dimension, blocks );
 
   
   if(dimension>2)
