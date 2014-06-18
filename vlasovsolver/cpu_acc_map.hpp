0c012d62a3b5d3ac2805b3f9a88857ebb7a277d1
Sebastian von Alfthan <galfthan@gmail.com>
2014-06-18 11:43:32 -0400

    Also some changes here

--- vlasovsolver/cpu_acc_map.hpp
+++ vlasovsolver/cpu_acc_map.hpp
@@ -15,6 +15,8 @@ Copyright 2013, 2014 Finnish Meteorological Institute
 #include "common.h"
 #include "spatial_cell.hpp"
 #include "cpu_1d_interpolations.hpp"
+#include "cpu_sort_blocks_for_acc.hpp"
+
 #ifdef ACC_SEMILAG_PCONSTM
 const int STENCIL_WIDTH=0;
 #endif
@@ -162,69 +164,6 @@ inline void copy_block_data(SpatialCell* spatial_cell, uint blockID,Real * __res
 	}
       }
     }
-}
-
-// Comparator function for sorting vector of pairs
-inline bool paircomparator( const pair<uint, uint> & l, const pair<uint, uint> & r ) {
-   return l.first < r.first;
-}
-
-/*
-   This function copies the block list from spatial cell to blocks and sorts it
-
-   Note: blocks must be allocated
-*/
-static void sort_blocklist_by_dimension( const SpatialCell* spatial_cell, 
-                                         const uint dimension,
-                                         uint* blocks ) {
-  const uint nBlocks = spatial_cell->number_of_blocks; // Number of blocks
-  // Copy block data to vector
-  vector<pair<uint, uint> > block_pairs;
-  block_pairs.resize( nBlocks );
-  for( uint i = 0; i < nBlocks; ++i ) {
-    const uint block = spatial_cell->velocity_block_list[i];
-    switch( dimension ) {
-    case 0:
-      {
-      const uint blockId_mapped = block; // Mapping the block id to different coordinate system if dimension is not zero:
-      block_pairs[i] = make_pair( blockId_mapped, block );
-      break;
-      }
-    case 1:
-      {
-      // Do operation: 
-      //   block = x + y*x_max + z*y_max*x_max 
-      //=> block' = block - (x + y*x_max) + y + x*y_max = x + y*x_max + z*y_max*x_max - (x + y*x_max) + y + x*y_max
-      //          = y + x*y_max + z*y_max*x_max
-      const uint x_indice = block%SpatialCell::vx_length;
-      const uint y_indice = (block/SpatialCell::vx_length)%SpatialCell::vy_length;
-      // Mapping the block id to different coordinate system if dimension is not zero:
-      const uint blockId_mapped = block - (x_indice + y_indice*SpatialCell::vx_length) + y_indice + x_indice * SpatialCell::vy_length;
-      block_pairs[i] = make_pair( blockId_mapped, block );
-      break;
-      }
-    case 2:
-      {
-      // Do operation: 
-      //   block = x + y*x_max + z*y_max*x_max 
-      //=> block' = z + y*z_max + x*z_max*y_max
-      const uint x_indice = block%SpatialCell::vx_length;
-      const uint y_indice = (block/SpatialCell::vx_length)%SpatialCell::vy_length;
-      const uint z_indice =  (block/(SpatialCell::vx_length*SpatialCell::vy_length));
-      // Mapping the block id to different coordinate system if dimension is not zero:
-      const uint blockId_mapped = z_indice + y_indice * SpatialCell::vz_length + x_indice*SpatialCell::vy_length*SpatialCell::vz_length;
-      block_pairs[i] = make_pair( blockId_mapped, block );
-      break;
-      }
-    }
-  }
-  // Sort the list:
-  sort( block_pairs.begin(), block_pairs.end(), paircomparator );
-  // Put in the sorted blocks:
-  for( uint i = 0; i < nBlocks; ++i ) {
-     blocks[i] = block_pairs[i].second;
-  }
-  return;
 }
 
 /* 
