25299f283a1c9c6f330c8cf2a59d3d8c26f37240
Sebastian von Alfthan <sebastian.von.alfthan@fmi.fi>
2014-06-25 17:16:10 +0300

    Now face values are stored separately left/right. Easier to read in my opinion. First version of ih6. There is an error in white's paper, I am not sure these are the correct coefficients either

--- vlasovsolver/cpu_1d_column_interpolations.hpp
+++ vlasovsolver/cpu_1d_column_interpolations.hpp
@@ -14,47 +14,101 @@ Copyright 2013, 2014 Finnish Meteorological Institute
 
 using namespace std;
 
-/*Compute all face values. For cell k (globla index), its left face
- * value is in fv[2*k] and right value in fv[2*k+1]. Based on explicit
- * h6 estimate*/
-inline void compute_h6_face_values(Vec4 *values, uint n_cblocks,  Vec4 *fv){   
 
-   /*we loop up to one extra cell. There is extra space in fv for the extra left value*/
-   for (int k = 0; k < n_cblocks * WID + 1; k++){
-      /*compute left values*/
-      fv[2 * k] = 1.0/60.0 * (values[k - 3 + WID]  - 8.0 * values[k - 2 + WID]  + 37.0 * values[k - 1 + WID] +
-                              37.0 * values[k  + WID] - 8.0 * values[k + 1 + WID] + values[k + 2 + WID]);
-      /*if not first cell, set right value of previous cell to same as left for current one*/
-      if(k > 0)
-         fv[2 * k -1] = fv[2 * k];
-   }
+void tridiag_ih6(uint n, Vec4 *u, Vec4 *r){
+  const Vec4 a(4.0/12.0);
+  const Vec4 b(1.0);
+  const Vec4 c(4.0/12.0);
+
+  static Vec4 gam[MAX_BLOCKS_PER_DIM*WID];
+  
+  Vec4 bet=b;
+  u[0]=r[0]/bet;
+  for(int i = 1;i< n ;i++){
+    gam[i] = c/bet;
+    bet = b - a * gam[i];
+    u[i] = (r[i] - a * u[i-1]) / bet;
+  }
+  
+  for(int i = (n-2); i>=0; i--){
+    u[i] -= gam[i+1]*u[i+1];
+  }
 }
 
-inline void filter_extrema(Vec4 *values, uint n_cblocks, Vec4 *fv){   
+
+/*Compute all face values. For cell k (globla index), its left face
+ * value is in fv_l[k] and right value in fv_r[k]. Based on explicit
+ * h6 estimate*/
+inline void compute_h6_face_values(Vec4 *values, uint n_cblocks,  Vec4 *fv_l, Vec4 *fv_r){   
+
+   /*we loop up to one extra cell. There is extra space in fv for the extra left value*/
+  for (int k = 0; k < n_cblocks * WID + 1; k++){
+      /*compute left values*/
+      fv_l[k] = 1.0/60.0 * (values[k - 3 + WID]  - 8.0 * values[k - 2 + WID]  + 37.0 * values[k - 1 + WID] +
+			    37.0 * values[k  + WID] - 8.0 * values[k + 1 + WID] + values[k + 2 + WID]);
+      /*set right value*/
+      if(k>0)
+	fv_r[k-1] = fv_l[k];
+  }
+}
+
+
+
+/*Compute all face values. For cell k (globla index), its left face
+ * value is in fv_l[k] and right value in fv_r[k]. Based on implicit
+ * ih6 estimate*/
+inline void compute_ih6_face_values(Vec4 *values, uint n_cblocks,  Vec4 *fv_l, Vec4 *fv_r){   
+  static Vec4 r[MAX_BLOCKS_PER_DIM*WID + 1];
+  
+  
+  for (uint k = 0; k < n_cblocks * WID + 1; k++){
+    //Note that these are now defined for left face values, so not exactly like in white
+    r[k] = (values[k - 2 + WID] + 29.0 * values[k - 1 + WID] + 29.0 * values[k + WID] + values[k + 1 + WID] )/36.0;
+  }
+  
+  tridiag_ih6(n_cblocks*WID + 1,fv_l,r);
+
+  /*copy right face values */
+  for (uint k = 1; k < n_cblocks * WID + 1; k++){
+    fv_r[k-1] = fv_l[k];
+  }
+
+  /*  if (n_cblocks>6) {
+    for (uint k = 0; k < n_cblocks * WID ; k++){
+      cout << k << " "<< fv_l[k][0] << " " <<values[ k + WID ][0]  << " " << fv_r[k][0] << endl;
+    }
+    exit(1);
+  }
+    
+  */
+}
+
+
+inline void filter_extrema(Vec4 *values, uint n_cblocks, Vec4 *fv_l, Vec4 *fv_r){   
    for (int k = 0; k < n_cblocks * WID; k++){
       //Coella1984 eq. 1.10, detect extrema and make algorithm constant if it is
-      Vec4 extrema_check = ((fv[2 * k + 1] - values[k + WID]) * (values[k + WID] - fv[2 * k]));
-      fv[2 * k] = select(extrema_check < 0, values[k + WID], fv[2 * k]);
-      fv[2 * k + 1] = select(extrema_check < 0, values[k + WID], fv[2 * k + 1]);
+      Vec4 extrema_check = ((fv_r[k] - values[k + WID]) * (values[k + WID] - fv_l[k]));
+      fv_l[k] = select(extrema_check < 0, values[k + WID], fv_l[k]);
+      fv_r[k] = select(extrema_check < 0, values[k + WID], fv_r[k]);
    }
 }
 
 /*Filter according to Eq. 19 in White et al.*/
-inline void filter_boundedness(Vec4 *values, uint n_cblocks, Vec4 *fv){   
+inline void filter_boundedness(Vec4 *values, uint n_cblocks, Vec4 *fv_l, Vec4 *fv_r){   
    /*First Eq. 19 & 20*/
    for (int k = 0; k < n_cblocks * WID; k++){
-      bool fix_bounds = horizontal_or((values[k -1 + WID] - fv[2 * k]) * (fv[2 * k] - values[k + WID]) < 0 ||
-                                      (values[k + 1 + WID] - fv[2 * k + 1]) * (fv[2 * k + 1] - values[k + WID]) < 0);
+      bool fix_bounds = horizontal_or((values[k - 1 + WID] - fv_l[k]) * (fv_l[k] - values[k + WID]) < 0 ||
+                                      (values[k + 1 + WID] - fv_r[k]) * (fv_r[k] - values[k + WID]) < 0);
       if(fix_bounds) {
          Vec4 slope_abs,slope_sign;
          slope_limiter(values[k -1 + WID], values[k + WID], values[k + 1 + WID], slope_abs, slope_sign);
          //detect and  fix boundedness, as in WHITE 2008
-         fv[2 * k] = select((values[k -1 + WID] - fv[2 * k]) * (fv[2 * k] - values[k + WID]) < 0,
-                         values[k + WID] - slope_sign * min( 0.5 * slope_abs, abs(fv[2 * k] - values[k + WID])),
-                         fv[2 * k]);
-         fv[2 * k + 1] = select((values[k + 1 + WID] - fv[2 * k + 1]) * (fv[2 * k + 1] - values[k + WID]) < 0,
-                                values[k + WID] + slope_sign * min( 0.5 * slope_abs, abs(fv[2 * k + 1] - values[k + WID])),
-                                fv[2 * k + 1]);
+         fv_l[k] = select((values[k -1 + WID] - fv_l[k]) * (fv_l[k] - values[k + WID]) < 0,
+                         values[k + WID] - slope_sign * min( 0.5 * slope_abs, abs(fv_l[k] - values[k + WID])),
+                         fv_l[k]);
+         fv_r[k] = select((values[k + 1 + WID] - fv_r[k]) * (fv_r[k] - values[k + WID]) < 0,
+                                values[k + WID] + slope_sign * min( 0.5 * slope_abs, abs(fv_r[k] - values[k + WID])),
+                                fv_r[k]);
       }
    }
 }
@@ -62,16 +116,16 @@ inline void filter_boundedness(Vec4 *values, uint n_cblocks, Vec4 *fv){
 /*"A final check is conducted to make sure
   that discontinuous edge values are monotonic at any edge. If edge values are discontinuous and nonmono-
   tonic, they are both replaced by their average" */
-inline void filter_face_monotonicity(Vec4 *values, uint n_cblocks, Vec4 *fv){   
+inline void filter_face_monotonicity(Vec4 *values, uint n_cblocks, Vec4 *fv_l, Vec4 *fv_r){   
    for (int k = 0; k > n_cblocks * WID - 1; k++){
       /*shift to mean is how much we need to add (subract) from right
        * cell face (left cell face of next cell) to make both values have the average value. It is zero if no
        * filtering needs to be done here*/
-      Vec4 shift_to_mean = select( (fv[2 * (k + 1)] - fv[2 * k + 1]) * (values[k + 1 + WID] - values[k + WID]) < 0.0,
-                                   (fv[2 * (k + 1)] + fv[2 * k + 1]) * 0.5 - fv[2 * k + 1],
+      Vec4 shift_to_mean = select( (fv_l[k+1] - fv_r[k]) * (values[k + 1 + WID] - values[k + WID]) < 0.0,
+                                   (fv_l[k+1] + fv_r[k]) * 0.5 - fv_r[k],
                                    0.0);
-      fv[2 * k + 1] += shift_to_mean;
-      fv[2 * (k + 1)] -= shift_to_mean;
+      fv_r[k] += shift_to_mean;
+      fv_l[k+1] -= shift_to_mean;
    }
 }
 
@@ -101,19 +155,19 @@ inline void compute_plm_coeff_explicit_column(Vec4 *values, uint n_cblocks, Vec4
 inline void compute_ppm_coeff_explicit_column(Vec4 *values, uint n_cblocks, Vec4 a[][RECONSTRUCTION_ORDER + 1]){
    Vec4 p_face;
    Vec4 m_face;
-   Vec4 fv[MAX_BLOCKS_PER_DIM * WID * 2 + 1]; /*space for left and right face value for each cell in column, + 1 extra left value to make some loops easier*/
+   Vec4 fv_l[MAX_BLOCKS_PER_DIM * WID + 1]; /*left face value, extra space for ease of implementation*/
+   Vec4 fv_r[MAX_BLOCKS_PER_DIM * WID + 1]; /*right face value*/
 
    //white 08 H6 face estimates, better than H5. Shift old unfilitered value at upper edge to the lower edge (identical edge)     
 
-   compute_h6_face_values(values,n_cblocks,fv); 
-   filter_boundedness(values,n_cblocks,fv); 
-   filter_extrema(values,n_cblocks,fv);
-
+   compute_ih6_face_values(values,n_cblocks,fv_l, fv_r); 
+   filter_boundedness(values,n_cblocks,fv_l, fv_r); 
+   filter_extrema(values,n_cblocks,fv_l, fv_r);
 //   filter_face_monotonicity(values,n_cblocks,fv);
    
    for (uint k = 0; k < n_cblocks * WID; k++){
-      m_face = fv[2 * k ];
-      p_face = fv[2 * k + 1];
+      m_face = fv_l[k];
+      p_face = fv_r[k];
       
       //Coella et al, check for monotonicity   
       m_face = select((p_face - m_face) * (values[k + WID] - 0.5 * (m_face + p_face)) >
@@ -135,79 +189,12 @@ inline void compute_ppm_coeff_explicit_column(Vec4 *values, uint n_cblocks, Vec4
 }
 
 
+
 // /*
 //   PQM reconstruction as published in:
-
 //   White, Laurent, and Alistair Adcroft. “A High-Order Finite Volume Remapping Scheme for Nonuniform Grids: The Piecewise Quartic Method (PQM).” Journal of Computational Physics 227, no. 15 (July 2008): 7394–7422. doi:10.1016/j.jcp.2008.04.026.
-
 // */
 
-// inline void compute_pqm_coeff_explicit_column(Real *values, uint n_cblocks, uint j, Vec4 a[][RECONSTRUCTION_ORDER + 1]){
-   
-//    Vec4 p_face;
-//    Vec4 m_face;
-//    Vec4 p_face_deriv;
-//    Vec4 m_face_deriv;
-   
-//    Vec4 p_face_unfiltered;
-//    Vec4 p_face_deriv_unfiltered;
-   
-//    Vec4 values[k - 2 + WID],mv,values[k + WID],values[k + 1 + WID],values[k + 2 + WID],values[k + 1 + WID]alues[k + 2 + WID]; /* values in z-direction*/
-//    /*set up shifting operation. These will be shifted to values[k - 2 + WID] - values[k + 1 + WID]*/
-//    values[k - 2 + WID].load(values + i_pcolumnv(n_cblocks, 0, j, -3));
-//    mv.load(values + i_pcolumnv(n_cblocks, 0, j, -2));
-//    values[k + WID].load(values + i_pcolumnv(n_cblocks, 0, j, -1));
-//    values[k + 1 + WID].load(values + i_pcolumnv(n_cblocks, 0, j, 0));
-//    values[k + 2 + WID].load(values + i_pcolumnv(n_cblocks, 0, j, 1));
-//    values[k + 1 + WID]alues[k + 2 + WID].load(values + i_pcolumnv(n_cblocks, 0, j, 2));
-//    p_face_unfiltered = 1.0/60.0 * (values[k - 2 + WID]  - 8.0 * mv  + 37.0 * values[k + WID] + 37.0 * values[k + 1 + WID] - 8.0 * values[k + 2 + WID] + values[k + 1 + WID]alues[k + 2 + WID]);   
-//    p_face_deriv_unfiltered = 1.0 / 180.0 * ( 245.0 * (values[k + 1 + WID] - values[k + WID]) - 25.0 * (values[k + 2 + WID] - mv) + 2.0 * (values[k + 1 + WID]alues[k + 2 + WID] - values[k - 2 + WID]));
-   
-//    for (uint block_i = 0; block_i < n_cblocks; block_i++){
-//       for (uint k = 0; k < WID; ++k){
-//          /*shift values*/
-//          values[k - 2 + WID] = mv;
-//          mv = values[k + WID];
-//          values[k + WID] = values[k + 1 + WID];
-//          values[k + 1 + WID] = values[k + 2 + WID];
-//          values[k + 2 + WID] = values[k + 1 + WID]alues[k + 2 + WID];
-//          values[k + 1 + WID]alues[k + 2 + WID].load(values + i_pcolumnv(n_cblocks, block_i, j, k + 3));
-//          //white 08 H6 face estimates, better than H5. Shift old unfilitered value at upper edge to the lower edge (identical edge)
-//          m_face = p_face_unfiltered;
-//          p_face_unfiltered = 1.0/60.0 * (values[k - 2 + WID]  - 8.0 * mv  + 37.0 * values[k + WID] + 37.0 * values[k + 1 + WID] - 8.0 * values[k + 2 + WID] + values[k + 1 + WID]alues[k + 2 + WID]);
-//          p_face = p_face_unfiltered;
-//          //white-08 H5 estimates for derivatives at cell faces. Same shifting as for face values
-//          m_face_deriv = p_face_deriv_unfiltered;
-//          p_face_deriv_unfiltered = 1.0 / 180.0 * ( 245.0 * (values[k + 1 + WID] - values[k + WID]) - 25.0 * (values[k + 2 + WID] - mv) + 2.0 * (values[k + 1 + WID]alues[k + 2 + WID] - values[k - 2 + WID]));
-//          p_face_deriv = p_face_deriv_unfiltered;
-
-//          /*now we limit the face values and derivatives*/
-         
-//          /* Edge values boundedness - Eq. 19 in white-08*/
-//          bool fix_bounds = horizontal_or((mv - m_face) * (m_face - values[k + WID]) < 0 || (values[k + 1 + WID] - p_face) * (p_face - values[k + WID]) < 0);
-//          if(fix_bounds) {
-//             Vec4 slope_abs,slope_sign;
-//             slope_limiter(mv, values[k + WID], values[k + 1 + WID], slope_abs, slope_sign);
-//             //detect and fix boundedness, as in WHITE 2008
-//             m_face = select((mv - m_face) * (m_face - values[k + WID]) < 0,
-//                             values[k + WID] - slope_sign * min( 0.5 * slope_abs, abs(m_face - values[k + WID])),
-//                             m_face);
-//             p_face = select((values[k + 1 + WID] - p_face) * (p_face - values[k + WID]) < 0,
-//                             values[k + WID] + slope_sign * min( 0.5 * slope_abs, abs(p_face - values[k + WID])),
-//                             p_face);
-//          }
-         
-
-
-         
-//          //Fit a second order polynomial for reconstruction see, e.g., White
-//          //2008 (PQM article) (note additional integration factors built in,
-//          //contrary to White (2008) eq. 4
-//          a[block_i * WID + k][0] = m_face;
-//          a[block_i * WID + k][1] = 3.0 * values[k + WID] - 2.0 * m_face - p_face;
-//          a[block_i * WID + k][2] = (m_face + p_face - 2.0 * values[k + WID]);
-//       }
-//    }
-// }
+// inline void compute_pqm_coeff_explicit_column(Real *values, uint n_cblocks, uint j, Vec4 a[][RECONSTRUCTION_ORDER + 1]){ }
 
 #endif
