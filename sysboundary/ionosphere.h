3e081a8992618e7c1e72caa6043535f9e0761507
Yann Kempf <yann.kempf@fmi.fi>
2014-11-17 18:55:12 +0200

    Created a function to obtain the normal vector to the inner boundary for all three geometries. Tested and debugged for layer 1, the vectors obtained are correct.

--- sysboundary/ionosphere.h
+++ sysboundary/ionosphere.h
@@ -104,6 +104,11 @@ namespace SBC {
          SpatialCell& cell
       );
       
+      std::array<Real, 3> fieldSolverGetNormalDirection(
+         const dccrg::Dccrg<SpatialCell,dccrg::Cartesian_Geometry>& mpiGrid,
+         const CellID& cellID
+      );
+      
       Real center[3]; /*!< Coordinates of the centre of the ionosphere. */
       Real radius; /*!< Radius of the ionosphere. */
       uint geometry; /*!< Geometry of the ionosphere, 0: inf-norm (diamond), 1: 1-norm (square), 2: 2-norm (circle, DEFAULT). */
