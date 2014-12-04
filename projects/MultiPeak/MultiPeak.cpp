d3077cf874d2ac2797ea79b35325da572f9b8642
Yann Kempf <yann.kempf@fmi.fi>
2014-12-04 09:23:59 +0200

    Arbitrary number of populations in MultiPeak. Solves #64.

--- projects/MultiPeak/MultiPeak.cpp
+++ projects/MultiPeak/MultiPeak.cpp
@@ -39,20 +39,14 @@ namespace projects {
 
    void MultiPeak::addParameters(){
       typedef Readparameters RP;
-      RP::add("MultiPeak.rho1", "Number density, first peak (m^-3)", 0.0);
-      RP::add("MultiPeak.rho2", "Number density, second peak (m^-3)", 0.0);
-      RP::add("MultiPeak.Tx1", "Temperature, first peak (K)", 0.0);
-      RP::add("MultiPeak.Tx2", "Temperature, second peak (K)", 0.0);
-      RP::add("MultiPeak.Ty1", "Temperature, first peak (K)", 0.0);
-      RP::add("MultiPeak.Ty2", "Temperature, second peak (K)", 0.0);
-      RP::add("MultiPeak.Tz1", "Temperature, first peak (K)", 0.0);
-      RP::add("MultiPeak.Tz2", "Temperature, second peak (K)", 0.0);
-      RP::add("MultiPeak.Vx1", "Bulk velocity x component, first peak (m/s)", 0.0);
-      RP::add("MultiPeak.Vx2", "Bulk velocity x component, second peak (m/s)", 0.0);
-      RP::add("MultiPeak.Vy1", "Bulk velocity y component, first peak (m/s)", 0.0);
-      RP::add("MultiPeak.Vy2", "Bulk velocity y component, second peak (m/s)", 0.0);
-      RP::add("MultiPeak.Vz1", "Bulk velocity z component, first peak (m/s)", 0.0);
-      RP::add("MultiPeak.Vz2", "Bulk velocity z component, second peak (m/s)", 0.0);
+      RP::add("MultiPeak.n", "Number of populations to use", 0);
+      RP::addComposing("MultiPeak.rho", "Number density (m^-3)");
+      RP::addComposing("MultiPeak.Tx", "Temperature (K)");
+      RP::addComposing("MultiPeak.Ty", "Temperature");
+      RP::addComposing("MultiPeak.Tz", "Temperature");
+      RP::addComposing("MultiPeak.Vx", "Bulk velocity x component (m/s)");
+      RP::addComposing("MultiPeak.Vy", "Bulk velocity y component (m/s)");
+      RP::addComposing("MultiPeak.Vz", "Bulk velocity z component (m/s)");
       RP::add("MultiPeak.Bx", "Magnetic field x component (T)", 0.0);
       RP::add("MultiPeak.By", "Magnetic field y component (T)", 0.0);
       RP::add("MultiPeak.Bz", "Magnetic field z component (T)", 0.0);
@@ -62,54 +56,60 @@ namespace projects {
       RP::add("MultiPeak.magXPertAbsAmp", "Absolute amplitude of the random magnetic perturbation along x (T)", 1.0e-9);
       RP::add("MultiPeak.magYPertAbsAmp", "Absolute amplitude of the random magnetic perturbation along y (T)", 1.0e-9);
       RP::add("MultiPeak.magZPertAbsAmp", "Absolute amplitude of the random magnetic perturbation along z (T)", 1.0e-9);
-      RP::add("MultiPeak.rho1PertAbsAmp", "Absolute amplitude of the density perturbation, first peak", 0.1);
-      RP::add("MultiPeak.rho2PertAbsAmp", "Absolute amplitude of the density perturbation, second peak", 0.1);
-//       RP::add("MultiPeak.Vx1PertAbsAmp", "Absolute amplitude of the Vx perturbation, first peak", 1.0e6);
-//       RP::add("MultiPeak.Vy1PertAbsAmp", "Absolute amplitude of the Vy perturbation, first peak", 1.0e6);
-//       RP::add("MultiPeak.Vz1PertAbsAmp", "Absolute amplitude of the Vz perturbation, first peak", 1.0e6);
-//       RP::add("MultiPeak.Vx2PertAbsAmp", "Absolute amplitude of the Vx perturbation, second peak", 1.0e6);
-//       RP::add("MultiPeak.Vy2PertAbsAmp", "Absolute amplitude of the Vy perturbation, second peak", 1.0e6);
-//       RP::add("MultiPeak.Vz2PertAbsAmp", "Absolute amplitude of the Vz perturbation, second peak", 1.0e6);
-      RP::add("MultiPeak.lambda", "B cosine perturbation wavelength (m)", 0.0);
-      RP::add("MultiPeak.nVelocitySamples", "Number of sampling points per velocity dimension", 5);
+      RP::addComposing("MultiPeak.rhoPertAbsAmp", "Absolute amplitude of the density perturbation");
+      RP::add("MultiPeak.lambda", "B cosine perturbation wavelength (m)", 1.0);
+      RP::add("MultiPeak.nVelocitySamples", "Number of sampling points per velocity dimension", 2);
    }
 
    void MultiPeak::getParameters(){
       typedef Readparameters RP;
       Project::getParameters();
-      RP::get("MultiPeak.rho1", this->rho[0]);
-      RP::get("MultiPeak.rho2", this->rho[1]);
-      RP::get("MultiPeak.Tx1", this->Tx[0]);
-      RP::get("MultiPeak.Tx2", this->Tx[1]);
-      RP::get("MultiPeak.Ty1", this->Ty[0]);
-      RP::get("MultiPeak.Ty2", this->Ty[1]);
-      RP::get("MultiPeak.Tz1", this->Tz[0]);
-      RP::get("MultiPeak.Tz2", this->Tz[1]);
-      RP::get("MultiPeak.Vx1", this->Vx[0]);
-      RP::get("MultiPeak.Vx2", this->Vx[1]);
-      RP::get("MultiPeak.Vy1", this->Vy[0]);
-      RP::get("MultiPeak.Vy2", this->Vy[1]);
-      RP::get("MultiPeak.Vz1", this->Vz[0]);
-      RP::get("MultiPeak.Vz2", this->Vz[1]);
+      RP::get("MultiPeak.n", this->numberOfPopulations);
+      if(this->numberOfPopulations < 1) {
+         std::cerr << "You should set MultiPeak.n to more than 0 populations." << std::endl;
+         abort();
+      }
+      RP::get("MultiPeak.rho", this->rho);
+      RP::get("MultiPeak.Tx", this->Tx);
+      RP::get("MultiPeak.Ty", this->Ty);
+      RP::get("MultiPeak.Tz", this->Tz);
+      RP::get("MultiPeak.Vx", this->Vx);
+      RP::get("MultiPeak.Vy", this->Vy);
+      RP::get("MultiPeak.Vz", this->Vz);
       RP::get("MultiPeak.Bx", this->Bx);
       RP::get("MultiPeak.By", this->By);
       RP::get("MultiPeak.Bz", this->Bz);
+      RP::get("MultiPeak.rhoPertAbsAmp", this->rhoPertAbsAmp);
+      if(!(this->rho.size() == this->Tx.size() &&
+         this->Tx.size() == this->Ty.size() &&
+         this->Ty.size() == this->Tz.size() &&
+         this->Tz.size() == this->Vx.size() &&
+         this->Vx.size() == this->Vy.size() &&
+         this->Vy.size() == this->Vz.size() &&
+         this->Vz.size() == this->rhoPertAbsAmp.size() &&
+         this->rhoPertAbsAmp.size() == this->rho.size()
+      )) {
+         std::cerr << "You should define all parameters (MultiPeak.rho, MultiPeak.Tx, MultiPeak.Ty, MultiPeak.Tz, MultiPeak.Vx, MultiPeak.Vy, MultiPeak.Vz, MultiPeak.rhoPertAbsAmp) for each of the populations." << std::endl;
+         abort();
+      }
+      if(this->numberOfPopulations > this->rho.size()) {
+         std::cerr << "You are requesting more populations than are currently defined. Change MultiPeak.n or define more populations." << std::endl;
+         abort();
+      }
+      
       RP::get("MultiPeak.magXPertAbsAmp", this->magXPertAbsAmp);
       RP::get("MultiPeak.magYPertAbsAmp", this->magYPertAbsAmp);
       RP::get("MultiPeak.magZPertAbsAmp", this->magZPertAbsAmp);
-      RP::get("MultiPeak.rho1PertAbsAmp", this->rhoPertAbsAmp[0]);
-      RP::get("MultiPeak.rho2PertAbsAmp", this->rhoPertAbsAmp[1]);
-//       RP::get("MultiPeak.Vx1PertAbsAmp", this->Vx1PertAbsAmp);
-//       RP::get("MultiPeak.Vy1PertAbsAmp", this->Vy1PertAbsAmp);
-//       RP::get("MultiPeak.Vz1PertAbsAmp", this->Vz1PertAbsAmp);
-//       RP::get("MultiPeak.Vx2PertAbsAmp", this->Vx2PertAbsAmp);
-//       RP::get("MultiPeak.Vy2PertAbsAmp", this->Vy2PertAbsAmp);
-//       RP::get("MultiPeak.Vz2PertAbsAmp", this->Vz2PertAbsAmp);
       RP::get("MultiPeak.dBx", this->dBx);
       RP::get("MultiPeak.dBy", this->dBy);
       RP::get("MultiPeak.dBz", this->dBz);
       RP::get("MultiPeak.lambda", this->lambda);
       RP::get("MultiPeak.nVelocitySamples", this->nVelocitySamples);
+      
+      // initialize that vector to avoid segmentation faults
+      for(uint i=0; i<this->numberOfPopulations; i++) {
+         this->rhoRnd.push_back(0.0);
+      }
    }
 
    Real MultiPeak::getDistribValue(creal& vx, creal& vy, creal& vz, creal& dvx, creal& dvy, creal& dvz) {
@@ -117,14 +117,15 @@ namespace projects {
       creal kb = physicalconstants::K_B;
       
       Real value = 0.0;
-      for(uint i=0; i<2; i++) {
+      for(uint i=0; i<this->numberOfPopulations; i++) {
          value += this->rhoRnd[i] * pow(mass / (2.0 * M_PI * kb ), 1.5) * 1.0 / sqrt(this->Tx[i]*this->Ty[i]*this->Tz[i]) *
       exp(- mass * (pow(vx - this->Vx[i], 2.0) / (2.0 * kb * this->Tx[i]) + pow(vy - this->Vy[i], 2.0) / (2.0 * kb * this->Ty[i]) + pow(vz - this->Vz[i], 2.0) / (2.0 * kb * this->Tz[i])));
       }
       return value;
    }
 
-   Real MultiPeak::calcPhaseSpaceDensity(creal& x, creal& y, creal& z, creal& dx, creal& dy, creal& dz, creal& vx, creal& vy, creal& vz, creal& dvx, creal& dvy, creal& dvz) {   
+   Real MultiPeak::calcPhaseSpaceDensity(creal& x, creal& y, creal& z, creal& dx, creal& dy, creal& dz, creal& vx, creal& vy, creal& vz, creal& dvx, creal& dvy, creal& dvz) {
+// FIXME allow only 1 sample
       creal d_vx = dvx / (this->nVelocitySamples-1);
       creal d_vy = dvy / (this->nVelocitySamples-1);
       creal d_vz = dvz / (this->nVelocitySamples-1);
@@ -153,7 +154,7 @@ namespace projects {
       cellParams[CellParams::PERBY] += this->magYPertAbsAmp * (0.5 - getRandomNumber());
       cellParams[CellParams::PERBZ] += this->magZPertAbsAmp * (0.5 - getRandomNumber());
       
-      for(uint i=0; i<2; i++) {
+      for(uint i=0; i<this->numberOfPopulations; i++) {
          this->rhoRnd[i] = this->rho[i] + this->rhoPertAbsAmp[i] * (0.5 - getRandomNumber());
       }
    }
@@ -173,7 +174,7 @@ namespace projects {
       creal z
    ) {
       vector<std::array<Real, 3>> centerPoints;
-      for(uint i=0; i<2; i++) {
+      for(uint i=0; i<this->numberOfPopulations; i++) {
          std::array<Real, 3> point {{this->Vx[i], this->Vy[i], this->Vz[i]}};
          centerPoints.push_back(point);
       }
