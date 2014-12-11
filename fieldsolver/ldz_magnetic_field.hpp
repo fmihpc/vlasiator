2feb02cb52012b0fd138559e85008403b4ecb578
Yann Kempf <yann.kempf@fmi.fi>
2014-12-11 16:06:25 +0200

    Fixed time stepping in magnetic field propagation.

--- fieldsolver/ldz_magnetic_field.hpp
+++ fieldsolver/ldz_magnetic_field.hpp
@@ -76,18 +76,23 @@ static void propagateMagneticField(
       }
       cp2 = mpiGrid[nbrID]->parameters;
 
-      # ifdef FS_1ST_ORDER_TIME
-      cp0[CellParams::PERBX] += dt/dz*(cp2[CellParams::EY] - cp0[CellParams::EY]) + dt/dy*(cp0[CellParams::EZ] - cp1[CellParams::EZ]);
-      # else
-      if(RKCase == RK_ORDER2_STEP1) {
-         cp0[CellParams::PERBX_DT2] =
-            cp0[CellParams::PERBX] + 0.5*dt*(1.0/dz*(cp2[CellParams::EY] - cp0[CellParams::EY]) +
-                                             1.0/dy*(cp0[CellParams::EZ] - cp1[CellParams::EZ]));
-      } else {
-         cp0[CellParams::PERBX] += dt * (1.0/dz*(cp2[CellParams::EY_DT2] - cp0[CellParams::EY_DT2]) +
-                                         1.0/dy*(cp0[CellParams::EZ_DT2] - cp1[CellParams::EZ_DT2]));
+      switch (RKCase) {
+         case RK_ORDER1:
+            cp0[CellParams::PERBX] += dt/dz*(cp2[CellParams::EY] - cp0[CellParams::EY]) + dt/dy*(cp0[CellParams::EZ] - cp1[CellParams::EZ]);
+            break;
+         case RK_ORDER2_STEP1:
+            cp0[CellParams::PERBX_DT2] =
+               cp0[CellParams::PERBX] + 0.5*dt*(1.0/dz*(cp2[CellParams::EY] - cp0[CellParams::EY]) +
+                                                1.0/dy*(cp0[CellParams::EZ] - cp1[CellParams::EZ]));
+            break;
+         case RK_ORDER2_STEP2:
+            cp0[CellParams::PERBX] += dt * (1.0/dz*(cp2[CellParams::EY_DT2] - cp0[CellParams::EY_DT2]) +
+                                            1.0/dy*(cp0[CellParams::EZ_DT2] - cp1[CellParams::EZ_DT2]));
+            break;
+         default:
+            std::cerr << __FILE__ << ":" << __LINE__ << ":" << "Invalid RK case." << std::endl;
+            abort();
       }
-      # endif
    }
    
    // Propagate face-averaged By:
@@ -122,18 +127,23 @@ static void propagateMagneticField(
       }
       cp2 = mpiGrid[nbrID]->parameters;
 
-      # ifdef FS_1ST_ORDER_TIME
-      cp0[CellParams::PERBY] += dt/dx*(cp2[CellParams::EZ] - cp0[CellParams::EZ]) + dt/dz*(cp0[CellParams::EX] - cp1[CellParams::EX]);
-      # else
-      if(RKCase == RK_ORDER2_STEP1) {
-         cp0[CellParams::PERBY_DT2] =
-            cp0[CellParams::PERBY] + 0.5*dt*(1.0/dx*(cp2[CellParams::EZ] - cp0[CellParams::EZ]) +
-                                             1.0/dz*(cp0[CellParams::EX] - cp1[CellParams::EX]));
-      } else {
-         cp0[CellParams::PERBY] += dt * (1.0/dx*(cp2[CellParams::EZ_DT2] - cp0[CellParams::EZ_DT2]) +
-                                         1.0/dz*(cp0[CellParams::EX_DT2] - cp1[CellParams::EX_DT2]));
+      switch (RKCase) {
+         case RK_ORDER1:
+            cp0[CellParams::PERBY] += dt/dx*(cp2[CellParams::EZ] - cp0[CellParams::EZ]) + dt/dz*(cp0[CellParams::EX] - cp1[CellParams::EX]);
+            break;
+         case RK_ORDER2_STEP1:
+            cp0[CellParams::PERBY_DT2] =
+               cp0[CellParams::PERBY] + 0.5*dt*(1.0/dx*(cp2[CellParams::EZ] - cp0[CellParams::EZ]) +
+                                                1.0/dz*(cp0[CellParams::EX] - cp1[CellParams::EX]));
+            break;
+         case RK_ORDER2_STEP2:
+            cp0[CellParams::PERBY] += dt * (1.0/dx*(cp2[CellParams::EZ_DT2] - cp0[CellParams::EZ_DT2]) +
+                                            1.0/dz*(cp0[CellParams::EX_DT2] - cp1[CellParams::EX_DT2]));
+            break;
+         default:
+            std::cerr << __FILE__ << ":" << __LINE__ << ":" << "Invalid RK case." << std::endl;
+            abort();
       }
-      # endif
    }
       
    // Propagate face-averaged Bz:
@@ -168,18 +178,23 @@ static void propagateMagneticField(
       }
       cp2 = mpiGrid[nbrID]->parameters;
       
-      # ifdef FS_1ST_ORDER_TIME
-      cp0[CellParams::PERBZ] += dt/dy*(cp2[CellParams::EX] - cp0[CellParams::EX]) + dt/dx*(cp0[CellParams::EY] - cp1[CellParams::EY]);
-      # else
-      if(RKCase == RK_ORDER2_STEP1) {
-         cp0[CellParams::PERBZ_DT2] =
-            cp0[CellParams::PERBZ] + 0.5*dt*(1.0/dy*(cp2[CellParams::EX] - cp0[CellParams::EX]) +
-                                             1.0/dx*(cp0[CellParams::EY] - cp1[CellParams::EY]));
-      } else {
-         cp0[CellParams::PERBZ] += dt  * (1.0/dy*(cp2[CellParams::EX_DT2] - cp0[CellParams::EX_DT2]) +
-                                          1.0/dx*(cp0[CellParams::EY_DT2] - cp1[CellParams::EY_DT2]));
+      switch (RKCase) {
+         case RK_ORDER1:
+            cp0[CellParams::PERBZ] += dt/dy*(cp2[CellParams::EX] - cp0[CellParams::EX]) + dt/dx*(cp0[CellParams::EY] - cp1[CellParams::EY]);
+            break;
+         case RK_ORDER2_STEP1:
+            cp0[CellParams::PERBZ_DT2] =
+               cp0[CellParams::PERBZ] + 0.5*dt*(1.0/dy*(cp2[CellParams::EX] - cp0[CellParams::EX]) +
+                                                1.0/dx*(cp0[CellParams::EY] - cp1[CellParams::EY]));
+               break;
+         case RK_ORDER2_STEP2:
+            cp0[CellParams::PERBZ] += dt  * (1.0/dy*(cp2[CellParams::EX_DT2] - cp0[CellParams::EX_DT2]) +
+                                             1.0/dx*(cp0[CellParams::EY_DT2] - cp1[CellParams::EY_DT2]));
+            break;
+         default:
+            std::cerr << __FILE__ << ":" << __LINE__ << ":" << "Invalid RK case." << std::endl;
+            abort();
       }
-      # endif
    }
 }
 
