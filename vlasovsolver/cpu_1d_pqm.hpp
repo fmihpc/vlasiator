b593e129376255fc424f7b0f00ecbad495f4a8a0
Sebastian von Alfthan <alfthan@voima-login2.(none)>
2015-01-26 09:40:46 +0200

    Removed slow inserts from PQM filters, speedup of ~5% for the complete propagation of vlasiator

--- vlasovsolver/cpu_1d_pqm.hpp
+++ vlasovsolver/cpu_1d_pqm.hpp
@@ -54,48 +54,68 @@ inline void filter_pqm_monotonicity(Vec *values, uint k, Vec &fv_l, Vec &fv_r, V
                              c0  + root1 * ( c1 + root1 * (c2 + root1 * c3 ) ),
                              slope_sign);
    Vec root2_slope = select(root2 >= 0.0 && root2 <= 1.0,
-                             c0  + root2 * ( c1 + root2 * (c2 + root2 * c3 ) ),
-                             slope_sign);
-   if (horizontal_or (root1_slope * slope_sign < 0.0 || root2_slope * slope_sign < 0.0 )) {
-   //serialized the handling of inflexion points, these do not happen for smooth regions
-   for(uint i = 0;i < VECL; i++) {
-      if(root1_slope[i] * slope_sign[i] < 0.0 || root2_slope[i] * slope_sign[i] < 0.0 ) {
-         //need to collapse, at least one inflexion point has wrong
-         //sign.
-         if(fabs(plm_slope_l[i]) <= fabs(plm_slope_r[i])) {
-            //collapse to left edge (eq 21)
-            fd_l.insert( i, 1.0 / 3.0 * ( 10 * values[k][i] - 2.0 * fv_r[i] - 8.0 * fv_l[i]));
-            fd_r.insert( i, -10.0 * values[k][i] + 6.0 * fv_r[i] + 4.0 * fv_l[i]);
-            //check if PLM slope is consistent (eq 28 & 29)
-            if (slope_sign[i] * fd_l[i] < 0) {
-               fd_l.insert( i, 0);
-               fv_r.insert( i, 5 * values[k][i] - 4 * fv_l[i]);
-               fd_r.insert( i, 20 * (values[k][i] - fv_l[i]));
+                            c0  + root2 * ( c1 + root2 * (c2 + root2 * c3 ) ),
+                            slope_sign);
+   Vecb fixInflexion = root1_slope * slope_sign < 0.0 || root2_slope * slope_sign < 0.0;
+   if (horizontal_or (fixInflexion) ){ 
+      Realv valuesa[VECL];
+      Realv fva_l[VECL];
+      Realv fva_r[VECL];
+      Realv fda_l[VECL];
+      Realv fda_r[VECL];
+      Realv slope_signa[VECL];
+      values[k].store(valuesa);
+      fv_l.store(fva_l);
+      fd_l.store(fda_l);
+      fv_r.store(fva_r);
+      fd_r.store(fda_r);
+      slope_sign.store(slope_signa);
+      
+      //todo store and then load data to avoid inserts (is it beneficial...?)
+      
+//serialized the handling of inflexion points, these do not happen for smooth regions
+      for(uint i = 0;i < VECL; i++) {
+         if(fixInflexion[i]){
+            //need to collapse, at least one inflexion point has wrong
+            //sign.
+            if(fabs(plm_slope_l[i]) <= fabs(plm_slope_r[i])) {
+               //collapse to left edge (eq 21)
+               fda_l[i] =  1.0 / 3.0 * ( 10 * valuesa[i] - 2.0 * fva_r[i] - 8.0 * fva_l[i]);
+               fda_r[i] =  -10.0 * valuesa[i] + 6.0 * fva_r[i] + 4.0 * fva_l[i];
+               //check if PLM slope is consistent (eq 28 & 29)
+               if (slope_signa[i] * fda_l[i] < 0) {
+                  fda_l[i] =  0;
+                  fva_r[i] =  5 * valuesa[i] - 4 * fva_l[i];
+                  fda_r[i] =  20 * (valuesa[i] - fva_l[i]);
+               }
+               else if (slope_signa[i] * fda_r[i] < 0) {
+                  fda_r[i] =  0;
+                  fva_l[i] =  0.5 * (5 * valuesa[i] - 3 * fva_r[i]);
+                  fda_l[i] =  10.0 / 3.0 * (-valuesa[i] + fva_r[i]);
+               }
             }
-            else if (slope_sign[i] * fd_r[i] < 0) {
-               fd_r.insert( i, 0);
-               fv_l.insert( i, 0.5 * (5 * values[k][i] - 3 * fv_r[i]));
-               fd_l.insert( i, 10.0 / 3.0 * (-values[k][i] + fv_r[i]));
-            }
-         }
-         else {
-            //collapse to right edge (eq 21)
-            fd_l.insert( i, 10.0 * values[k][i] - 6.0 * fv_l[i] - 4.0 * fv_r[i]);
-            fd_r.insert( i, 1.0 / 3.0 * ( - 10.0 * values[k][i] + 2 * fv_l[i] + 8 * fv_r[i]));
-            //check if PLM slope is consistent (eq 28 & 29)
-            if (slope_sign[i] * fd_l[i] < 0) {
-               fd_l.insert( i, 0);
-               fv_r.insert( i, 0.5 * ( 5 * values[k][i] - 3 * fv_l[i]));
-               fd_r.insert( i, 10.0 / 3.0 * (values[k][i] - fv_l[i]));
-            }
-            else if (slope_sign[i] * fd_r[i] < 0) {
-               fd_r.insert( i, 0);
-               fv_l.insert( i, 5 * values[k][i] - 4 * fv_r[i]);
-               fd_l.insert( i, 20.0 * ( - values[k][i] + fv_r[i]));
+            else {
+               //collapse to right edge (eq 21)
+               fda_l[i] =  10.0 * valuesa[i] - 6.0 * fva_l[i] - 4.0 * fva_r[i];
+               fda_r[i] =  1.0 / 3.0 * ( - 10.0 * valuesa[i] + 2 * fva_l[i] + 8 * fva_r[i]);
+               //check if PLM slope is consistent (eq 28 & 29)
+               if (slope_signa[i] * fda_l[i] < 0) {
+                  fda_l[i] =  0;
+                  fva_r[i] =  0.5 * ( 5 * valuesa[i] - 3 * fva_l[i]);
+                  fda_r[i] =  10.0 / 3.0 * (valuesa[i] - fva_l[i]);
+               }
+               else if (slope_signa[i] * fda_r[i] < 0) {
+                  fda_r[i] =  0;
+                  fva_l[i] =  5 * valuesa[i] - 4 * fva_r[i];
+                  fda_l[i] =  20.0 * ( - valuesa[i] + fva_r[i]);
                }
             }
          }
-      }
+      }      
+      fv_l.load(fva_l);
+      fd_l.load(fda_l);
+      fv_r.load(fva_r);
+      fd_r.load(fda_r);
    }
 }
 
