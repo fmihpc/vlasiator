93e7f7e34b749422ddf428e936443b131f80b436
Sebastian von Alfthan <sebastian.von.alfthan@fmi.fi>
2015-03-31 10:15:57 +0300

    Revert "Added sensible scripts for the current use."
    This reverts commit 74a933fcb07dec8b48c16597068b747da2d01395.

---
+++ tools/HornetQueueWizard/general_job_16384_24h.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#PBS -N c16384_24h
+#PBS -j oe
+# Request the number of cores that you need in total
+#PBS -l mppwidth=16384
+#PBS -l mppnppn=32
+# Request the time you need for computation
+#PBS -l walltime=24:00:00
+#PBS -W umask=002
+
+umask 002
+
+# Change to the directory that the job was submitted from
+cd $PBS_O_WORKDIR
+
+# Set job list to choose from
+JOBLIST=${PBS_O_WORKDIR}/joblist_16384.txt
+# Set the number of OpenMP threads per node
+export OMP_NUM_THREADS=8
+# Determine the number of processes
+NUM_PROCESSES=$(( $(qstat -f $BATCH_JOBID | grep "Resource_List.mppwidth" | cut -d "=" -f 2) / $OMP_NUM_THREADS ))
+# Set the number of restarts to write
+NUMBER_OF_RESTARTS=2
+# Allow for this buffer for the restart IO (in s)
+RESTART_IO_EXTRA_TIME=1800.0
+# Determine walltime
+WALLTIME=$(qstat -f $BATCH_JOBID | grep "Resource_List.walltime" | cut -d "=" -f 2 | cut -d ":" -f 1)
+
+source vlasiator_run_next.sh
