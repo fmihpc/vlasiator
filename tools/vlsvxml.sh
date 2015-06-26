3a1be9d2897a06cec8a86b91be0f118939534d35
Sebastian von Alfthan <sebastian.von.alfthan@fmi.fi>
2015-06-26 10:51:29 +0200

    Updated vlsvls and created vlsvxml
    vlsvls is now faster (byte dependent tail), and does not print the unsupported value in xml. Instead writes out mesh
    
    Also created vlsvxml that just prints out the xml footer

---
+++ tools/vlsvxml.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 1 ] 
+then
+    cat    <<EOF 
+
+Prints out XML footer of a vlsv file
+
+Usage: $0 file.vlsv
+
+EOF
+    exit
+fi
+
+
+
+if [ -e $1 ]
+then
+tail -c $(( 10 * 1024))  $1 |strings |gawk 'BEGIN {xmlstarted=0} {if($1 == "<VLSV>") xmlstarted=1; if(xmlstarted) print $0;}' 
+
+else
+
+echo "File $1 does not exist!"
+
+fi
