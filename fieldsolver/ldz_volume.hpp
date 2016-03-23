1cdcd3edacaca882e38aeaac48d8c225dfd683ab
Yann Kempf <yann.kempf@fmi.fi>
2016-03-23 12:02:16 +0200

    Fixed ldz_volume function declaration. NOW COMPILES!

--- fieldsolver/ldz_volume.hpp
+++ fieldsolver/ldz_volume.hpp
@@ -20,11 +20,11 @@ Copyright 2010-2015 Finnish Meteorological Institute
  * \sa reconstructionCoefficients
  */
 void calculateVolumeAveragedFields(
-   const FsGrid< std::array<Real, fsgrids::bfield::N_BFIELD>, 2> & perBGrid,
-   const FsGrid< std::array<Real, fsgrids::efield::N_EFIELD>, 2> & EGrid,
-   const FsGrid< std::array<Real, fsgrids::dperb::N_DPERB>, 2> & dPerBGrid,
+   FsGrid< std::array<Real, fsgrids::bfield::N_BFIELD>, 2> & perBGrid,
+   FsGrid< std::array<Real, fsgrids::efield::N_EFIELD>, 2> & EGrid,
+   FsGrid< std::array<Real, fsgrids::dperb::N_DPERB>, 2> & dPerBGrid,
    FsGrid< std::array<Real, fsgrids::volfields::N_VOL>, 2> & volGrid,
-   const FsGrid< fsgrids::technical, 2> & technicalGrid
+   FsGrid< fsgrids::technical, 2> & technicalGrid
 );
 
 #endif
