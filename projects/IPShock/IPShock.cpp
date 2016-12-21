11524ccaff25e41138b2112d6c015e3737dc69fd
Markus Battarbee <markus.battarbee@gmail.com>
2016-12-21 14:15:16 +0200

    Added license headers to IPshock project

--- projects/IPShock/IPShock.cpp
+++ projects/IPShock/IPShock.cpp
@@ -1,7 +1,24 @@
 /*
-This file is part of Vlasiator.
+ * This file is part of Vlasiator.
+ * Copyright 2010-2016 Finnish Meteorological Institute
+ *
+ * For details of usage, see the COPYING file and read the "Rules of the Road"
+ * at http://vlasiator.fmi.fi/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
 
-Copyright 2016 Finnish Meteorological Institute
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 %
 Interplanetary shock project by Markus Battarbee (markus.battarbee@gmail.com)
 Based on SilvaShock project by Urs Ganse
