2e9bc7ae1cdba3d26936536199c4875deb4ebe10
ykempf <yann.kempf@fmi.fi>
2017-02-22 13:14:58 +0200

    Updated Dispersion.

--- projects/Dispersion/Dispersion.h
+++ projects/Dispersion/Dispersion.h
@@ -37,9 +37,9 @@ namespace projects {
       virtual bool initialize(void);
       static void addParameters(void);
       virtual void getParameters(void);
-      virtual void setCellBackgroundField(spatial_cell::SpatialCell* cell);
+      virtual void setCellBackgroundField(spatial_cell::SpatialCell* cell) const;
     protected:
-      Real getDistribValue(creal& vx, creal& vy, creal& vz);
+      Real getDistribValue(creal& vx, creal& vy, creal& vz) const;
       virtual void calcCellParameters(spatial_cell::SpatialCell* cell,creal& t);
       virtual Real calcPhaseSpaceDensity(
                                          creal& x, creal& y, creal& z,
@@ -47,7 +47,7 @@ namespace projects {
                                          creal& vx, creal& vy, creal& vz,
                                          creal& dvx, creal& dvy, creal& dvz,
                                          const int& popID
-                                        );
+                                        ) const;
 
       Real B0;
       Real VX0;
