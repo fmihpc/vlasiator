b9da201bd00274a5e4341dc65d216dd2906f72ac
tkoskela <tskoskela@gmail.com>
2018-05-22 17:22:46 +0300

    Added functions for calculating ppm coefficients on nonuniform grid.

--- vlasovsolver/cpu_face_estimates.hpp
+++ vlasovsolver/cpu_face_estimates.hpp
@@ -167,6 +167,30 @@ inline void compute_h4_left_face_value(const Vec * const values, uint k, Vec &fv
 }
 
 
+/*!
+
+  Compute left face value based on the explicit h4 estimate for nonuniform grid.
+  Eqn 45 in White et. al. 2008
+
+  \param u Array with volume averages. It is assumed a large enough stencil is defined around i.
+  \param i Index of cell in values for which the left face is computed
+  \param fv_l Face value on left face of cell i
+  \param h Array with cell widths. Can be in abritrary units since they always cancel. Maybe 1/refinement ratio?
+*/
+inline void compute_h4_left_face_value_nonuniform(const Vec * const h, const Vec * const u, uint k, Vec &fv_l) {
+
+  fv_l = (
+	  1.0 / ( h[k - 2] + h[k - 1] + h[k] + h[k + 1] )
+	  * ( ( h[k - 2] + h[k - 1] ) * ( h[k] + h[k + 1] ) / ( h[k - 1] + h[k] )
+	      * ( u[k - 1] * h[k] + u[k] * h[k - 1] )
+	      * (1.0 / ( h[k - 2] + h[k - 1] + h[k] ) + 1.0 / ( h[k - 1] + h[k] + h[k + 1] ) )
+	      + ( h[k] * ( h[k] + h[k + 1] ) ) / ( ( h[k - 2] + h[k - 1] + h[k] ) * (h[k - 2] + h[k - 1] ) )
+	      * ( u[k - 1] * (h[k - 2] * 2.0 * h[k - 1] ) - ( u[k - 2] * h[k - 1] ) )
+	      + h[k - 1] * ( h[k - 2] + h[k - 1] ) / ( ( h[k - 1] + h[k] + h[k + 1] ) * ( h[k] + h[k + 1] ) )
+	      * ( u[k] * ( 2.0 * h[k] + h[k + 1] ) - u[k + 1] * h[k] ) )
+	  );
+}							    
+  
 /*! 
   Compute left face value based on the explicit h4 estimate.
 
@@ -297,5 +321,52 @@ inline void compute_filtered_face_values(const Vec * const values,uint k, face_e
       fv_r=select(filter, values[k] + slope_sign * 0.5 * slope_abs, fv_r);
    }
 }
+
+
+inline void compute_filtered_face_values_nonuniform(const Vec * const dv, const Vec * const values,uint k, face_estimate_order order, Vec &fv_l, Vec &fv_r){   
+  switch(order){
+  case h4:
+    compute_h4_left_face_value_nonuniform(dv, values, k, fv_l);
+    compute_h4_left_face_value_nonuniform(dv, values, k + 1, fv_r);
+    break;
+  // case h5:
+  //   compute_h5_face_values(dv, values, k, fv_l, fv_r);
+  //   break;
+  // case h6:
+  //   compute_h6_left_face_value(dv, values, k, fv_l);
+  //   compute_h6_left_face_value(dv, values, k + 1, fv_r);   
+  //   break;
+  // case h8:
+  //   compute_h8_left_face_value(dv, values, k, fv_l);
+  //   compute_h8_left_face_value(dv, values, k + 1, fv_r);   
+  //   break;
+  default:
+    break;
+  }
+   Vec slope_abs,slope_sign;
+   slope_limiter(values[k -1], values[k], values[k + 1], slope_abs, slope_sign);
+   
+   //check for extrema, flatten if it is
+   Vecb is_extrema = (slope_abs == Vec(0.0));
+   if(horizontal_or(is_extrema)) {
+      fv_r = select(is_extrema, values[k], fv_r);
+      fv_l = select(is_extrema, values[k], fv_l);
+   }
+
+   //Fix left face if needed; boundary value is not bounded
+   Vecb filter = (values[k -1] - fv_l) * (fv_l - values[k]) < 0 ;
+   if(horizontal_or (filter)) {  
+      //Go to linear (PLM) estimates if not ok (this is always ok!)
+      fv_l=select(filter, values[k ] - slope_sign * 0.5 * slope_abs, fv_l);
+   }
+
+   //Fix  face if needed; boundary value is not bounded    
+   filter = (values[k + 1] - fv_r) * (fv_r - values[k]) < 0;
+   if(horizontal_or (filter)) {  
+      //Go to linear (PLM) estimates if not ok (this is always ok!)
+      fv_r=select(filter, values[k] + slope_sign * 0.5 * slope_abs, fv_r);
+   }
+}
+
 
 #endif
