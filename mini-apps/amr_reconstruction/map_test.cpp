c52dce741fcba1feef64a16beb9c9be3017afb94
tkoskela <tskoskela@gmail.com>
2018-05-25 16:02:45 +0300

    Added refinement to the uniform grid to test. Shifted values by 0.5 cells which makes the reconstructions much smoother. Changed h-parameter in the coefficient calculation call to h4.

--- mini-apps/amr_reconstruction/map_test.cpp
+++ mini-apps/amr_reconstruction/map_test.cpp
@@ -29,31 +29,27 @@ void print_reconstruction(int step, Vec dv[], Vec values[], uint  blocks_per_dim
   FILE* fp=fopen(name,"w");
   FILE* fp2=fopen("a.dat","w");
 
-  Vec v_r = v_min;
+  Vec v0 = v_min;
   const int subcells = 50;
   /*loop through all blocks in column and divide into subcells. Print value of reconstruction*/
   for (unsigned int k_block = 0; k_block<blocks_per_dim;k_block++){
     for (uint k_cell=0; k_cell<WID; ++k_cell){ 
 #ifdef ACC_SEMILAG_PPM
       Vec a[3];
-      //Vec a2[3];
-      //compute_ppm_coeff(               values, h6, (k_block + 1) * WID + k_cell, a);
-      compute_ppm_coeff_nonuniform(dv, values, h6, (k_block + 1) * WID + k_cell, a);
-      fprintf(fp2,"%12.8g %12.8g %12.8g\n",a[0][0],a[1][0],a[2][0]);
-#endif
-      
-      Vec v_l = v_min + (k_block * WID + k_cell) * dv0;
+      //compute_ppm_coeff(               values, h4, (k_block + 1) * WID + k_cell, a);
+      compute_ppm_coeff_nonuniform(dv, values, h4, (k_block + 1) * WID + k_cell, a);
+      fprintf(fp2,"%12.8g %12.8g %12.8g %12.8g\n",values[(k_block + 1) * WID + k_cell][0], a[0][0],a[1][0],a[2][0]);
+#endif     
       
       int iend = k_block * WID + k_cell;
       if (iend > 0)
-	v_r += dv[iend-1+WID];
+	v0 += dv[iend-1+WID];
+	
       
       for (uint k_subcell=0; k_subcell< subcells; ++k_subcell){ 
 	Vec v_norm = (Real)(k_subcell + 0.5)/subcells; //normalized v of subcell in source cell
-	Vec v0 = v_l + v_norm * dv0;       
-
-	Vec v = v_r + v_norm * dv[k_block * WID + k_cell + WID];
-
+	Vec v = v0 + v_norm * dv[k_block * WID + k_cell + WID];
+	
 #ifdef ACC_SEMILAG_PPM
 	Vec target = 
 	  a[0] +
@@ -74,41 +70,60 @@ void print_reconstruction(int step, Vec dv[], Vec values[], uint  blocks_per_dim
 int main(void) {
   
   const Real dv0 = 20000;
-  const Real v_min = -4e6;
+  //const Real v_min = -4e6;
   const int blocks_per_dim = 100;
   const int i_block = 0; //x index of block, fixed in this simple test
   const int j_block = 0; //y index of block, fixed in this simple test
   const int j_cell = 0; // y index of cell within block (0..WID-1)
 
   Vec dv[(blocks_per_dim+2)*WID];
-  Real dvTmp[(blocks_per_dim+2)*WID];
   Vec values[(blocks_per_dim+2)*WID];
-  Real rnd;
-  //std::random_device rd;
-  //std::mt19937 e2(rd());
-  //std::uniform_real_distribution<> dist(0, 10);
 
   boost::mt19937 rng;
   boost::uniform_real<Real> u(0.0, 2.0 * dv0);
   boost::variate_generator<boost::mt19937&, boost::uniform_real<Real> > gen(rng, u);
+  gen.distribution().reset();
+  gen.engine().seed(12345);
   
   /*initial values*/  
   /*clear target & values array*/
   for (uint k=0; k<WID* (blocks_per_dim + 2); ++k){ 
     values[k] = 0.0;
-    //    dv[k] = dv0;
-    dv[k] = gen();
+    dv[k] = dv0;
+    //dv[k] = gen();
   }
 
+  int max_refinement = 5;
+  int cells_per_level = 2;
+  for (uint k=0; k < max_refinement * cells_per_level; ++k) {
+    dv[(blocks_per_dim + 2) * WID / 2 + k] = dv[(blocks_per_dim + 2) * WID / 2 + k]/pow(2,(max_refinement - k / cells_per_level));
+    if (k > 0)
+      dv[(blocks_per_dim + 2) * WID / 2 - k] = dv[(blocks_per_dim + 2) * WID / 2 - k]/pow(2,(max_refinement - k / cells_per_level));
+  }
+
+  Real v_min = 0.0;
+  for (uint k=WID;k < (blocks_per_dim + 2) * WID / 2; ++k) {    
+    v_min -= dv[k][0];
+  }
+  
   Real T = 500000;
   Real rho = 1.0e6;
   Real v = v_min;
+  Real const v1 = 10 * dv0;
   
   for(uint i=0; i < blocks_per_dim * WID; i++){
     // Real v=v_min + i*dv;
+
+    // Evaluate the function at the middle of the v cell
+    v = v + 0.5 * dv[i + WID][0];
     values[i + WID] = rho * pow(physicalconstants::MASS_PROTON / (2.0 * M_PI * physicalconstants::K_B * T), 1.5) *
       exp(- physicalconstants::MASS_PROTON * v * v / (2.0 * physicalconstants::K_B * T));
-    v = v + dv[i + WID][0];
+
+    //values[i + WID] = rho * pow(physicalconstants::MASS_PROTON / (2.0 * M_PI * physicalconstants::K_B * T), 1.5) *
+    //  ( exp(- physicalconstants::MASS_PROTON * v * v / (2.0 * physicalconstants::K_B * T)) +
+    //    exp(- physicalconstants::MASS_PROTON * (v+v1) * (v+v1) / (2.0 * physicalconstants::K_B * T)));
+    // Move to the end of the cell for the next iteration
+    v = v + 0.5 * dv[i + WID][0];
   }
   
   // print_values(0,values,blocks_per_dim, v_min, dv);
--- mini-apps/amr_reconstruction/plot_reconstruction.py
+++ mini-apps/amr_reconstruction/plot_reconstruction.py
@@ -1,15 +1,27 @@
 from pylab import *
+from cycler import cycler
 
-dat = loadtxt('reconstructions_000.dat')
+fname = 'reconstruction_10000'
+
+dat = loadtxt('reconstructions_10000.dat')
 figure()
 clf()
-plot(dat[:,0],dat[:,1], '.', label = 'Values')
-plot(dat[:,0],dat[:,2], '-', label = 'Reconstruction')
+T = 5e5
+m_p = 1.67262158e-27
+k_B = 1.3806503e-23
+f = 1.0e6 * (m_p / (2.0 * pi * k_B * T)) ** 1.5 * exp(-m_p * dat[:,0] ** 2 / (2.0 * k_B * T))
+#f = 1.0e6 * (m_p / (2.0 * pi * k_B * T)) ** 1.5 * (
+#    exp(-m_p * dat[:,0] ** 2 / (2.0 * k_B * T)) +
+#    exp(-m_p * (dat[:,0] + 2e5) ** 2 / (2.0 * k_B * T)))
+rc('axes', prop_cycle = cycler('color', ['c','m','y','k']))
+plot(dat[:,0],f       , '-', lw = 2, label = 'Maxwellian')
+plot(dat[:,0],dat[:,1], '-', lw = 2, label = 'Volume Average')
+plot(dat[:,0],dat[:,2], '-', lw = 2, label = 'Reconstruction')
 imax = find(dat[:,1] == max(dat[:,1]))
 vmax = dat[imax[0],0]
 grid(True)
 legend()
 xlim(vmax-5e5,vmax+5e5)
-savefig('reconstruction.png')
-savefig('reconstruction.eps')
+savefig(fname+'.png')
+savefig(fname+'.eps')
 show()
