9bfbe4f93f7215fce33d40436c954a11547adce0
tkoskela <tskoskela@gmail.com>
2018-05-25 16:04:55 +0300

    Some writes added for debugging, commented out

--- vlasovsolver/cpu_1d_ppm_nonuniform.hpp
+++ vlasovsolver/cpu_1d_ppm_nonuniform.hpp
@@ -43,6 +43,10 @@ inline void compute_ppm_coeff_nonuniform(const Vec * const dv, const Vec * const
    //Coella et al, check for monotonicity   
    Vec m_face = fv_l;
    Vec p_face = fv_r;
+
+   //std::cout << "value = " << values[k][0] << ", m_face = " << m_face[0] << ", p_face = " << p_face[0] << "\n";
+   //std::cout << values[k][0] - m_face[0] << ", " << values[k][0] - p_face[0] << "\n";
+   
    m_face = select((p_face - m_face) * (values[k] - 0.5 * (m_face + p_face)) >
                    (p_face - m_face)*(p_face - m_face) * one_sixth,
                    3 * values[k] - 2 * p_face,
@@ -58,6 +62,11 @@ inline void compute_ppm_coeff_nonuniform(const Vec * const dv, const Vec * const
    a[0] = m_face;
    a[1] = 3.0 * values[k] - 2.0 * m_face - p_face;
    a[2] = (m_face + p_face - 2.0 * values[k]);
+
+   //std::cout << "value = " << values[k][0] << ", m_face = " << m_face[0] << ", p_face = " << p_face[0] << "\n";
+   //std::cout << values[k][0] - m_face[0] << ", " << values[k][0] - p_face[0] << "\n";
+
+   //std::cout << values[k][0] << " " << m_face[0] << " " << p_face[0] << "\n";
 }
 
 #endif
