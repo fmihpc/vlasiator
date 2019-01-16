c65662ffbfddd5bdf5f575ccf223f6e5a864c097
tkoskela <tskoskela@gmail.com>
2019-01-16 11:18:09 +0200

    Added refine call to Magnetosphere project

--- projects/Magnetosphere/Magnetosphere.h
+++ projects/Magnetosphere/Magnetosphere.h
@@ -63,6 +63,7 @@ namespace projects {
                            creal& dvx, creal& dvy, creal& dvz,
                            const uint popID
                           ) const;
+      bool refineSpatialCells( dccrg::Dccrg<spatial_cell::SpatialCell,dccrg::Cartesian_Geometry>& mpiGrid ) const;
       virtual void calcCellParameters(spatial_cell::SpatialCell* cell,creal& t);
       virtual std::vector<std::array<Real, 3> > getV0(
                                                       creal x,
