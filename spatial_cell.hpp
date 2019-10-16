b48d566eff6982b2a8e7c72b7a6304c9d1dd2eb1
Urs Ganse <urs.ganse@helsinki.fi>
2019-10-16 10:19:35 +0300

    Squelch some PGI compiler warnings, add modules to makefile

--- spatial_cell.hpp
+++ spatial_cell.hpp
@@ -983,10 +983,10 @@ namespace spatial_cell {
                                                                 const int& i_cell,const int& j_cell,const int& k_cell) {
       uint8_t ref = refLevel;
 
-      vmesh::LocalID i_child,j_child,k_child;
-      i_child = 2*i_child + i_cell/2;
-      j_child = 2*j_child + j_cell/2;
-      k_child = 2*k_child + k_cell/2;
+      //vmesh::LocalID i_child,j_child,k_child;
+      //i_child = 2*i_child + i_cell/2;
+      //j_child = 2*j_child + j_cell/2;
+      //k_child = 2*k_child + k_cell/2;
 
       while (ref != populations[popID].vmesh.getMaxAllowedRefinementLevel()) {
          vmesh::LocalID i_child,j_child,k_child;
@@ -1599,7 +1599,7 @@ namespace spatial_cell {
     functions of the containers in spatial cell
     */
    inline uint64_t SpatialCell::get_cell_memory_size() {
-      const uint64_t VEL_BLOCK_SIZE = 2*WID3*sizeof(Realf) + BlockParams::N_VELOCITY_BLOCK_PARAMS*sizeof(Real);
+      //const uint64_t VEL_BLOCK_SIZE = 2*WID3*sizeof(Realf) + BlockParams::N_VELOCITY_BLOCK_PARAMS*sizeof(Real);
       uint64_t size = 0;
       size += vmeshTemp.sizeInBytes();
       size += blockContainerTemp.sizeInBytes();
@@ -1623,7 +1623,7 @@ namespace spatial_cell {
     the size() functions of the containers in spatial cell
     */
    inline uint64_t SpatialCell::get_cell_memory_capacity() {
-      const uint64_t VEL_BLOCK_SIZE = 2*WID3*sizeof(Realf) + BlockParams::N_VELOCITY_BLOCK_PARAMS*sizeof(Real);
+      //const uint64_t VEL_BLOCK_SIZE = 2*WID3*sizeof(Realf) + BlockParams::N_VELOCITY_BLOCK_PARAMS*sizeof(Real);
       uint64_t capacity = 0;
       
       capacity += vmeshTemp.capacityInBytes();
--- vlasovsolver/cpu_acc_map.cpp
+++ vlasovsolver/cpu_acc_map.cpp
@@ -21,7 +21,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include <cmath>
+#include <math.h>
 #include <algorithm>
 #include <utility>
 
