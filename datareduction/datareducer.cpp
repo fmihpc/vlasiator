8a237940fb3498bbd352f8a27f0fb441d043b48a
Yann Pfau-Kempf <yann.kempf@helsinki.fi>
2019-11-14 14:45:50 +0200

    Removed extraneous +.

--- datareduction/datareducer.cpp
+++ datareduction/datareducer.cpp
@@ -634,7 +634,7 @@ void initializeDataReducers(DataReducer * outputReducer, DataReducer * diagnosti
                   for(int z=0; z<gridSize[2]; z++) {
                      for(int y=0; y<gridSize[1]; y++) {
                         for(int x=0; x<gridSize[0]; x++) {
-                           retval[gridSize[1]*gridSize[0]*z + gridSize[0]*y + x] =  + (*EHallGrid.get(x,y,z))[index];
+                           retval[gridSize[1]*gridSize[0]*z + gridSize[0]*y + x] = (*EHallGrid.get(x,y,z))[index];
                         }
                      }
                   }
