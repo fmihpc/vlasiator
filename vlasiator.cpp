d367d8d84163acf0be939cb6950e5c72190ae1ff
Markus Battarbee <markus.battarbee@gmail.com>
2020-03-11 15:12:26 +0200

    Re-implemented check for cubical cells

--- vlasiator.cpp
+++ vlasiator.cpp
@@ -433,6 +433,13 @@ int main(int argn,char* args[]) {
       = momentsDt2Grid.physicalGlobalStart = dPerBGrid.physicalGlobalStart = dMomentsGrid.physicalGlobalStart
       = BgBGrid.physicalGlobalStart = volGrid.physicalGlobalStart = technicalGrid.physicalGlobalStart
       = {P::xmin, P::ymin, P::zmin};
+
+   // Checking that spatial cells are cubic, otherwise field solver is incorrect (cf. derivatives in E, Hall term)
+   if((abs((technicalGrid.DX-technicalGrid.DY)/technicalGrid.DX) > 0.001) ||
+      (abs((technicalGrid.DX-technicalGrid.DZ)/technicalGrid.DX) > 0.001) ||
+      (abs((technicalGrid.DY-technicalGrid.DZ)/technicalGrid.DY) > 0.001)) {
+     std::cerr << "WARNING: Your spatial cells seem not to be cubic. However the field solver is assuming them to be. Use at your own risk and responsibility!" << std::endl;
+   }
    phiprof::stop("Init fieldsolver grids");
    
    // Initialize grid.  After initializeGrid local cells have dist
