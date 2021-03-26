4456493c49c44bfe81381aabc1a876f8edbfc482
Urs Ganse <urs.ganse@helsinki.fi>
2021-03-26 13:53:30 +0200

    Fix another typo in upmappedArea calculation

--- sysboundary/ionosphere.cpp
+++ sysboundary/ionosphere.cpp
@@ -1371,7 +1371,7 @@ namespace SBC {
               std::array<Real, 3> avgB = {(B[0][0] + B[1][0] + B[2][0])/3.,
                  (B[0][1] + B[1][1] + B[2][1]) / 3.,
                  (B[0][2] + B[1][2] + B[2][2]) / 3.};
-              upmappedArea += fabs(areaVector[0] * avgB[0] + areaVector[1]*avgB[1] + areaVector[2]+avgB[2]) /
+              upmappedArea += fabs(areaVector[0] * avgB[0] + areaVector[1]*avgB[1] + areaVector[2]*avgB[2]) /
                  sqrt(avgB[0]*avgB[0] + avgB[1]*avgB[1] + avgB[2]*avgB[2]);
            }
 
