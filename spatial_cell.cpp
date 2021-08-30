54e70dcaaa661d2110c6e0d35f4c547985fe30e3
Leo Kotipalo <lkotipal@helsinki.fi>
2021-08-30 12:13:15 +0300

    Communicate coordinates in CELL_DIMENSIONS

--- spatial_cell.cpp
+++ spatial_cell.cpp
@@ -671,8 +671,8 @@ namespace spatial_cell {
          
          // send  spatial cell dimensions
          if ((SpatialCell::mpi_transfer_type & Transfer::CELL_DIMENSIONS)!=0){
-            displacements.push_back((uint8_t*) &(this->parameters[CellParams::DX]) - (uint8_t*) this);
-            block_lengths.push_back(sizeof(Real) * 3);
+            displacements.push_back((uint8_t*) &(this->parameters[CellParams::XCRD]) - (uint8_t*) this);
+            block_lengths.push_back(sizeof(Real) * 6);
          }
                   
          // send  BGBXVOL BGBYVOL BGBZVOL PERBXVOL PERBYVOL PERBZVOL
