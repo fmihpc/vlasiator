529e3fdbf2fb85f457065a3747bee422aafa8850
Hongyang Zhou <hyzhou@umich.edu>
2021-09-05 21:10:56 +0300

    Turn on updateState call for BC

--- vlasiator.cpp
+++ vlasiator.cpp
@@ -880,9 +880,9 @@ int main(int argn,char* args[]) {
       }
       phiprof::stop("Spatial-space",computedCells,"Cells");
       
-      // phiprof::start("Update dynamic boundary cells");
-      // boundaries.updateState(mpiGrid, perBGrid, P::t + 0.5 * P::dt);
-      // phiprof::stop("Update dynamic boundary cells");
+      phiprof::start("Update dynamic boundary cells");
+      sysBoundaries.updateState(mpiGrid, perBGrid, P::t + 0.5 * P::dt);
+      phiprof::stop("Update dynamic boundary cells");
 
       // Apply boundary conditions
       if (P::propagateVlasovTranslation || P::propagateVlasovAcceleration ) {
