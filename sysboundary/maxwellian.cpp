557a085e091b04f4e593e5a6adb73ebccc9f531c
Hongyang Zhou <hyzhou@umich.edu>
2021-10-15 14:43:56 +0300

    Remove blanks in comments

--- sysboundary/maxwellian.cpp
+++ sysboundary/maxwellian.cpp
@@ -122,10 +122,10 @@ Real Maxwellian::maxwellianDistribution(const uint popID, creal& rho, creal& T, 
           exp(-MASS * (vx * vx + vy * vy + vz * vz) / (2.0 * physicalconstants::K_B * T));
 }
 
-/*  Here the while loop iterates  from the centre of the maxwellian in blocksize (4*dvx) increments,
+/*  Here the while loop iterates from the centre of the maxwellian in blocksize (4*dvx) increments,
  *  and looks at the centre of the first velocity cell in the block (+0.5dvx), checking if the
  *  phase-space density there is large enough to be included due to sparsity threshold.
- *  That results in a "blocks radius"  vRadiusSquared from the centre of the maxwellianDistribution.
+ *  That results in a "blocks radius" vRadiusSquared from the centre of the maxwellianDistribution.
  *  Then we iterate through the actual blocks and calculate their radius R2 based on their velocity coordinates
  *  and the plasma bulk velocity. Blocks that fullfil R2<vRadiusSquared are included to blocksToInitialize.
  */
