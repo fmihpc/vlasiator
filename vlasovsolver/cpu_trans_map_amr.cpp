23d959c270867c4a8ea44b07543da0ad7b170a77
ykempf <yann.kempf@helsinki.fi>
2021-12-14 15:22:40 +0200

    Indexing fix in translation check function.

--- vlasovsolver/cpu_trans_map_amr.cpp
+++ vlasovsolver/cpu_trans_map_amr.cpp
@@ -697,7 +697,7 @@ setOfPencils buildPencilsWithNeighbors( const dccrg::Dccrg<SpatialCell,dccrg::Ca
 }
 
 bool check_skip_remapping(Vec* values) {
-   for (int index=-VLASOV_STENCIL_WIDTH; index<VLASOV_STENCIL_WIDTH+1; ++index) {
+   for (int index=0; index<2*VLASOV_STENCIL_WIDTH+1; ++index) {
       if (horizontal_or(values[index] > Vec(0))) return false;
    }
    return true;
