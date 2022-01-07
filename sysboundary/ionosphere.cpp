eb89e888efd7bdb35cb790808ab841da0bf2224e
Markus Battarbee <markus.battarbee@gmail.com>
2022-01-07 15:42:31 +0200

    uncapitalized get

--- sysboundary/ionosphere.cpp
+++ sysboundary/ionosphere.cpp
@@ -99,7 +99,7 @@ namespace SBC {
         if(sP.T == 0) {
            // If the user does *not* specify a temperature, it defaults to the ambient magnetospheric temperature
            // (compare the corresponding handling in projects/Magnetosphere/Magnetosphere.cpp)
-           Readparameters::Get(pop + "_Magnetosphere.T", sP.T);
+           Readparameters::get(pop + "_Magnetosphere.T", sP.T);
         }
         Readparameters::get(pop + "_Magnetosphere.nSpaceSamples", sP.nSpaceSamples);
         Readparameters::get(pop + "_Magnetosphere.nVelocitySamples", sP.nVelocitySamples);
