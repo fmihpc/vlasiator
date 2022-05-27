f907e9c779bb49296c119f012b28fcb4a6cc9d85
Leo Kotipalo <lkotipal@helsinki.fi>
2022-05-27 21:16:37 +0300

    Less stringent boundary check

--- projects/Magnetosphere/Magnetosphere.cpp
+++ projects/Magnetosphere/Magnetosphere.cpp
@@ -518,7 +518,7 @@ namespace projects {
    }
 
    bool Magnetosphere::canRefine(spatial_cell::SpatialCell* cell) const {
-      return cell->sysBoundaryLayer == 0;
+      return (cell->sysBoundaryLayer == 0 || cell->sysBoundaryLayer > 2);
    }
 
    bool Magnetosphere::refineSpatialCells( dccrg::Dccrg<spatial_cell::SpatialCell,dccrg::Cartesian_Geometry>& mpiGrid ) const {
