7a1e878343df03724b22ad1746b3b052419ef14e
Urs Ganse <urs.ganse@helsinki.fi>
2022-06-05 17:18:06 +0300

    Properly quote backslash in ig_precipavgenergy latex units

--- datareduction/datareducer.cpp
+++ datareduction/datareducer.cpp
@@ -2869,7 +2869,7 @@ void initializeDataReducers(DataReducer * outputReducer, DataReducer * diagnosti
                      }
                      return retval;
                      }));
-         outputReducer->addMetadata(outputReducer->size()-1, "eV", "eV", "$\bar{E}_\mathrm{precip}$", "1.0");
+         outputReducer->addMetadata(outputReducer->size()-1, "eV", "eV", "$\\bar{E}_\mathrm{precip}$", "1.0");
          continue;
       }
       if(lowercase == "ig_potential") {
