b20c5b5d2bb16250e9a4deec2e0b591ddc7d266d
Hongyang Zhou <hyzhou@umich.edu>
2022-08-10 20:07:25 +0300

    warning on master only

--- vlasiator.cpp
+++ vlasiator.cpp
@@ -450,10 +450,14 @@ int main(int argn,char* args[]) {
       = {P::xmin, P::ymin, P::zmin};
 
    // Checking that spatial cells are cubic, otherwise field solver is incorrect (cf. derivatives in E, Hall term)
-   if((abs((technicalGrid.DX-technicalGrid.DY)/technicalGrid.DX) > 0.001) ||
-      (abs((technicalGrid.DX-technicalGrid.DZ)/technicalGrid.DX) > 0.001) ||
-      (abs((technicalGrid.DY-technicalGrid.DZ)/technicalGrid.DY) > 0.001)) {
-     std::cerr << "WARNING: Your spatial cells seem not to be cubic. However the field solver is assuming them to be. Use at your own risk and responsibility!" << std::endl;
+   if ((abs((technicalGrid.DX - technicalGrid.DY) / technicalGrid.DX) > 0.001) ||
+       (abs((technicalGrid.DX - technicalGrid.DZ) / technicalGrid.DX) > 0.001) ||
+       (abs((technicalGrid.DY - technicalGrid.DZ) / technicalGrid.DY) > 0.001)) {
+      if (myRank == MASTER_RANK) {
+         std::cerr << "WARNING: Your spatial cells seem not to be cubic. However the field solver is assuming them to "
+                      "be. Use at your own risk and responsibility!"
+                   << std::endl;
+      }
    }
    phiprof::stop("Init fieldsolver grids");
 
