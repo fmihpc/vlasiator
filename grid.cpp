1334b2e7896689b2d284388fddd0b6249a72e22b
Urs Ganse <urs.ganse@helsinki.fi>
2022-10-28 12:11:06 +0300

    Skip non existent spatial cells in shrink to fit

--- grid.cpp
+++ grid.cpp
@@ -715,10 +715,14 @@ void shrink_to_fit_grid_data(dccrg::Dccrg<SpatialCell,dccrg::Cartesian_Geometry>
    const std::vector<CellID> remote_cells = mpiGrid.get_remote_cells_on_process_boundary(FULL_NEIGHBORHOOD_ID);
    #pragma omp parallel for
    for(size_t i=0; i<cells.size() + remote_cells.size(); ++i) {
-      if(i < cells.size())
+      if(i < cells.size()) {
          mpiGrid[cells[i]]->shrink_to_fit();
-      else
-         mpiGrid[remote_cells[i - cells.size()]]->shrink_to_fit();
+      } else {
+        if(mpiGrid[remote_cells[i - cells.size()]] == nullptr) {
+           continue;
+        }
+        mpiGrid[remote_cells[i - cells.size()]]->shrink_to_fit();
+      }
    }
 }
 
