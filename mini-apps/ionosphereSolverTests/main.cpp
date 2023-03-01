967758831ebf3ff83fc364874a315daa561e4733
Urs Ganse <urs.ganse@helsinki.fi>
2023-03-01 14:58:28 +0200

    Fix ionosphereMiniApp help output.
    Equatorial pinning options were largely undocumented

--- mini-apps/ionosphereSolverTests/main.cpp
+++ mini-apps/ionosphereSolverTests/main.cpp
@@ -148,7 +148,7 @@ int main(int argc, char** argv) {
       }
       cerr << "Unknown command line option \"" << argv[i] << "\"" << endl;
       cerr << endl;
-      cerr << "main [-N num] [-r <lat0> <lat1>] [-sigma (identity|random|35|53|file)] [-fac (constant|dipole|quadrupole|octopole|hexadecapole||file)] [-facfile <filename>] [-gaugeFix equator|pole|integral|none] [-np]" << endl;
+      cerr << "main [-N num] [-r <lat0> <lat1>] [-sigma (identity|random|35|53|file)] [-fac (constant|dipole|quadrupole|octopole|hexadecapole||file)] [-facfile <filename>] [-gaugeFix equator|equator40|equator60|pole|integral|none] [-np]" << endl;
       cerr << "Paramters:" << endl;
       cerr << " -N:        Number of ionosphere mesh nodes (default: 64)" << endl;
       cerr << " -r:        Refine grid between the given latitudes (can be specified multiple times)" << endl;
@@ -170,6 +170,11 @@ int main(int argc, char** argv) {
       cerr << "            file       - read FAC distribution from vlsv input file" << endl;
       cerr << " -infile:   Read FACs from this input file" << endl;
       cerr << " -gaugeFix: Solver gauge fixing method (default: pole)" << endl;
+      cerr << "            options are:" << endl;
+      cerr << "            pole      - Fix potential in a single node at the north pole" << endl;
+      cerr << "            equator   - Fix potential on all nodes +- 10 degrees of the equator" << endl;
+      cerr << "            equator40 - Fix potential on all nodes +- 40 degrees of the equator" << endl;
+      cerr << "            equator60 - Fix potential on all nodes +- 60 degrees of the equator" << endl;
       cerr << " -np:       DON'T use the matrix preconditioner (default: do)" << endl;
       cerr << " -maxIter:  Maximum number of solver iterations" << endl;
       
