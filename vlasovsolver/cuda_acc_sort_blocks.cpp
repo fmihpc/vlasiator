d7f7c4be77550bf02f69a334f1b566869193d4a1
Markus Battarbee <markus.battarbee@gmail.com>
2023-03-13 14:31:50 +0200

    Reverted to sort-based column construction but now with thrust::sort

--- vlasovsolver/cuda_acc_sort_blocks.cpp
+++ vlasovsolver/cuda_acc_sort_blocks.cpp
@@ -41,11 +41,6 @@
 using namespace std;
 using namespace spatial_cell;
 
-// Comparator function for sorting vector of pairs
-inline bool tripletcomparator( const std::pair<std::pair<uint, uint>, uint> & l, const std::pair<std::pair<uint, uint>, uint> & r ) {
-   return l.first.first < r.first.first;
-}
-
 // Kernels for converting GIDs to dimension-sorted indices
 __global__ void blocksID_mapped_dim0_kernel(
    const vmesh::VelocityMesh* vmesh,
@@ -212,7 +207,6 @@ void sortBlocklistByDimension( //const spatial_cell::SpatialCell* spatial_cell,
    columnData->setColumnOffsets.clear();
    columnData->setNumColumns.clear();
 
-#ifndef UNDEFINED
    //thrust::sort(thrust::device, data, data + size);
    // thrust::sort_by_key(keys, keys + N, values);
    // auto dptr = thrust::device_ptr<uint64_t>(data);
@@ -244,7 +238,8 @@ void sortBlocklistByDimension( //const spatial_cell::SpatialCell* spatial_cell,
          cudaStreamSynchronize(stream);
          thrust::device_ptr<vmesh::GlobalID> dptrMapped(blocksID_mapped);
          thrust::device_ptr<vmesh::LocalID> dptrLID(blocksLID);
-         thrust::sort_by_key(dptrMapped, dptrMapped + nBlocks, dptrLID);
+         thrust::sort_by_key(thrust::cuda::par(stream), dptrMapped, dptrMapped + nBlocks, dptrLID);
+         //thrust::sort_by_key(dptrMapped, dptrMapped + nBlocks, dptrLID);
          //cudaStreamSynchronize(stream);
          cudaDeviceSynchronize();
          break;
@@ -261,10 +256,10 @@ void sortBlocklistByDimension( //const spatial_cell::SpatialCell* spatial_cell,
          // Sort (with thrust)
          // vmesh::GlobalID *dptrMapped = thrust::device_ptr<vmesh::GlobalID>(blocksID_mapped);
          // vmesh::LocalID *dptrLID = thrust::device_ptr<vmesh::LocalID>(blocksLID);
-         //thrust::sort_by_key(thrust::cuda::par(stream), dptrMapped, dptrMapped + nBlocks, dptrLID);
-         thrust::device_ptr<vmesh::GlobalID> dptrMapped(blocksID_mapped);
+                  thrust::device_ptr<vmesh::GlobalID> dptrMapped(blocksID_mapped);
          thrust::device_ptr<vmesh::LocalID> dptrLID(blocksLID);
-         thrust::sort_by_key(dptrMapped, dptrMapped + nBlocks, dptrLID);
+         //thrust::sort_by_key(dptrMapped, dptrMapped + nBlocks, dptrLID);
+         thrust::sort_by_key(thrust::cuda::par(stream), dptrMapped, dptrMapped + nBlocks, dptrLID);
          //cudaStreamSynchronize(stream);
          cudaDeviceSynchronize();
          // static CUB_RUNTIME_FUNCTION cudaError_t cub::DeviceRadixSort::SortPairs
@@ -294,10 +289,10 @@ void sortBlocklistByDimension( //const spatial_cell::SpatialCell* spatial_cell,
          // Sort (with thrust)
          // vmesh::GlobalID *dptrMapped = thrust::device_ptr<vmesh::GlobalID>(blocksID_mapped);
          // vmesh::LocalID *dptrLID = thrust::device_ptr<vmesh::LocalID>(blocksLID);
-         //thrust::sort_by_key(thrust::cuda::par(stream), dptrMapped, dptrMapped + nBlocks, dptrLID);
          thrust::device_ptr<vmesh::GlobalID> dptrMapped(blocksID_mapped);
          thrust::device_ptr<vmesh::LocalID> dptrLID(blocksLID);
-         thrust::sort_by_key(dptrMapped, dptrMapped + nBlocks, dptrLID);
+         //thrust::sort_by_key(dptrMapped, dptrMapped + nBlocks, dptrLID);
+         thrust::sort_by_key(thrust::cuda::par(stream), dptrMapped, dptrMapped + nBlocks, dptrLID);
          //cudaStreamSynchronize(stream);
          cudaDeviceSynchronize();
          break;
@@ -305,8 +300,13 @@ void sortBlocklistByDimension( //const spatial_cell::SpatialCell* spatial_cell,
       default:
          printf("Incorrect dimension in cuda_acc_sort_blocks.cpp\n");
    }
-   // Gather GIDs in order
-   order_GIDs_kernel<<<grid, block, 0, stream>>> (
+   // We can now do two things in parallel.
+   // Need to create a new stream for this.
+   cudaStream_t GIDorderstream;
+   cudaStreamCreate(&GIDorderstream);
+
+// Gather GIDs in order
+   order_GIDs_kernel<<<grid, block, 0, GIDorderstream>>> (
       vmesh,
       blocksLID,
       blocksGID,
@@ -325,127 +325,9 @@ void sortBlocklistByDimension( //const spatial_cell::SpatialCell* spatial_cell,
       nBlocks
       );
 
-   // And then we synchronize
+   cudaStreamSynchronize(GIDorderstream);
+   cudaStreamDestroy(GIDorderstream);
+   // And then we synchronize columns as well
    cudaStreamSynchronize(stream);
-   
-#else
-
-   //const uint nBlocks = spatial_cell->get_number_of_velocity_blocks(); // Number of blocks
-   const vmesh::LocalID nBlocks = vmesh->size();
-
-   // Velocity mesh refinement level, has no effect here
-   // but is needed in some vmesh::VelocityMesh function calls.
-   const uint8_t REFLEVEL = 0;
-   // Copy block data to vector
-   std::vector<std::pair<std::pair<vmesh::GlobalID,vmesh::GlobalID>,vmesh::LocalID> > block_triplets;
-   block_triplets.resize( nBlocks );
-   for (vmesh::LocalID i = 0; i < nBlocks; ++i ) {
-      //const vmesh::GlobalID block = spatial_cell->get_velocity_block_global_id(i);
-      const vmesh::GlobalID block = vmesh->getGlobalID(i);
-      switch( dimension ) {
-       case 0: {
-          const vmesh::GlobalID blockId_mapped = block; // Mapping the block id to different coordinate system if dimension is not zero:
-          block_triplets[i] = std::make_pair( std::make_pair( blockId_mapped, block ), i);
-       }
-         break;
-       case 1: {
-          // Do operation:
-          //   block = x + y*x_max + z*y_max*x_max
-          //=> block' = block - (x + y*x_max) + y + x*y_max = x + y*x_max + z*y_max*x_max - (x + y*x_max) + y + x*y_max
-          //          = y + x*y_max + z*y_max*x_max
-          //const uint x_indice = block%SpatialCell::get_velocity_grid_length()[0];
-          //const uint y_indice = (block/SpatialCell::get_velocity_grid_length()[0])%SpatialCell::SpatialCell::get_velocity_grid_length()[1];
-          const vmesh::LocalID x_index = block % vmesh->getGridLength(REFLEVEL)[0];
-          const vmesh::LocalID y_index = (block / vmesh->getGridLength(REFLEVEL)[0]) % vmesh->getGridLength(REFLEVEL)[1];
-
-          // Mapping the block id to different coordinate system if dimension is not zero:
-          //const uint blockId_mapped
-          //        = block - (x_indice + y_indice*SpatialCell::get_velocity_grid_length()[0])
-          //        + y_indice
-          //        + x_indice * SpatialCell::SpatialCell::get_velocity_grid_length()[1];
-          const vmesh::GlobalID blockId_mapped
-                  = block - (x_index + y_index*vmesh->getGridLength(REFLEVEL)[0])
-                  + y_index
-                  + x_index * vmesh->getGridLength(REFLEVEL)[1];
-          block_triplets[i] = std::make_pair( std::make_pair( blockId_mapped, block ), i);
-       }
-         break;
-       case 2: {
-          // Do operation:
-          //   block = x + y*x_max + z*y_max*x_max
-          //=> block' = z + y*z_max + x*z_max*y_max
-          //const uint x_indice = block%SpatialCell::get_velocity_grid_length()[0];
-          //const uint y_indice = (block/SpatialCell::get_velocity_grid_length()[0])%SpatialCell::SpatialCell::get_velocity_grid_length()[1];
-          //const uint z_indice =  (block/(SpatialCell::get_velocity_grid_length()[0]*SpatialCell::SpatialCell::get_velocity_grid_length()[1]));
-          const vmesh::LocalID x_index = block % vmesh->getGridLength(REFLEVEL)[0];
-          const vmesh::LocalID y_index = (block / vmesh->getGridLength(REFLEVEL)[0]) % vmesh->getGridLength(REFLEVEL)[1];
-          const vmesh::LocalID z_index = (block / (vmesh->getGridLength(REFLEVEL)[0]*vmesh->getGridLength(REFLEVEL)[1]));
-
-          // Mapping the block id to different coordinate system if dimension is not zero:
-          //const uint blockId_mapped
-          //  = z_indice
-          //  + y_indice * SpatialCell::SpatialCell::get_velocity_grid_length()[2]
-          //  + x_indice*SpatialCell::SpatialCell::get_velocity_grid_length()[1]*SpatialCell::SpatialCell::get_velocity_grid_length()[2];
-          const vmesh::GlobalID blockId_mapped
-            = z_index
-            + y_index*vmesh->getGridLength(REFLEVEL)[2]
-            + x_index*vmesh->getGridLength(REFLEVEL)[1]*vmesh->getGridLength(REFLEVEL)[2];
-          block_triplets[i] = std::make_pair(std::make_pair( blockId_mapped, block ), i);
-       }
-         break;
-      }
-   }
-   // Sort the list:
-   std::sort( block_triplets.begin(), block_triplets.end(), tripletcomparator );
-
-   // Put in the sorted blocks, and also compute column offsets and lengths:
-   columnData->columnBlockOffsets.push_back(0); //first offset
-   columnData->setColumnOffsets.push_back(0); //first offset
-   uint prev_column_id, prev_dimension_id;
-
-   for (vmesh::LocalID i=0; i<nBlocks; ++i) {
-       // identifies a particular column
-      vmesh::LocalID column_id = block_triplets[i].first.first / vmesh->getGridLength(REFLEVEL)[dimension];
-
-       // identifies a particular block in a column (along the dimension)
-       vmesh::LocalID dimension_id = block_triplets[i].first.first % vmesh->getGridLength(REFLEVEL)[dimension];
-
-       //sorted lists
-       blocksGID[i] = block_triplets[i].first.second;
-       blocksLID[i] = vmesh->getLocalID(block_triplets[i].first.second);
-
-       if ( i > 0 &&  ( column_id != prev_column_id || dimension_id != (prev_dimension_id + 1) )){
-         //encountered new column! For i=0, we already entered the correct offset (0).
-         //We also identify it as a new column if there is a break in the column (e.g., gap between two populations)
-         /*add offset where the next column will begin*/
-         columnData->columnBlockOffsets.push_back(i);
-         /*add length of the current column that now ended*/
-         columnData->columnNumBlocks.push_back(columnData->columnBlockOffsets[columnData->columnBlockOffsets.size()-1] - columnData->columnBlockOffsets[columnData->columnBlockOffsets.size()-2]);
-
-         if (column_id != prev_column_id ){
-            //encountered new set of columns, add offset to new set starting at present column
-            columnData->setColumnOffsets.push_back(columnData->columnBlockOffsets.size() - 1);
-            /*add length of the previous column set that ended*/
-            columnData->setNumColumns.push_back(columnData->setColumnOffsets[columnData->setColumnOffsets.size()-1] - columnData->setColumnOffsets[columnData->setColumnOffsets.size()-2]);
-         }
-      }
-      prev_column_id = column_id;
-      prev_dimension_id = dimension_id;
-   }
-
-   columnData->columnNumBlocks.push_back(nBlocks - columnData->columnBlockOffsets[columnData->columnBlockOffsets.size()-1]);
-   columnData->setNumColumns.push_back(columnData->columnNumBlocks.size() - columnData->setColumnOffsets[columnData->setColumnOffsets.size()-1]);
-
-#endif
-    printf("\n Output for dimension %d ",dimension);
-    printf("\nColumnBlockOffsets %d\n", columnData->columnBlockOffsets.size());
-   // for (auto i : columnData->columnBlockOffsets) printf("%d ",i);
-    printf("\ncolumnNumBlocks %d\n", columnData->columnNumBlocks.size());
-   // for (auto i : columnData->columnNumBlocks) printf("%d ",i);
-    printf("\nsetColumnOffsets %d\n", columnData->setColumnOffsets.size());
-   // for (auto i : columnData->setColumnOffsets) printf("%d ",i);
-    printf("\nsetNumColumns %d\n", columnData->setNumColumns.size());
-   // for (auto i : columnData->setNumColumns) printf("%d ",i);
-    printf("\n \n",dimension);
 
 }
