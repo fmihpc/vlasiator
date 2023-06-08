41027a3aaa293291ebc3e7423a9df708872b69c3
Urs Ganse <urs.ganse@helsinki.fi>
2023-06-08 13:07:01 +0300

    Build testpackage definition automatically from directories.
    Every test should now have a test_spec.sh shell fragment that adds to
    the corresponding shell arrays (the preexisting ones have been added
    appropriately)

---
+++ testpackage/tests/acctest_4_helium/test_spec.sh
@@ -0,0 +1,5 @@
+comparison_vlsv[$num_tests]="fullf.0000001.vlsv"
+comparison_phiprof[$num_tests]="phiprof_0.txt"
+variable_names[$num_tests]="helium/vg_rho helium/vg_v helium/vg_v helium/vg_v"
+variable_components[$num_tests]="0 0 1 2"
+single_cell[$num_tests]=1
---
+++ testpackage/tests/test_fp_fsolver_only_3D/test_spec.sh
@@ -0,0 +1,4 @@
+comparison_vlsv[$num_tests]="fullf.0000001.vlsv"
+comparison_phiprof[$num_tests]="phiprof_0.txt"
+variable_names[$num_tests]="fg_b fg_b fg_b fg_e fg_e fg_e"
+variable_components[$num_tests]="0 1 2 0 1 2"
