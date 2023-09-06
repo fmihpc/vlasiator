49f3a8d1847cafd54a01ab1f826a525033c1b374
Jaro Hokkanen <jaro.hokkanen@csc.fi>
2023-09-06 13:42:19 +0300

    Add runtime check for the sufficient shared memory

--- arch/arch_device_cuda.h
+++ arch/arch_device_cuda.h
@@ -93,6 +93,7 @@
 
 /* Set CUDA blocksize used for reductions */
 #define ARCH_BLOCKSIZE_R 512
+#define ARCH_BLOCKSIZE_R_SMALL 64
 
 /* GPU blocksize used by Vlasov solvers */
 #ifndef GPUBLOCKS
@@ -394,10 +395,6 @@ namespace arch{
       for(uint i = 0; i < NDim; i++)
          n_total *= limits[i];
 
-      /* Set the kernel dimensions */
-      const uint blocksize = ARCH_BLOCKSIZE_R;
-      const uint gridsize = (n_total - 1 + blocksize) / blocksize;
-
       /* Check the CUDA default mempool settings and correct if wrong */
       device_mempool_check(UINT64_MAX);
 
@@ -426,22 +423,50 @@ namespace arch{
       /* Call the reduction kernel with different arguments depending
        * on if the number of reductions is known at the compile time
        */
-      T* d_thread_data_dynamic;
+      T* d_thread_data_dynamic = 0; // declared zero to suppress unitialized use warning
       if(NReduStatic == 0) {
-         /* Get the cub temp storage size for the dynamic shared memory kernel argument */
-         constexpr auto cub_temp_storage_type_size = sizeof(typename cub::BlockReduce<T, ARCH_BLOCKSIZE_R, cub::BLOCK_REDUCE_RAKING_COMMUTATIVE_ONLY, 1, 1>::TempStorage);
+             /* Get the cub temp storage sizes for the dynamic shared memory kernel argument */
+    constexpr auto cub_temp_storage_type_size = sizeof(typename cub::BlockReduce<T, ARCH_BLOCKSIZE_R, cub::BLOCK_REDUCE_RAKING_COMMUTATIVE_ONLY, 1, 1>::TempStorage);
+    constexpr auto cub_temp_storage_type_size_small = sizeof(typename cub::BlockReduce<T, ARCH_BLOCKSIZE_R_SMALL, cub::BLOCK_REDUCE_RAKING_COMMUTATIVE_ONLY, 1, 1>::TempStorage);
+    /* Query device properties */
+    int device_id;
+    CHK_ERR(cudaGetDevice(&device_id));
+    cudaDeviceProp_t deviceProp;
+    CHK_ERR(cudaGetDeviceProperties(&deviceProp, device_id));
+    /* Make sure there is enough shared memory for the used block size */
+    uint blocksize;
+    size_t shared_mem_bytes_per_block_request;
+    if(n_reductions * cub_temp_storage_type_size <= deviceProp.sharedMemPerBlock){
+      blocksize = ARCH_BLOCKSIZE_R;
+      shared_mem_bytes_per_block_request = n_reductions * cub_temp_storage_type_size;
+    }
+    else if(n_reductions * cub_temp_storage_type_size_small <= deviceProp.sharedMemPerBlock){
+      blocksize = ARCH_BLOCKSIZE_R_SMALL;
+      shared_mem_bytes_per_block_request = n_reductions * cub_temp_storage_type_size_small;
+    }
+    else{
+      printf("The device %d (%s) does not have enough shared memory even for the small blocksize (%d)! The error occurred in %s at line %d\n", device_id, deviceProp.name, ARCH_BLOCKSIZE_R_SMALL, __FILE__, __LINE__);
+      exit(1);
+    }
+    /* Set the kernel grid dimensions */
+    const uint gridsize = (n_total - 1 + blocksize) / blocksize;
          /* Allocate memory for the thread data values */
          CHK_ERR(cudaMallocAsync(&d_thread_data_dynamic, n_reductions * blocksize * gridsize * sizeof(T), gpuStreamList[thread_id]));
          /* Call the kernel (the number of reductions not known at compile time) */
-         reduction_kernel<Op, NDim, 0><<<gridsize, blocksize, n_reductions * cub_temp_storage_type_size, gpuStreamList[thread_id]>>>(loop_body, d_const_buf, d_buf, d_limits, n_total, n_reductions, d_thread_data_dynamic);
+         reduction_kernel<Op, NDim, 0><<<gridsize, blocksize, shared_mem_bytes_per_block_request, gpuStreamList[thread_id]>>>(loop_body, d_const_buf, d_buf, d_limits, n_total, n_reductions, d_thread_data_dynamic);
+         /* Check for kernel launch errors */
          CHK_ERR(cudaPeekAtLastError());
          /* Synchronize and free the thread data allocation */
          CHK_ERR(cudaStreamSynchronize(gpuStreamList[thread_id]));
          CHK_ERR(cudaFreeAsync(d_thread_data_dynamic, gpuStreamList[thread_id]));
       }
       else{
+         /* Set the kernel dimensions */
+         const uint blocksize = ARCH_BLOCKSIZE_R;
+         const uint gridsize = (n_total - 1 + blocksize) / blocksize;
          /* Call the kernel (the number of reductions known at compile time) */
          reduction_kernel<Op, NDim, NReduStatic><<<gridsize, blocksize, 0, gpuStreamList[thread_id]>>>(loop_body, d_const_buf, d_buf, d_limits, n_total, n_reductions, d_thread_data_dynamic);
+         /* Check for kernel launch errors */
          CHK_ERR(cudaPeekAtLastError());
          /* Synchronize after kernel call */
          CHK_ERR(cudaStreamSynchronize(gpuStreamList[thread_id]));
