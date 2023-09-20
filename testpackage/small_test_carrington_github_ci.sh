5449801e4b9d4d83b5a8a854f4ea2a5fceaafc51
lkotipal <leo.kotipalo@helsinki.fi>
2023-09-20 13:18:40 +0300

    Learn to use bash

--- testpackage/small_test_carrington_github_ci.sh
+++ testpackage/small_test_carrington_github_ci.sh
@@ -128,8 +128,10 @@ for run in ${run_tests[*]}; do
    RUN_ERROR=${PIPESTATUS[0]}
 
    # Phiprof error means failed test
-   if [[ egrep -q 'PHIPROF-ERROR' $GITHUB_WORKSPACE/stderr.txt ]]; then
+   if grep -Fq 'PHIPROF-ERROR' $GITHUB_WORKSPACE/stderr.txt
+   then
       RUN_ERROR=1
+   fi
 
    if [[ $RUN_ERROR != 0 ]]; then
       echo -e "<details><summary>:red_circle: ${test_name[$run]}: Failed to run or died with an error.</summary>\n"  >> $GITHUB_STEP_SUMMARY
