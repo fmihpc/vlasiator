3ae14803ebecbc2269a90db3341f239c22a02886
Markus Battarbee <markus.battarbee@gmail.com>
2023-10-18 13:20:10 +0300

    Included lumi gpu testpackage script

--- testpackage/small_test_lumi_gpu.sh
+++ testpackage/small_test_lumi_gpu.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 #SBATCH --job-name=vlasi_g_tp
-#SBATCH --partition=dev-g
-##SBATCH --reservation=training_gpu
+##SBATCH --partition=dev-g
+#SBATCH --partition=small-g
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --gpus-per-node=8
 
-#SBATCH --time=02:00:00
-#SBATCH --account=project_465000538
+#SBATCH --time=24:00:00
+#SBATCH --account=project_462000358
 #SBATCH --exclusive
 #SBATCH --mem=0
 #SBATCH --hint=nomultithread
@@ -18,24 +18,23 @@
 create_verification_files=0
 
 # Folder for all reference data 
-reference_dir="/scratch/project_465000538/battarbe/testpackage/"
+reference_dir="/scratch/project_462000358/testpackage/"
 cd $SLURM_SUBMIT_DIR
 
-#bin="/scratch/project_465000538/battarbe/testpackage/vlasiator_NOMAD_WID8_tp"
-bin="/scratch/project_465000538/battarbe/testpackage/vlasiator_NOMAD2_WID4_tp"
-diffbin="/scratch/project_465000538/battarbe/testpackage/vlsvdiff_DP_NOMAD"
+bin="/scratch/project_462000358/testpackage/vlasiator_gpu_wid4_tp"
+diffbin="/scratch/project_462000358/testpackage/vlsvdiff_DP_gpu"
 
 # compare agains which revision?
 reference_revision="current"
 
 # set up GPU/CPU bindings
-cat << EOF > select_gpu
+cat << EOF > select_gpu_${SLURM_JOB_ID}
 #!/bin/bash
 export ROCR_VISIBLE_DEVICES=\$SLURM_LOCALID
 export OMP_NUM_THREADS=7
 exec \$*
 EOF
-chmod +x ./select_gpu
+chmod +x ./select_gpu_${SLURM_JOB_ID}
 # this should set the ordering correctly: "4 5 2 3 6 7 0 1"
 CPU_BIND="mask_cpu:7e000000000000,7e00000000000000"
 CPU_BIND="${CPU_BIND},7e0000,7e000000"
@@ -53,14 +52,15 @@ export OMP_PLACES=cores
 export OMP_PROC_BIND=close
 export MPICH_OFI_NIC_POLICY=GPU
 export MPICH_GPU_SUPPORT_ENABLED=1
-#Turn on managed memory paging
+# Turn off forced managed memory paging
 export HSA_XNACK=0
 # use extra threads for MPI in background
 export MPICH_ASYNC_PROGRESS=1
 
-
-#Command for running tests and diffs
-run_command="srun --cpu-bind=${CPU_BIND} ${SLURM_SUBMIT_DIR}/select_gpu "
+# Command for running tests and diffs with MPI
+# run_command="srun --cpu-bind=${CPU_BIND} ${SLURM_SUBMIT_DIR}/select_gpu "
+# No MPI testing for now
+run_command="srun -n 1 --cpu-bind=${CPU_BIND} ${SLURM_SUBMIT_DIR}/select_gpu "
 small_run_command="srun -n 1 --cpu-bind=${CPU_BIND} ${SLURM_SUBMIT_DIR}/select_gpu "
 run_command_tools="srun -n 1 --cpu-bind=${CPU_BIND} ${SLURM_SUBMIT_DIR}/select_gpu"
 
@@ -76,6 +76,7 @@ source run_tests.sh
 wait 2
 
 # hello jobstep verification
-srun --cpu-bind=${CPU_BIND} ${SLURM_SUBMIT_DIR}/select_gpu ${SLURM_SUBMIT_DIR}/hello_jobstep
-#cleanup
-rm -f ${SLURM_SUBMIT_DIR}/select_gpu
+# srun --cpu-bind=${CPU_BIND} ${SLURM_SUBMIT_DIR}/select_gpu ${SLURM_SUBMIT_DIR}/hello_jobstep
+
+# cleanup
+rm -f ${SLURM_SUBMIT_DIR}/select_gpu_${SLURM_JOB_ID}
