7d4e1f6d916c6b6049837757f2a421dc95835f46
Jaro Hokkanen <jaro.hokkanen@csc.fi>
2024-03-05 10:16:41 +0200

    Fix an undesired change from the previous commit

--- iowrite.cpp
+++ iowrite.cpp
@@ -939,8 +939,7 @@ bool writeFsGridMetadata(FsGrid< fsgrids::technical, FS_STENCIL_WIDTH> & technic
   vlsvWriter.writeArray("MESH_DOMAIN_SIZES", xmlAttributes, 1, 2, &meshDomainSize[0]);
 
   // how many MPI ranks we wrote from
-  int size;
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  int size = technicalGrid.getSize();
   vlsvWriter.writeParameter("numWritingRanks", &size);
 
   // Save the FSgrid decomposition
