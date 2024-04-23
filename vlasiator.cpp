3abd197e4b6d9e555739361a276db6655951fc81
lkotipal <leo.kotipalo@helsinki.fi>
2024-04-23 13:04:21 +0300

    More line numbers

--- vlasiator.cpp
+++ vlasiator.cpp
@@ -730,7 +730,8 @@ int main(int argn,char* args[]) {
             writeGhosts
          ) == false
       ) {
-         abort_mpi("FAILED TO WRITE GRID", 1);
+         // TODO make this std::format when we get C++20
+         abort_mpi(std::string(__FILE__) + ":" + std::to_string(__LINE__) + ": FAILED TO WRITE GRID", 1);
       }
 
       P::systemWriteDistributionWriteStride.pop_back();
@@ -947,7 +948,8 @@ int main(int argn,char* args[]) {
                writeGhosts
                ) == false
             ) {
-               abort_mpi("FAILED TO WRITE GRID", 1);
+               // TODO make this std::format when we get C++20
+               abort_mpi(std::string(__FILE__) + ":" + std::to_string(__LINE__) + ": FAILED TO WRITE GRID", 1);
             }
             P::systemWrites[i]++;
             // Special case for large timesteps
