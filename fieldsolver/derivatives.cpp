40ba2a9b303adfb7b35893e5bbcf92d5e8279eb1
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-12-19 12:12:43 +0200

    Replace repeated statement with a simple for loop

--- fieldsolver/derivatives.cpp
+++ fieldsolver/derivatives.cpp
@@ -87,7 +87,6 @@ void calculateDerivatives(std::span<const std::array<Real, fsgrids::bfield::N_BF
    const Real Peupstream = Parameters::electronTemperature * Parameters::electronDensity * physicalconstants::K_B;
    const Real Peconst = Peupstream * pow(Parameters::electronDensity, -Parameters::electronPTindex);
 
-   // clang-format off
    auto computePresE = [&dMoments, &Peconst](const auto& right, const auto& left, const auto& center) {
       // pres_e = const * np.power(rho_e, index)
       return Peconst * limiter(pow(left[mom::RHOQ] / physicalconstants::CHARGE, Parameters::electronPTindex),
@@ -131,37 +130,33 @@ void calculateDerivatives(std::span<const std::array<Real, fsgrids::bfield::N_BF
 #endif
       }
 
-      // clang-format off
+      static constexpr std::array mom{mom::RHOM, mom::RHOQ, mom::P_11, mom::P_22, mom::P_33, mom::VX, mom::VY, mom::VZ};
+
       // x
-      dMoments[dmo::drhomdx] = computeDerivative(notSysBoundary, mom::RHOM, momData.right, momData.left, momData.center);
-      dMoments[dmo::drhoqdx] = computeDerivative(notSysBoundary, mom::RHOQ, momData.right, momData.left, momData.center);
-      dMoments[dmo::dp11dx ] = computeDerivative(notSysBoundary, mom::P_11, momData.right, momData.left, momData.center);
-      dMoments[dmo::dp22dx ] = computeDerivative(notSysBoundary, mom::P_22, momData.right, momData.left, momData.center);
-      dMoments[dmo::dp33dx ] = computeDerivative(notSysBoundary, mom::P_33, momData.right, momData.left, momData.center);
-      dMoments[dmo::dVxdx  ] = computeDerivative(notSysBoundary, mom::VX  , momData.right, momData.left, momData.center);
-      dMoments[dmo::dVydx  ] = computeDerivative(notSysBoundary, mom::VY  , momData.right, momData.left, momData.center);
-      dMoments[dmo::dVzdx  ] = computeDerivative(notSysBoundary, mom::VZ  , momData.right, momData.left, momData.center);
+      static constexpr std::array dmix{
+          dmo::drhomdx, dmo::drhoqdx, dmo::dp11dx, dmo::dp22dx, dmo::dp33dx, dmo::dVxdx, dmo::dVydx, dmo::dVzdx,
+      };
+      for (size_t i = 0; i < mom.size(); i++) {
+         dMoments[dmix[i]] = computeDerivative(notSysBoundary, mom[i], momData.right, momData.left, momData.center);
+      }
 
       // y
-      dMoments[dmo::drhomdy] = computeDerivative(notSysBoundary, mom::RHOM, momData.up, momData.down, momData.center);
-      dMoments[dmo::drhoqdy] = computeDerivative(notSysBoundary, mom::RHOQ, momData.up, momData.down, momData.center);
-      dMoments[dmo::dp11dy ] = computeDerivative(notSysBoundary, mom::P_11, momData.up, momData.down, momData.center);
-      dMoments[dmo::dp22dy ] = computeDerivative(notSysBoundary, mom::P_22, momData.up, momData.down, momData.center);
-      dMoments[dmo::dp33dy ] = computeDerivative(notSysBoundary, mom::P_33, momData.up, momData.down, momData.center);
-      dMoments[dmo::dVxdy  ] = computeDerivative(notSysBoundary, mom::VX  , momData.up, momData.down, momData.center);
-      dMoments[dmo::dVydy  ] = computeDerivative(notSysBoundary, mom::VY  , momData.up, momData.down, momData.center);
-      dMoments[dmo::dVzdy  ] = computeDerivative(notSysBoundary, mom::VZ  , momData.up, momData.down, momData.center);
+      static constexpr std::array dmiy{
+          dmo::drhomdy, dmo::drhoqdy, dmo::dp11dy, dmo::dp22dy, dmo::dp33dy, dmo::dVxdy, dmo::dVydy, dmo::dVzdy,
+      };
+      for (size_t i = 0; i < mom.size(); i++) {
+         dMoments[dmiy[i]] = computeDerivative(notSysBoundary, mom[i], momData.up, momData.down, momData.center);
+      }
 
       // z
-      dMoments[dmo::drhomdz] = computeDerivative(notSysBoundary, mom::RHOM, momData.near, momData.far, momData.center);
-      dMoments[dmo::drhoqdz] = computeDerivative(notSysBoundary, mom::RHOQ, momData.near, momData.far, momData.center);
-      dMoments[dmo::dp11dz ] = computeDerivative(notSysBoundary, mom::P_11, momData.near, momData.far, momData.center);
-      dMoments[dmo::dp22dz ] = computeDerivative(notSysBoundary, mom::P_22, momData.near, momData.far, momData.center);
-      dMoments[dmo::dp33dz ] = computeDerivative(notSysBoundary, mom::P_33, momData.near, momData.far, momData.center);
-      dMoments[dmo::dVxdz  ] = computeDerivative(notSysBoundary, mom::VX  , momData.near, momData.far, momData.center);
-      dMoments[dmo::dVydz  ] = computeDerivative(notSysBoundary, mom::VY  , momData.near, momData.far, momData.center);
-      dMoments[dmo::dVzdz  ] = computeDerivative(notSysBoundary, mom::VZ  , momData.near, momData.far, momData.center);
+      static constexpr std::array dmiz{
+          dmo::drhomdz, dmo::drhoqdz, dmo::dp11dz, dmo::dp22dz, dmo::dp33dz, dmo::dVxdz, dmo::dVydz, dmo::dVzdz,
+      };
+      for (size_t i = 0; i < mom.size(); i++) {
+         dMoments[dmiz[i]] = computeDerivative(notSysBoundary, mom[i], momData.near, momData.far, momData.center);
+      }
 
+      // clang-format off
       // electron pressure
       dMoments[dmo::dPedx] = computePresE(momData.right, momData.left, momData.center);
       dMoments[dmo::dPedy] = computePresE(momData.up,    momData.down, momData.center);
