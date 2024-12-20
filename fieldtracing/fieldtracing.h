1c247d034be63c95d5bf9c35e56aaa68f8a4e470
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-12-20 09:49:38 +0200

    Change value to reference

--- fieldtracing/fieldtracing.h
+++ fieldtracing/fieldtracing.h
@@ -174,8 +174,8 @@ bool traceFullFieldFunction(fsgrid::FsGrid<std::array<Real, fsgrids::bfield::N_B
        getGlobalFsGridCellIndexForCoord(technicalGrid, {(TReal)r[0], (TReal)r[1], (TReal)r[2]});
    std::array<fsgrid::FsIndex_t, 3> fsgridCell = {(fsgrid::FsIndex_t)fsgridCellu[0], (fsgrid::FsIndex_t)fsgridCellu[1],
                                                   (fsgrid::FsIndex_t)fsgridCellu[2]};
-   const std::array<fsgrid::FsIndex_t, 3> localStart = technicalGrid.getLocalStart();
-   const std::array<fsgrid::FsIndex_t, 3> localSize = technicalGrid.getLocalSize();
+   const auto& localStart = technicalGrid.getLocalStart();
+   const auto& localSize = technicalGrid.getLocalSize();
    // Make the global index a local one, bypass the fsgrid function that yields (-1,-1,-1) also for ghost cells.
    fsgridCell[0] -= localStart[0];
    fsgridCell[1] -= localStart[1];
