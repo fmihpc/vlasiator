2fad5b95eebe93b53af93a6f80b01bc7e4c41677
veetihaaja <veeti.haaja1@gmail.com>
2025-02-12 16:29:11 +0200

    added new parameters for the extents of timeclass haloes

--- parameters.h
+++ parameters.h
@@ -71,6 +71,8 @@ struct Parameters {
    static int tc_test_type;
    static int tcMomentInterpolationType;
    static bool tcVMomentPropagation;
+   static int timeclassExactHaloExtent;
+   static int timeclassOuterHaloExtent;
 
    static bool tcDebugBox;          // Force timeclass 1 with the box
    static int tcOverrideTimeclass;  // Set fixed timeclass everywhere [debugging, default -1 i.e. off]
