ba6ca7d0e9de17a0b5ace8dbb0c2dcf18d8207cf
Juhana Lankinen <juhana.lankinen@csc.fi>
2025-02-14 11:49:23 +0200

    Add override to Diffusion

--- projects/Diffusion/Diffusion.h
+++ projects/Diffusion/Diffusion.h
@@ -40,20 +40,19 @@ namespace projects {
     public:
       Diffusion();
       virtual ~Diffusion();
-      
-      virtual bool initialize(void);
+
+      virtual bool initialize(void) override;
       static void addParameters(void);
-      virtual void getParameters(void);
+      virtual void getParameters(void) override;
       /*! set background field, should set it for all cells */
       virtual void setProjectBField(std::span<std::array<Real, fsgrids::bfield::N_BFIELD>> perb,
                                     std::span<std::array<Real, fsgrids::bgbfield::N_BGB>> bgb,
-                                    std::span<fsgrids::technical> technical, FieldSolverGrid &fsgrid);
+                                    std::span<fsgrids::technical> technical, FieldSolverGrid& fsgrid) override;
 
-      virtual Realf fillPhaseSpace(spatial_cell::SpatialCell *cell,
-                                  const uint popID,
-                                  const uint nRequested) const;
-      virtual void calcCellParameters(spatial_cell::SpatialCell* cell,creal& t);
-      
+      virtual Realf fillPhaseSpace(spatial_cell::SpatialCell* cell, const uint popID,
+                                   const uint nRequested) const override;
+      virtual void calcCellParameters(spatial_cell::SpatialCell* cell, creal& t) override;
+
       Real B0;
       std::vector<DiffusionSpeciesParameters> speciesParams;
    } ; // class Diffusion
