d75805dc25c7a7bcbe3e8cf868ef436b65f225d6
JonasSuni <jonas.suni@helsinki.fi>
2025-08-06 09:49:41 +0300

    Fix rho initialisation to assure constant total pressure

--- projects/Reconnection/Reconnection.cpp
+++ projects/Reconnection/Reconnection.cpp
@@ -99,9 +99,9 @@ namespace projects {
       const Real initV0Y = 0;
       const Real initV0Z = 0;
 
-      creal rhofac = (this->BX0*this->BX0 + this->BY0*this->BY0 + this->BZ0*this->BZ0) / 2.0 / physicalconstants::MU_0;
+      creal rhofac = (this->BX0*this->BX0 + this->BY0*this->BY0 + this->BZ0*this->BZ0) / 2.0 / physicalconstants::MU_0  / physicalconstants::K_B / initT;
 
-      initRho *= (1.0 + (1.0 - 1.0 / pow(cosh(z / (this->SCA_LAMBDA)), 2.0)) * rhofac / pow(cosh(x / (this->SCA_LAMBDA)), 2.0));
+      initRho += rhofac / pow(cosh(x / (this->SCA_LAMBDA)), 2.0);
 
       #ifdef USE_GPU
       vmesh::VelocityMesh *vmesh = cell->dev_get_velocity_mesh(popID);
