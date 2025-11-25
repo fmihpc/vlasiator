6c9a73947350cc3fa9ffe8079420788ef7637add
kstppd <kpapadakis@protonmail.com>
2025-11-25 12:16:04 +0200

    remove tinyAI mini app

--- mini-apps/tinyAI_tests/train.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "moving_image.h"
-
-double learn(MovingImage& img, std::size_t max_epochs, std::size_t batchsize, std::size_t neurons, std::size_t ff,
-           type_t scale, type_t lr);
