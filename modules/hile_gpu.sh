2b9d16e313d28414d91cb9991a9e9a221135e63f
Yann Pfau-Kempf <yann.pfau-kempf@csc.fi>
2026-04-28 00:58:57 +0300

    New modules scripts and more fixes

---
+++ modules/hile_gpu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export MPICH_GPU_SUPPORT_ENABLED=1
+module load papi
+module load rocm/6.3.4
+module load cray-pmi
+module load craype-accel-amd-gfx90a
+module load libfabric/1.22.0
+
