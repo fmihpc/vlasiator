5db6f08d1b840797b55c3bbf12b674647ff5eff3
Yann Pfau-Kempf <yann.pfau-kempf@csc.fi>
2026-04-28 12:03:38 +0300

    Check for existence of file in modules/modules.sh

--- modules/modules.sh
+++ modules/modules.sh
@@ -2,18 +2,30 @@
 
 if [[ z$VLASIATOR_ARCH != "z" ]]
 then
-   echo -e "\nLoading modules for VLASIATOR_ARCH="$VLASIATOR_ARCH"\n"
-   source modules/${VLASIATOR_ARCH}.sh
+   if [[ -f modules/${VLASIATOR_ARCH}.sh ]]
+   then
+      echo -e "\nLoading modules for VLASIATOR_ARCH="$VLASIATOR_ARCH"\n"
+      source modules/${VLASIATOR_ARCH}.sh
+      echo -e "Module list now:\n"
+      module list
+   else
+      echo -e "\nERROR, modules/"${VLASIATOR_ARCH}".sh not found!!\n"
+   fi
 else
    if [[ z$1 != "z" ]]
    then
-      echo -e "\nLoading modules for passed architecture "$1"\n"
-      source modules/${1}.sh
+      if [[ -f modules/${1}.sh ]]
+      then
+         echo -e "\nLoading modules for passed architecture "$1"\n"
+         source modules/${1}.sh
+         echo -e "Module list now:\n"
+         module list
+      else
+         echo -e "\nERROR, modules/"${1}".sh not found!!\n"
+      fi
    else
       echo -e "\nDefine VLASIATOR_ARCH or pass as argument the target architecture for which to load modules!\n"
    fi
 fi
 
-echo -e "Module list now:\n"
-module list
 
