3b31df6b506c0864df3b2a40f336fdc67944f2f1
Yann Pfau-Kempf <yann.pfau-kempf@csc.fi>
2026-05-07 19:00:47 +0300

    CMake in Carrington modules

--- modules/carrington_gcc_openmpi.sh
+++ modules/carrington_gcc_openmpi.sh
@@ -6,5 +6,6 @@ module load OpenMPI/4.1.6-GCC-13.2.0
 module load PMIx/4.2.6-GCCcore-13.2.0
 module load PAPI/7.1.0-GCCcore-13.2.0
 module load Boost/1.83.0-GCC-13.2.0
+module load CMake/3.27.6-GCCcore-13.2.0
 export VLASIATOR_ARCH=carrington_gcc_openmpi
 
