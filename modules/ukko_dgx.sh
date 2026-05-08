3b1bac430c134d5508d5cf107ee0fa7f783b2355
Yann Pfau-Kempf <yann.pfau-kempf@csc.fi>
2026-05-08 08:38:00 +0300

    CMake for ukko

--- modules/ukko_dgx.sh
+++ modules/ukko_dgx.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-module purge; module load OpenMPI/4.1.6.withucx-GCC-13.2.0 PAPI/7.1.0-GCCcore-13.2.0 CUDA/12.6.0
+module purge
+module load OpenMPI/4.1.6.withucx-GCC-13.2.0
+module load PAPI/7.1.0-GCCcore-13.2.0
+module load CUDA/12.6.0
+module load CMake/3.27.6-GCCcore-13.2.0
 
