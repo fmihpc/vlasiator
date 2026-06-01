80dd0e6b105de4f313438ae2e049409d2896579e
lassjsc <lasse.b.sihvonen@helsinki.fi>
2026-06-01 17:27:50 +0300

    chaning the map_order rng thingy to use mersenne-twister with scalar multiplier for less correlated seeds

--- vlasovsolver/vlasovmover.cpp
+++ vlasovsolver/vlasovmover.cpp
@@ -22,6 +22,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <random>
 #include <vector>
 #include <stdint.h>
 
@@ -421,8 +422,8 @@ void calculateAcceleration(const uint popID,const uint globalMaxSubcycles,const 
 
    // set seed, initialise generator and get value. The order is the same
    // for all cells, but varies with timestep.
-   std::default_random_engine rndState;
-   rndState.seed(P::tstep);
+   std::mt19937 rndState;
+   rndState.seed(9245*P::tstep); //magic number for less correlated seeds
    uint map_order = std::uniform_int_distribution<>(0,2)(rndState);
 
    // Calculate length of step for each cell
