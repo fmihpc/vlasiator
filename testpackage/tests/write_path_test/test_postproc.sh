7bbe3c5cf95a0878415b76a29eaa8bf21a109f13
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-06-15 10:33:13 +0300

    Remade the test to actually use the usual test system because it wasnt actually run_tests that does the test in CI

---
+++ testpackage/tests/write_path_test/test_postproc.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "--------------------------------------------------------------------------------------------"
+echo "   Testing write path                                                                       "
+echo "--------------------------------------------------------------------------------------------"
+
+filepath_test="./testing/foo/bar/bulk.0000000.vlsv" 
+if [[ ! -f $filepath_test ]]; then 
+   echo "File was not written to correct path at $filepath_test"
+   exit 1
+fi
+echo "Done"
