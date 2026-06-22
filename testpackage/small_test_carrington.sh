b933a50ee6645f5509c10802f04c8db65503e8a3
lassjsc <lasse.b.sihvonen@helsinki.fi>
2026-06-22 18:04:22 +0300

    batch script for generting repeated runs with shifting seed and also the analysis script

--- testpackage/small_test_carrington.sh
+++ testpackage/small_test_carrington.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#SBATCH -t 01:30:00        # Run time (hh:mm:ss)
-#SBATCH --job-name=ctestpackage
+#SBATCH -t 00:30:00        # Run time (hh:mm:ss)
+#SBATCH --job-name=repeat_test_acc
 ##SBATCH -A spacephysics
-#SBATCH --constraint="carrington"
+#SBATCH --constraint="amd"
 # test short medium 20min1d 3d
 #SBATCH -p short
 #SBATCH --exclusive
 ##SBATCH --nodes=1
 #SBATCH -c 1                 # CPU cores per task
 #SBATCH -n 1                  # number of tasks
-#SBATCH --mem-per-cpu=5G
+#SBATCH --array=0-9
+#SBATCH --mem=10G
+#SBATCH --no-requeue
 ##SBATCH --hint=multithread
 
 # If 1, the reference vlsv files are generated
@@ -17,33 +19,33 @@
 create_verification_files=0
 
 # folder for all reference data
-reference_dir="/home/lassejsc/testing/"
-# cd $SLURM_SUBMIT_DIR
+reference_dir="/home/siclasse/"
+mainfolder="/home/siclasse/vlasiator_hile/vlasiator/testpackage/"
+cd $SLURM_SUBMIT_DIR
 
 diffbin="../vlsvdiff_DP"
 
 #compare agains which revision
 #reference_revision="CI_reference"
-reference_revision="local"
+reference_revision="rng_ref2"
 
-# module purge
-# module load GCC/13.2.0
-# module load OpenMPI/4.1.6-GCC-13.2.0
-# module load PMIx/4.2.6-GCCcore-13.2.0
-# module load PAPI/7.1.0-GCCcore-13.2.0
-# module load Boost/1.83.0-GCC-13.2.0
-#module load xthi
-# export UCX_TLS=dc_mlx5
-# export UCX_NET_DEVICES=mlx5_0:1
+module purge
+module load GCC/13.2.0
+module load OpenMPI/4.1.6-GCC-13.2.0
+module load PMIx/4.2.6-GCCcore-13.2.0
+module load PAPI/7.1.0-GCCcore-13.2.0
+module load Boost/1.83.0-GCC-13.2.0
+module load xthi
+export UCX_TLS=dc_mlx5
+export UCX_NET_DEVICES=mlx5_0:1
 
-# export OMPI_MCA_btl='^uct,ofi'
-# export OMPI_MCA_pml='ucx'
-# export OMPI_MCA_mtl='^ofi'
+export OMPI_MCA_btl='^uct,ofi'
+export OMPI_MCA_pml='ucx'
+export OMPI_MCA_mtl='^ofi'
 #Carrington has 2 x 16 cores per node, plus hyperthreading
 ht=2
 # t=$SLURM_CPUS_PER_TASK
 # export OMP_NUM_THREADS=$t
-mainfolder="$PWD"
 #command for running stuff
 run_command="mpirun -n 1 "
 small_run_command="mpirun -n 1 "
@@ -62,13 +64,19 @@ umask 007
 source test_definitions_small.sh
 wait
 # Run tests
-for n in {1..100}; do
-  bin="/home/lassejsc/vlasiator/vlasiator --map_order_shift $n"
+jobcount=$(($SLURM_ARRAY_TASK_MAX - $SLURM_ARRAY_TASK_MIN + 1))
+index=$(($SLURM_ARRAY_TASK_ID - $SLURM_ARRAY_TASK_MIN))
+
+# echo $index $jobcount $SLURM_ARRAY_TASK_COUNT $SLURM_ARRAY_TASK_ID
+# echo $index
+for n in $(seq $(($index * 50)) $((($index + 1) * 50 - 1))); do
+  echo "N=$n"
+  bin="/home/siclasse/vlasiator_hile/vlasiator/vlasiator --map_order_shift $n"
   source run_tests.sh
 
   wait
   # mv logfile.txt logfile_$n.txt
   cd $mainfolder
   # cd $SLURM_SUBMIT_DIR
-  # wait
+  wait
 done
