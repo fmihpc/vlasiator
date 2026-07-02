c9fb31d774b5ed7836e1a6dcf027682f684926fc
lassejsc <lasse.b.sihvonen@helsinki.fi>
2026-07-02 15:48:20 +0300

    testing

--- testpackage/test_definitions_small.sh
+++ testpackage/test_definitions_small.sh
@@ -1,23 +1,21 @@
-
 ## Define test and runs
 
 # Assume binaries are set in job script
 #bin=vlasiator
 #diffbin=vlsvdiff_DP
 
-if [ ! -f $bin ]
-then
-   echo Executable $bin does not exist
-   exit
+if [ ! -f $bin ]; then
+  echo Executable $bin does not exist
+  exit
 fi
 
 # where the tests are run
 run_dir="run"
 
-# where the directories for different tests, including cfg and other needed data files are located 
+# where the directories for different tests, including cfg and other needed data files are located
 test_dir="tests"
 
-# Counter for creating tests
+## Counter for creating tests
 index=1
 
 #######
@@ -31,7 +29,7 @@ comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v proton"
 variable_components[${index}]="0 0 1 2"
 single_cell[${index}]=1
-((index+=1))
+((index += 1))
 
 # 2  basic multipeak acceleration test (dynamic timestep)
 test_name[${index}]="acctest_2_maxw_500k_100k_20kms_10deg"
@@ -40,7 +38,7 @@ comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v proton"
 variable_components[${index}]="0 0 1 2"
 single_cell[${index}]=1
-((index+=1))
+((index += 1))
 
 # 3  acceleration test w/ substepping
 test_name[${index}]="acctest_3_substeps"
@@ -49,7 +47,7 @@ comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v proton"
 variable_components[${index}]="0 0 1 2"
 single_cell[${index}]=1
-((index+=1))
+((index += 1))
 
 # 4  Helium acceleration
 test_name[${index}]="acctest_4_helium"
@@ -58,7 +56,7 @@ comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="helium/vg_rho helium/vg_v helium/vg_v helium/vg_v"
 variable_components[${index}]="0 0 1 2"
 single_cell[${index}]=1
-((index+=1))
+((index += 1))
 
 # 5  Gyration test with protons and antiprotons (multipop)
 test_name[${index}]="acctest_5_proton_antiproton"
@@ -67,7 +65,7 @@ comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v proton"
 variable_components[${index}]="0 0 1 2"
 single_cell[${index}]=1
-((index+=1))
+((index += 1))
 
 #######
 # 1D/2D TRANSLATION TESTS (6..9)
@@ -79,7 +77,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv bulk.0000002.vlsv bulk.0000003.vlsv
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v"
 variable_components[${index}]="0 0 1 2"
-((index+=1))
+((index += 1))
 
 # 7  Inflow and outflow boundaries
 test_name[${index}]="Flowthrough_x_inflow_y_outflow"
@@ -87,7 +85,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv bulk.0000002.vlsv bulk.0000003.vlsv
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v"
 variable_components[${index}]="0 0 1 2"
-((index+=1))
+((index += 1))
 
 # 8  Inflow and outflow boundaries together with acceleration
 test_name[${index}]="Flowthrough_x_inflow_y_outflow_acc"
@@ -95,7 +93,7 @@ comparison_vlsv[${index}]="bulk.0000002.vlsv bulk.0000003.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v fg_b fg_b fg_b fg_e fg_e fg_e"
 variable_components[${index}]="0 0 1 2 0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 # 9  Flowthrough with timevarying inflow
 test_name[${index}]="Flowthrough_1D_timevarying"
@@ -104,7 +102,7 @@ comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v fg_b fg_b fg_b fg_e fg_e fg_e"
 variable_components[${index}]="0 0 1 2 0 1 2 0 1 2 0 0"
 single_cell[${index}]=1
-((index+=1))
+((index += 1))
 
 #######
 # 3D AMR TRANSLATION TESTS (10..14)
@@ -116,7 +114,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv bulk.0000010.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v"
 variable_components[${index}]="0 0 1 2"
-((index+=1))
+((index += 1))
 
 # 11 AMR translation, single v-cell, sinewave signal
 test_name[${index}]="transtest_2_amr_sinewave"
@@ -124,7 +122,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv bulk.0000010.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v"
 variable_components[${index}]="0 0 1 2"
-((index+=1))
+((index += 1))
 
 # 12 AMR translation, single v-cell, square signal
 test_name[${index}]="transtest_3_amr_square"
@@ -132,7 +130,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv bulk.0000010.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v"
 variable_components[${index}]="0 0 1 2"
-((index+=1))
+((index += 1))
 
 # 13 Large AMR translation flowthrough test
 test_name[${index}]="Flowthrough_amr"
@@ -140,7 +138,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v fg_b fg_b fg_b fg_e fg_e fg_e"
 variable_components[${index}]="0 0 1 2 0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 # 14 Large (dynamic) AMR translation flowthrough test
 test_name[${index}]="Flowthrough_damr"
@@ -148,7 +146,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv bulk.0000002.vlsv bulk.0000003.vlsv
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v vg_amr_alpha1 vg_amr_alpha2 fg_b fg_b fg_b fg_e fg_e fg_e"
 variable_components[${index}]="0 0 1 2 0 0 0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 #######
 # RESTARTING TESTS (15..18)
@@ -161,7 +159,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v fg_b fg_b fg_b fg_e fg_e fg_e"
 variable_components[${index}]="0 0 1 2 0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 # 16 Restart read and propagate with translation only
 test_name[${index}]="restart_read"
@@ -169,7 +167,7 @@ comparison_vlsv[${index}]="initial-grid.0000000.vlsv bulk.0000002.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v fg_b fg_b fg_b fg_e fg_e fg_e"
 variable_components[${index}]="0 0 1 2 0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 # 17 Restart write with translation, acceleration, and fieldsolver
 test_name[${index}]="restart_write_acc"
@@ -177,7 +175,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v fg_b fg_b fg_b fg_e fg_e fg_e"
 variable_components[${index}]="0 0 1 2 0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 # 18 Restart read and propagate with translation, acceleration, and fieldsolver
 test_name[${index}]="restart_read_acc"
@@ -185,7 +183,7 @@ comparison_vlsv[${index}]="initial-grid.0000000.vlsv bulk.0000002.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v fg_b fg_b fg_b fg_e fg_e fg_e"
 variable_components[${index}]="0 0 1 2 0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 #######
 # FIELDSOLVER TESTS (19..20)
@@ -197,7 +195,7 @@ comparison_vlsv[${index}]="fullf.0000001.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="fg_b fg_b fg_b fg_e fg_e fg_e"
 variable_components[${index}]="0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 # 20 3D Field solver test w/ subcycles
 test_name[${index}]="test_fp_substeps"
@@ -205,7 +203,7 @@ comparison_vlsv[${index}]="fullf.0000001.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="fg_b fg_b fg_b fg_e fg_e fg_e"
 variable_components[${index}]="0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 #######
 # GLOBAL 2D TESTS (21..22)
@@ -217,7 +215,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv bulk.0000002.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v fg_b fg_b fg_b fg_e fg_e fg_e proton"
 variable_components[${index}]="0 0 1 2 0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 # 22 Very small polar magnetosphere, with subcycling in ACC or FS
 test_name[${index}]="Magnetosphere_polar_small"
@@ -225,7 +223,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv bulk.0000002.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v fg_b fg_b fg_b fg_e fg_e fg_e proton/vg_v_nonthermal proton/vg_v_nonthermal proton/vg_v_nonthermal proton/vg_ptensor_nonthermal_diagonal proton/vg_ptensor_nonthermal_diagonal proton/vg_ptensor_nonthermal_diagonal proton"
 variable_components[${index}]="0 0 1 2 0 1 2 0 1 2 0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 #######
 # GLOBAL 3D TESTS (23..24)
@@ -237,7 +235,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv bulk.0000002.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v fg_b fg_b fg_b fg_e fg_e fg_e"
 variable_components[${index}]="0 0 1 2 0 1 2 0 1 2"
-((index+=1))
+((index += 1))
 
 # 24 Ionosphere 3D (not a very physical or successful test at the moment but verifies some things about IG grid and outputs all datareducers)
 test_name[${index}]="Ionosphere_small"
@@ -245,7 +243,7 @@ comparison_vlsv[${index}]="bulk.0000001.vlsv bulk.0000002.vlsv"
 comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_v proton/vg_v proton/vg_v fg_b fg_b fg_b fg_e fg_e fg_e ig_upmappedarea ig_fac ig_rhon ig_potential"
 variable_components[${index}]="0 0 1 2 0 1 2 0 1 2 0 0 0 0"
-((index+=1))
+((index += 1))
 
 #######
 # SUBGRID TESTS (25)
@@ -258,13 +256,12 @@ comparison_phiprof[${index}]="phiprof_0.txt"
 variable_names[${index}]="proton/vg_rho proton/vg_ptensor_diagonal proton/vg_ptensor_diagonal proton/vg_ptensor_diagonal proton/vg_ptensor_offdiagonal proton/vg_ptensor_offdiagonal proton/vg_ptensor_offdiagonal proton"
 variable_components[${index}]="0 0 1 2 0 1 2"
 single_cell[${index}]=1
-((index+=1))
+((index += 1))
 
 # choose tests to run (default: all tests)
-run_tests=( )
-for (( c=1; c<$index; c++ ))
-do
-    run_tests+=($c)
+run_tests=()
+for ((c = 1; c < $index; c++)); do
+  run_tests+=($c)
 done
 
 # Alternatively, set tests manually, e.g.
