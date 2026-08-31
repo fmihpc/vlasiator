3577984e16c8016673db495d1e6c9747fc896220
Urs Ganse <urs.ganse@helsinki.fi>
2026-08-31 23:27:32 +0300

    Attempt to fix CI on HILE by enabling http_proxy on the compute nodes.
    This way, the fetch_library script should work. Maybe.

--- modules/hile_cpu.sh
+++ modules/hile_cpu.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+export HTTP_PROXY=http://www-cache.cs.helsinki.fi:3128
+export HTTPS_PROXY=http://www-cache.cs.helsinki.fi:3128
+export https_proxy=http://www-cache.cs.helsinki.fi:3128
+export http_proxy=http://www-cache.cs.helsinki.fi:3128
+
 module load papi
 module load cray-pmi
 module load libfabric/1.22.0
--- modules/hile_gpu.sh
+++ modules/hile_gpu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export HTTP_PROXY=http://www-cache.cs.helsinki.fi:3128
+export HTTPS_PROXY=http://www-cache.cs.helsinki.fi:3128
+export https_proxy=http://www-cache.cs.helsinki.fi:3128
+export http_proxy=http://www-cache.cs.helsinki.fi:3128
+
 export MPICH_GPU_SUPPORT_ENABLED=1
 module load papi
 module load rocm/6.3.4
