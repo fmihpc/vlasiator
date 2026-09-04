a78a889100716fd502d4e8680e2f1c05d1945304
Markku Alho <markku.alho@helsinki.fi>
2026-09-04 16:34:42 +0300

    Draft Makefile and testpackage sbatch for maahinen

---
+++ testpackage/small_test_maahinen.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#SBATCH -t 01:30:00        # Run time (hh:mm:ss)
+#SBATCH --job-name=ctestpackage
+# test short medium 20min1d 3d
+#SBATCH -p turso
+#SBATCH --exclusive
+#SBATCH --nodes=1
+#SBATCH -w ukko3-[814-816]
+#SBATCH -c 4                 # CPU cores per task
+#SBATCH -n 16                  # number of tasks
+#SBATCH --mem=64G
+#SBATCH --hint=multithread
+
+# If 1, the reference vlsv files are generated
+# if 0 then we check the v1 against reference files
+create_verification_files=0
+
+# folder for all reference data
+reference_dir="/turso/group/spacephysics/vlasiator/testpackage/"
+cd $SLURM_SUBMIT_DIR
+
+bin="/home/mjalho/maahinen/vlasiator/vlasiator"
+diffbin="/home/mjalho/maahinen/vlasiator/vlsvdiff_DP"
+
+#compare agains which revision
+#reference_revision="CI_reference"
+reference_revision="current"
+
+source ~/modules_maahinen.sh
+# export UCX_NET_DEVICES=eth5,mlx5_0:1 # This is important for multi-node performance!
+export UCX_TLS=dc_mlx5
+export UCX_NET_DEVICES=mlx5_0:1
+
+export OMPI_MCA_btl='^uct,ofi'
+export OMPI_MCA_pml='ucx'
+export OMPI_MCA_mtl='^ofi'
+#Carrington has 2 x 16 cores per node, plus hyperthreading
+ht=2
+t=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=$t
+
+#command for running stuff
+run_command="srun --mpi=pmix -n $SLURM_NTASKS "
+small_run_command="srun --mpi=pmix -n 1"
+run_command_tools="srun -n 1 "
+
+umask 007
+# Launch the OpenMP job to the allocated compute node
+echo "Running $exec on $SLURM_NTASKS mpi tasks, with $t threads per task on $SLURM_NNODES nodes ($ht threads per physical core)"
+
+# Optional debug printouts
+# srun -np 1 /appl/bin/hostinfo
+# srun --cpu-bind=cores bash -c 'echo -n "task $SLURM_PROCID (node $SLURM_NODEID): "; taskset -cp $$' | sort
+# srun --mpi=pmix --cpu-bind=cores xthi
+
+# Define test
+source test_definitions_small.sh
+wait
+# Run tests
+source run_tests.sh
+wait
