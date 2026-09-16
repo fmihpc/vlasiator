955e73ad5e00cdcf45915f0b3a9787bbad23a6a0
Yann Pfau-Kempf <yann.pfau-kempf@csc.fi>
2026-09-16 15:26:18 +0300

    Do not remove header library folders to avoid wiping out WIP.

--- fetch_submodules.sh
+++ fetch_submodules.sh
@@ -33,37 +33,34 @@ git_use_commit() {
 mkdir -p submodules
 cd submodules
 
-rm -rf fsgrid
+# Not rm -fr'ing the old folders so it'll error out on cloning.
+# Do not accidentally nuke Markku et al.'s WIP on libraries.
+
 git clone --depth=1 https://github.com/fmihpc/fsgrid.git
 cd fsgrid
 git_use_commit "$FSGRID_COMMIT"
 cd ..
 
-rm -rf dccrg
 git clone --depth=1 -b vlasiator-version https://github.com/fmihpc/dccrg.git
 cd dccrg
 git_use_commit "$DCCRG_COMMIT"
 cd ..
 
-rm -rf eigen
 git clone --depth=1 -b master https://gitlab.com/libeigen/eigen.git
 cd eigen
 git_use_commit "$EIGEN_COMMIT"
 cd ..
 
-rm -rf vectorclass
 git clone --depth=1 https://github.com/vectorclass/version2 vectorclass
 cd vectorclass
 git_use_commit "$VECTORCLASS_COMMIT"
 cd ..
 
-rm -rf vectorclass-addon
 git clone --depth=1 https://github.com/vectorclass/add-on vectorclass-addon
 cd vectorclass-addon
 git_use_commit "$VECTORCLASS_ADDON_COMMIT"
 cd ..
 
-rm -rf hashinator
 git clone --depth=1 https://github.com/fmihpc/hashinator.git
 cd hashinator
 git_use_commit "$HASHINATOR_COMMIT"
